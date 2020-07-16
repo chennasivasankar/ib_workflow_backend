1e4535ed5fcfb154ff1e5306fe2d355cf845ab72
chennasivasankar <siva.chenna@ibhubs.co.in>
2020-07-16 20:22:44 +0530

    refractor clean code

--- ib_iam/tests/views/user_logout/test_case_01.py
+++ ib_iam/tests/views/user_logout/test_case_01.py
@@ -1,6 +1,8 @@
 """
 Update user password
 """
+from unittest.mock import patch, Mock
+
 import pytest
 from django_swagger_utils.utils.test_v1 import TestUtils
 from . import APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX
@@ -12,8 +14,13 @@ class TestCase01UserLogoutAPITestCase(TestUtils):
     REQUEST_METHOD = REQUEST_METHOD
     URL_SUFFIX = URL_SUFFIX
     SECURITY = {'oauth': {'scopes': ['write']}}
+
     @pytest.mark.django_db
-    def test_case(self, snapshot):
+    @patch(
+        "ib_iam.adapters.auth_service.AuthService.user_log_out_from_a_device"
+    )
+    def test_case(self, user_log_out_from_a_device_mock, snapshot):
+        user_log_out_from_a_device_mock.return_value = Mock()
         body = {}
         path_params = {}
         query_params = {}
