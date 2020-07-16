f2d69c7b443880475ada41f07898a6c8a41eb659
Mukunda <mukunda.pedda@ibhubs.co.in>
2020-07-16 17:14:44 +0530

    adds models

---
+++ ib_tasks/tests/factories/models.py
@@ -0,0 +1,10 @@
+# import factory
+# from ib_tasks.models.field import Field
+#
+# class FieldFactory(factory.Factory)
+#
+#     class Meta:
+#         model = Field
+#
+#     gof_id = "FIN_GOF_VENDOR_TYPE"
+#     field_id = factory.Sequence(lambda n: 'field%d' % n)
