b94843ec04d21c12c39e9f5c5fbcdd98525502fd
geetha.k <geetha.k@ibhubs.co.in>
2020-07-16 12:19:58 +0530

    add input type in presenter get task details interactor

--- ib_boards/interactors/presenter_interfaces/presenter_interface.py
+++ ib_boards/interactors/presenter_interfaces/presenter_interface.py
@@ -1,7 +1,14 @@
 import abc
+from typing import List
+
+from ib_boards.interactors.storage_interfaces.dtos import (
+    TaskFieldsDTO, TaskActionsDTO)
 
 
 class PresenterInterface(abc.ABC):
     @abc.abstractmethod
-    def get_response_for_task_details(self, task_fields_dto, task_actions_dto, task_ids):
+    def get_response_for_task_details(self,
+                                      task_fields_dto: List[TaskFieldsDTO],
+                                      task_actions_dto: List[TaskActionsDTO],
+                                      task_ids: List[str]):
         pass
