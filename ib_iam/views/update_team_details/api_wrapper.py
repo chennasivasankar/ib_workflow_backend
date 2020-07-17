f87dc71718b4bab22f805fc1a1779181a5566e7b
Your Name <jaswanth.mamidipudi@ibhubs.co.in>
2020-07-17 15:38:19 +0530

    Add API wrapper implementation for update team details

--- ib_iam/views/update_team_details/api_wrapper.py
+++ ib_iam/views/update_team_details/api_wrapper.py
@@ -1,48 +1,40 @@
 from django_swagger_utils.drf_server.utils.decorator.interface_decorator \
     import validate_decorator
 from .validator_class import ValidatorClass
+from ib_iam.storages.team_storage_implementation import (
+    TeamStorageImplementation
+)
+from ib_iam.presenters.team_presenter_implementation import (
+    TeamPresenterImplementation
+)
+from ib_iam.interactors.team_interactor import (
+    TeamInteractor
+)
+from ib_iam.interactors.storage_interfaces.dtos import UpdateTeamParametersDTO
 
 
 @validate_decorator(validator_class=ValidatorClass)
 def api_wrapper(*args, **kwargs):
-    # ---------MOCK IMPLEMENTATION---------
+    user_obj = kwargs["user"]
+    user_id = str(user_obj.id)
+    request_data = kwargs["request_data"]
+    team_id = request_data["team_id"]
+    name = request_data["name"]
+    description = request_data["description"]
 
-    try:
-        from ib_iam.views.update_team_details.request_response_mocks \
-            import REQUEST_BODY_JSON
-        body = REQUEST_BODY_JSON
-    except ImportError:
-        body = {}
+    storage = TeamStorageImplementation()
+    presenter = TeamPresenterImplementation()
+    interactor = TeamInteractor(storage=storage)
 
-    test_case = {
-        "path_params": {'team_id': 122},
-        "query_params": {},
-        "header_params": {},
-        "body": body,
-        "securities": [{'oauth': ['update']}]
-    }
+    update_team_parameters_dto = UpdateTeamParametersDTO(
+        team_id=team_id,
+        name=name,
+        description=description
+    )
 
-    from django_swagger_utils.drf_server.utils.server_gen.mock_response \
-        import mock_response
-    try:
-        response = ''
-        status_code = 200
-        if '200' in ['200', '400', '401', '404']:
-            from ib_iam.views.update_team_details.request_response_mocks \
-                import RESPONSE_200_JSON
-            response = RESPONSE_200_JSON
-            status_code = 200
-        elif '201' in ['200', '400', '401', '404']:
-            from ib_iam.views.update_team_details.request_response_mocks \
-                import RESPONSE_201_JSON
-            response = RESPONSE_201_JSON
-            status_code = 201
-    except ImportError:
-        response = ''
-        status_code = 200
-    response_tuple = mock_response(
-        app_name="ib_iam", test_case=test_case,
-        operation_name="update_team_details",
-        kwargs=kwargs, default_response_body=response,
-        group_name="", status_code=status_code)
-    return response_tuple
+    response = interactor.get_list_of_teams_wrapper(
+        user_id=user_id,
+        update_team_parameters_dto=update_team_parameters_dto,
+        presenter=presenter
+    )
+    return response
