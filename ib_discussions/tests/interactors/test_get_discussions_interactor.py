db9760b12dcbdacbcceafcd148b50afe5aa6237c
chennasivasankar <siva.chenna@ibhubs.co.in>
2020-07-17 20:38:04 +0530

    elp for get discussions

---
+++ ib_discussions/tests/interactors/test_get_discussions_interactor.py
@@ -0,0 +1,15 @@
+'''
+# TODO
+1. validate offset
+2. validate limit
+3. validate the entity id
+4. validate the entity type for entity id
+5. get discussion_set_id for entity id and entity type
+6. get discussions for discussion_set_id
+7. get total discussions count
+8. get user details
+'''
+
+
+class TestGetDiscussionsInteractor:
+    pass
