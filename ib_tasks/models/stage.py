fb6dba637ddf0237d43d4a02ffe318dddc2c876f
geetha.k <geetha.k@ibhubs.co.in>
2020-07-18 21:05:43 +0530

    "changes in stage model fields name"

--- ib_tasks/models/stage.py
+++ ib_tasks/models/stage.py
@@ -4,4 +4,4 @@ class Stage(models.Model):
     stage_id = models.CharField(max_length=400, unique=True)
     display_name = models.TextField()
     value = models.IntegerField()
-    display_name = models.TextField()
+    display_logic = models.TextField()
