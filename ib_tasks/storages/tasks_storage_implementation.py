abcb2b616a12be4b582983fe2976308e0ec2bdf4
Mukunda <mukunda.pedda@ibhubs.co.in>
2020-07-18 15:23:11 +0530

    adds storage implementation for create and update fields

--- ib_tasks/storages/tasks_storage_implementation.py
+++ ib_tasks/storages/tasks_storage_implementation.py
@@ -7,6 +7,8 @@ from ib_tasks.interactors.storage_interfaces.dtos import (
 )
 from ib_tasks.interactors.storage_interfaces.task_storage_interface import \
     TaskStorageInterface
+from ib_tasks.models.field import Field
+from ib_tasks.models.field_role import FieldRole
 
 
 class TasksStorageImplementation(TaskStorageInterface):
@@ -25,7 +27,9 @@ class TasksStorageImplementation(TaskStorageInterface):
         pass
 
     def create_fields(self, field_dtos: List[FieldDTO]):
-        pass
+        from ib_tasks.models.field import Field
+        fields = self._get_fields(field_dtos)
+        Field.objects.bulk_create(fields)
 
     def get_existing_gof_ids_in_given_gof_ids(
             self, gof_ids: List[str]
@@ -114,16 +118,104 @@ class TasksStorageImplementation(TaskStorageInterface):
         pass
 
     def update_fields(self, field_dtos: List[FieldDTO]):
-        pass
+        list_of_fields = [
+            'display_name', 'gof_id',
+            'required', 'field_type',
+            'field_values', 'allowed_formats',
+            'help_text', 'tooltip',
+            'placeholder_text', 'error_messages',
+            'validation_regex'
+        ]
+        fields = self._get_fields(field_dtos)
+        Field.objects.bulk_update(fields, list_of_fields)
 
-    def update_fields_roles(self, field_roles_dto: List[FieldRoleDTO]):
-        pass
+    def update_fields_roles(self, field_role_dtos: List[FieldRoleDTO]):
+        list_of_fields = ["permission_type"]
+        field_ids = [
+            field_role_dto.field_id
+            for field_role_dto in field_role_dtos
+        ]
 
-    def create_fields_roles(self, field_roles_dto: List[FieldRoleDTO]):
-        pass
+        field_role_objs = list(FieldRole.objects.filter(field_id__in=field_ids))
+        for field_role_obj in field_role_objs:
+            field_role_dto = self._get_matching_field_role_dto(
+                field_role_obj, field_role_dtos
+            )
+            field_role_obj.permission_type = field_role_dto.permission_type
+
+        FieldRole.objects.bulk_update(field_role_objs, list_of_fields)
+
+    def create_fields_roles(self, field_role_dtos: List[FieldRoleDTO]):
+        fields_roles = self._get_fields_roles(field_role_dtos)
+        FieldRole.objects.bulk_create(fields_roles)
 
     def get_existing_field_ids(self, field_ids: List[str]) -> List[str]:
-        pass
+        from ib_tasks.models.field import Field
+        existing_field_ids = list(
+            Field.objects.filter(field_id__in=field_ids).values_list("field_id", flat=True)
+        )
+        return existing_field_ids
 
     def get_existing_gof_ids(self, gof_ids: List[str]) -> List[str]:
-        pass
+        from ib_tasks.models.gof import GoF
+        existing_gof_ids = list(
+            GoF.objects.filter(gof_id__in=gof_ids).values_list("gof_id", flat=True)
+        )
+        return existing_gof_ids
+
+    def get_fields_role_dtos(self, field_ids: List[str]) -> List[str]:
+        field_role_objs = FieldRole.objects.filter(field_id__in=field_ids)
+        field_role_dtos = [
+            FieldRoleDTO(
+                field_id=field_role_obj.field_id,
+                role=field_role_obj.role,
+                permission_type=field_role_obj.permission_type
+            )
+            for field_role_obj in field_role_objs
+        ]
+        return field_role_dtos
+
+    @staticmethod
+    def _get_fields(field_dtos: List[FieldDTO]):
+        fields = [
+            Field(
+                field_id=field_dto.field_id,
+                display_name=field_dto.field_display_name,
+                required=field_dto.required,
+                field_type=field_dto.field_type,
+                field_values=field_dto.field_values,
+                allowed_formats=field_dto.allowed_formats,
+                help_text=field_dto.help_text,
+                tooltip=field_dto.tool_tip,
+                placeholder_text=field_dto.placeholder_text,
+                error_messages=field_dto.error_message,
+                validation_regex=field_dto.validation_regex,
+                gof_id=field_dto.gof_id
+            )
+            for field_dto in field_dtos
+        ]
+        return fields
+
+    @staticmethod
+    def _get_matching_field_role_dto(
+            field_role_obj: FieldRole, field_role_dtos: List[FieldRoleDTO]
+    ):
+        old_field_id = field_role_obj.field_id
+        old_role = field_role_obj.role
+        for field_role_dto in field_role_dtos:
+            new_field_id = field_role_dto.field_id
+            new_role = field_role_dto.role
+            if old_field_id == new_field_id and old_role == new_role:
+                return field_role_dto
+
+    @staticmethod
+    def _get_fields_roles(field_role_dtos):
+        fields_roles = [
+            FieldRole(
+                field_id=field_role_dto.field_id,
+                role=field_role_dto.role,
+                permission_type=field_role_dto.permission_type
+            )
+            for field_role_dto in field_role_dtos
+        ]
+        return fields_roles
