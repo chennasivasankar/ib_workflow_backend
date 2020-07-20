3f58b0f07447b81875dadcb14a8a26794e986713
prathyushakl <jonathan@notmyemail.com>
2020-07-20 19:47:56 +0530

    added user role validation interactor

---
+++ ib_tasks/interactors/user_role_validation_interactor.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class UserRoleValidationInteractor:
+
+    def user_role_validation(self, user_id: str,
+                             role_ids: List[str]) -> bool:
+        if "ALL_ROLES" in role_ids:
+            return True
+
+        user_role_ids = self._get_user_role_ids(user_id)
+        for user_role_id in user_role_ids:
+            if user_role_id in role_ids:
+                return True
+        return False
+
+    @staticmethod
+    def _get_user_role_ids(user_id: str) -> List[str]:
+        from ib_tasks.adapters.roles_service_adapter import \
+            get_roles_service_adapter
+        roles_service_adapter = get_roles_service_adapter()
+        roles_service = roles_service_adapter.roles_service
+        user_role_ids = \
+            roles_service.get_user_role_ids(user_id=user_id)
+        return user_role_ids
