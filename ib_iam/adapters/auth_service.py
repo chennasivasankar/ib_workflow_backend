de001d17fa42930054ef108ec2da0050854463bb
chennasivasankar <siva.chenna@ibhubs.co.in>
2020-07-21 17:41:27 +0530

    Add adpaters

--- ib_iam/adapters/auth_service.py
+++ ib_iam/adapters/auth_service.py
@@ -17,19 +17,49 @@ class UserTokensDTO:
 
 class AuthService:
 
+    @property
+    def interface(self):
+        from ib_users.interfaces.service_interface import ServiceInterface
+        service_interface = ServiceInterface()
+        return service_interface
+
     def get_user_tokens_dto_for_given_email_and_password_dto(
             self, email_and_password_dto: EmailAndPasswordDTO
     ) -> UserTokensDTO:
-        pass
+        user_auth_tokens_dto = self.interface.get_user_auth_tokens_for_login_with_email_and_password(
+            email=email_and_password_dto.email,
+            password=email_and_password_dto.password
+        )
+        converted_user_tokens_dto = self._convert_to_user_tokens_dto(
+            user_auth_tokens_dto)
+        return converted_user_tokens_dto
 
     def user_log_out_from_a_device(self, user_id: int):
         pass
 
     def get_reset_password_token(self, email: str,
                                  expires_in_sec: int) -> str:
-        pass
+        reset_password_token = self.interface.get_reset_password_token_for_reset_password(
+            email=email, token_expiry_in_seconds=expires_in_sec
+        )
+        return reset_password_token
 
     def update_user_password_with_reset_password_token(
             self, reset_password_token: str, password: str
     ):
-        pass
+        self.interface.reset_password_for_given_user_password_reset_token(
+            token=reset_password_token, new_password=password
+        )
+
+    @staticmethod
+    def _convert_to_user_tokens_dto(user_auth_tokens_dto):
+        from datetime import datetime
+        converted_user_tokens_dto = UserTokensDTO(
+            user_id=user_auth_tokens_dto.user_id,
+            access_token=user_auth_tokens_dto.access_token,
+            refresh_token=user_auth_tokens_dto.refresh_token,
+            expires_in_seconds=(
+                    user_auth_tokens_dto.expires_in-datetime.now()
+            ).total_seconds()
+        )
+        return converted_user_tokens_dto
--- ib_iam/tests/adapters/test_auth_service_adapter.py
+++ ib_iam/tests/adapters/test_auth_service_adapter.py
@@ -1,2 +1,104 @@
-class AuthService:
-    pass
+from unittest.mock import patch
+
+from freezegun import freeze_time
+
+
+class TestAuthService:
+
+    @patch(
+        "ib_users.interfaces.service_interface.ServiceInterface.get_user_auth_tokens_for_login_with_email_and_password"
+    )
+    @freeze_time("2020-01-14 12:00:01")
+    def test_get_user_tokens_dto_for_given_email_and_password_dto(
+            self, get_user_auth_tokens_for_login_with_email_and_password_mock
+    ):
+        # Arrange
+        from ib_iam.adapters.auth_service import EmailAndPasswordDTO
+        email_and_password_dto = EmailAndPasswordDTO(
+            email="test@gmail.com",
+            password="test123"
+        )
+
+        from ib_users.interactors.third_party.user_tokens_generator import \
+            UserAuthTokensDTO
+        from datetime import datetime
+        user_auth_tokens_dto = UserAuthTokensDTO(
+            access_token="asdfaldskfjdfdlsdkf",
+            refresh_token="sadfenkljkdfeller",
+            expires_in=datetime(year=2199, month=1, day=2),
+            user_id="11"
+        )
+
+        from ib_iam.adapters.auth_service import UserTokensDTO
+        expected_user_tokens_dtos = UserTokensDTO(
+            access_token='asdfaldskfjdfdlsdkf',
+            refresh_token='sadfenkljkdfeller',
+            expires_in_seconds=5647665599,
+            user_id='11'
+        )
+
+        get_user_auth_tokens_for_login_with_email_and_password_mock.return_value \
+            = user_auth_tokens_dto
+
+        from ib_iam.adapters.service_adapter import ServiceAdapter
+        service_adapter = ServiceAdapter()
+        auth_service = service_adapter.auth_service
+
+        # Act
+        response = auth_service.get_user_tokens_dto_for_given_email_and_password_dto(
+            email_and_password_dto=email_and_password_dto
+        )
+
+        # Assert
+        assert response == expected_user_tokens_dtos
+
+    @patch(
+        "ib_users.interfaces.service_interface.ServiceInterface.get_reset_password_token_for_reset_password"
+    )
+    def test_get_reset_password_token(
+            self, get_reset_password_token_for_reset_password_mock
+    ):
+        # Arrange
+        email = "test@gmail.com"
+        expires_in_sec = 5647665599
+        expected_token = "string"
+
+        get_reset_password_token_for_reset_password_mock.return_value \
+            = expected_token
+
+        from ib_iam.adapters.service_adapter import ServiceAdapter
+        service_adapter = ServiceAdapter()
+        auth_service = service_adapter.auth_service
+
+        # Act
+        response = auth_service.get_reset_password_token(
+            email=email, expires_in_sec=expires_in_sec
+        )
+
+        # Assert
+        assert response == expected_token
+
+    @patch(
+        "ib_users.interfaces.service_interface.ServiceInterface.reset_password_for_given_user_password_reset_token"
+    )
+    def test_update_user_password_with_reset_password_token(
+            self, reset_password_for_given_user_password_reset_token_mock
+    ):
+        # Arrange
+        reset_password_token = "string"
+        password = "string123"
+
+        # Act
+        from ib_iam.adapters.service_adapter import ServiceAdapter
+        service_adapter = ServiceAdapter()
+        auth_service = service_adapter.auth_service
+
+        # Act
+        response = auth_service.update_user_password_with_reset_password_token(
+            reset_password_token=reset_password_token,
+            password=password
+        )
+
+        # Assert
+        reset_password_for_given_user_password_reset_token_mock. \
+            assert_called_once()
