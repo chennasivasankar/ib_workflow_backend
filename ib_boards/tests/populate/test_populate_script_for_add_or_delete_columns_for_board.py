5cce085ee1a0a415ff8ea02adcad348702308b49
pavankumar <pavankumar.pamuru@ibhubs.com.in>
2020-07-21 16:34:08 +0530

    add tests for invalid json field

--- ib_boards/tests/populate/test_populate_script_for_add_or_delete_columns_for_board.py
+++ ib_boards/tests/populate/test_populate_script_for_add_or_delete_columns_for_board.py
@@ -43,6 +43,20 @@ class TestPopulateBoardsAndColumnsInteractor:
                 boards_columns_dicts=data_dict
             )
 
+    def test_with_invalid_json_in_task_summary_fields_dict_raise_exception(self):
+        # Arrange
+        from ib_boards.tests.common_fixtures.populate_data import \
+            populate_dict_with_invalid_json_for_summary_fields
+
+        data_dict = populate_dict_with_invalid_json_for_summary_fields()
+        populate_script = PopulateCreateBoardsAndColumns()
+
+        # Act
+        with pytest.raises(InvalidJsonFormat) as error:
+            populate_script.populate_create_boards_and_columns(
+                boards_columns_dicts=data_dict
+            )
+
     def test_with_valid_data_return_dtos(self):
         # Arrange
         from ib_boards.tests.common_fixtures.populate_data import \
