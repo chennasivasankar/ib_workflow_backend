5985aa4b7a9ebcfcd2d66da2df79688ec23a097d
pavankumar <pavankumar.pamuru@ibhubs.com.in>
2020-07-21 13:30:56 +0530

    add population script for create boards and columns

---
+++ ib_boards/populate/populate_script_for_add_or_delete_columns_for_board.py
@@ -0,0 +1,111 @@
+"""
+Created on: 21/07/20
+Author: Pavankumar Pamuru
+
+"""
+from typing import List, Dict
+
+from ib_boards.interactors.dtos import BoardDTO, ColumnDTO
+
+
+class InvalidDataFormat(Exception):
+    pass
+
+
+class StorageImplementation(object):
+    pass
+
+
+class PopulateAddOrDeleteColumnsForBoard:
+
+    def populate_add_or_delete_for_columns_for_board(
+            self, boards_columns_dicts: List[Dict]):
+        self.validate_keys_in_given_dict(
+            boards_columns_dicts=boards_columns_dicts
+        )
+        column_dtos = self.get_column_dtos_from_dict(boards_columns_dicts)
+        from ib_boards.interactors.add_or_delete_columns_for_board_interactor \
+            import AddOrDeleteColumnsForBoardInteractor
+        storage = StorageImplementation()
+        interactor = AddOrDeleteColumnsForBoardInteractor(
+            storage=storage
+        )
+        interactor.add_or_delete_columns_for_board_wrapper(
+            column_dtos=column_dtos
+        )
+
+    def get_column_dtos_from_dict(self, boards_columns_dicts: List[Dict]):
+        column_dtos = [
+            self._convert_column_dict_to_column_dto(
+                column_dict=column_dict
+            )
+            for column_dict in boards_columns_dicts
+        ]
+        return column_dtos
+
+    def _convert_column_dict_to_column_dto(self, column_dict: Dict) -> ColumnDTO:
+        task_template_stages = self._get_task_template_stages_dto(
+            column_dict['task_template_stages']
+        )
+        list_view_fields = self._get_task_template_summary_fields_dto(
+            column_dict['list_view_fields']
+        )
+        kanban_view_fields = self._get_task_template_summary_fields_dto(
+            column_dict['kanban_view_fields']
+        )
+        return ColumnDTO(
+            column_id=column_dict['column_id'],
+            display_name=column_dict['column_display_name'],
+            display_order=column_dict['display_order'],
+            task_template_stages=column_dict['task_template_stages'],
+            user_role_ids=column_dict['user_role_ids'],
+            column_summary=column_dict['column_summary'],
+            list_view_fields=column_dict['list_view_fields'],
+            kanban_view_fields=column_dict['kanban_view_fields'],
+            board_id=column_dict['board_id'],
+        )
+
+    def validate_keys_in_given_dict(self, boards_columns_dicts: List[Dict]):
+        from schema import Schema, SchemaError, And, Optional
+        import json
+        schema = Schema(
+            [{
+                "board_id": And(str, len),
+                "board_display_name": And(str, len),
+                "column_id": And(str, len),
+                "column_display_name": And(str, len),
+                "display_order": And(str, len),
+                "user_role_ids": List[And(str, len)],
+                Optional("column_summary"): str,
+                Optional("column_actions"): str,
+                "task_template_stages": json,
+                "kanban_view_fields": json,
+                "list_view_fields": json
+
+            }]
+        )
+        try:
+            schema.validate(boards_columns_dicts)
+        except SchemaError:
+            self._raise_exception_for_board_valid_format()
+
+    def _raise_exception_for_board_valid_format(self):
+        raise InvalidDataFormat("""{
+                "board_id": "board_id"
+                "board_display_name": "board_display_name"
+                "column_id": "column_id"
+                "column_display_name": "column_display_name"
+                "display_order": "display_order"
+                "user_role_ids": "user_role_ids"
+                "column_summary": "column_summary"
+                "column_actions": "column_actions"
+                "task_template_stages": "task_template_stages"
+                "kanban_view_fields": "kanban_view_fields"
+                "list_view_fields": "list_view_fields"
+        }""")
+
+    def _get_task_template_stages_dto(self, json_object):
+        return 1
+
+    def _get_task_template_summary_fields_dto(self, json_object):
+        return 1
