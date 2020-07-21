4ce008256765fbadd09acac9bda8c93e2dd8a399
chennasivasankar <siva.chenna@ibhubs.co.in>
2020-07-21 16:15:20 +0530

    Add populate spreadsheet

---
+++ ib_iam/populate/spreedsheet_utils.py
@@ -0,0 +1,38 @@
+import gspread
+
+
+class SpreadSheetUtil:
+    def __init__(self):
+        scope = [
+            "https://www.googleapis.com/auth/drive",
+            "https://spreadsheets.google.com/feeds",
+        ]
+
+        from oauth2client.service_account import ServiceAccountCredentials
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            "ib_iam/populate/ibworkflow-5916d0f027b0.json", scope)
+
+        self.gc = gspread.authorize(credentials)
+
+    def read_spread_sheet_data_and_get_row_wise_dicts(
+            self, spread_sheet_name, sub_sheet_name):
+        wks = self.gc.open(spread_sheet_name).worksheet(sub_sheet_name)
+        spread_sheet_data = wks.get_all_values()
+
+        headers = spread_sheet_data[0]
+        headers = [header.strip() for header in headers]
+
+        row_wise_dicts = []
+        row = 1
+        while row < len(spread_sheet_data):
+            data = spread_sheet_data[row]
+
+            row_wise_dict = dict()
+            for index, key in enumerate(headers):
+                row_wise_dict[key] = data[index].strip()
+            row_wise_dicts.append(row_wise_dict)
+
+            row += 1
+
+        return row_wise_dicts
+
