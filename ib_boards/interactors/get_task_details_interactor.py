9a9a7081cc9728d389c18c771a3518bc1ce35067
geetha.k <geetha.k@ibhubs.co.in>
2020-07-21 19:35:39 +0530

    "add venv to git ignore"

--- ib_boards/interactors/get_task_details_interactor.py
+++ ib_boards/interactors/get_task_details_interactor.py
@@ -13,12 +13,10 @@ class GetTaskDetailsInteractor:
 
     def get_task_details_wrapper(self, presenter: PresenterInterface,
                                  tasks_dtos: List[TaskDTO], user_id: str):
-
         task_fields_dtos, task_actions_dtos, task_details = self.get_task_details(
             tasks_dtos, user_id)
         return presenter.get_response_for_task_details(
             task_actions_dtos, task_fields_dtos, task_details)
-
 
     def get_task_details(self, tasks_dtos: List[TaskDTO], user_id: str):
         task_details = self._get_task_and_column_ids(tasks_dtos)
@@ -31,7 +29,7 @@ class GetTaskDetailsInteractor:
     @staticmethod
     def _get_task_and_column_ids(tasks_dtos):
         task_details = [TaskColumnDTO(
-                    task_id=task_dto.task_id,
-                    column_id=task_dto.column_id)
-                for task_dto in tasks_dtos]
+            task_id=task_dto.task_id,
+            column_id=task_dto.column_id)
+            for task_dto in tasks_dtos]
         return task_details
