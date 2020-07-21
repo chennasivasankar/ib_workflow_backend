71f39e3637c9bfb7827ea0c498e9868a51f17dfe
chennasivasankar <siva.chenna@ibhubs.co.in>
2020-07-21 20:27:23 +0530

    Add interface with tests

--- ib_iam/adapters/auth_service.py
+++ ib_iam/adapters/auth_service.py
@@ -1,5 +1,7 @@
 import dataclasses
 
+from ib_users.validators.base_validator import CustomException
+
 
 @dataclasses.dataclass
 class EmailAndPasswordDTO:
@@ -26,30 +28,88 @@ class AuthService:
     def get_user_tokens_dto_for_given_email_and_password_dto(
             self, email_and_password_dto: EmailAndPasswordDTO
     ) -> UserTokensDTO:
-        user_auth_tokens_dto = self.interface.get_user_auth_tokens_for_login_with_email_and_password(
-            email=email_and_password_dto.email,
-            password=email_and_password_dto.password
-        )
-        converted_user_tokens_dto = self._convert_to_user_tokens_dto(
-            user_auth_tokens_dto)
-        return converted_user_tokens_dto
+        try:
+            user_auth_tokens_dto = self.interface.get_user_auth_tokens_for_login_with_email_and_password(
+                email=email_and_password_dto.email,
+                password=email_and_password_dto.password
+            )
+        except CustomException as err:
+            from ib_users.exceptions.custom_exception_constants import \
+                INVALID_EMAIL
+            from ib_iam.interactors.user_login_interactor import \
+                IncorrectPassword
+            if err.error_type == INVALID_EMAIL.code:
+                from ib_iam.exceptions.custom_exceptions import InvalidEmail
+                raise InvalidEmail
+            from ib_users.exceptions.custom_exception_constants import \
+                PASSWORD_AT_LEAST_1_SPECIAL_CHARACTER
+            if err.error_type == PASSWORD_AT_LEAST_1_SPECIAL_CHARACTER.code:
+                raise IncorrectPassword
+            from ib_users.exceptions.custom_exception_constants import \
+                PASSWORD_MIN_LENGTH_IS
+            if err.error_type == PASSWORD_MIN_LENGTH_IS.code:
+                raise IncorrectPassword
+            from ib_users.exceptions.custom_exception_constants import \
+                USER_ACCOUNT_IS_DEACTIVATED
+            if err.error_type == USER_ACCOUNT_IS_DEACTIVATED.code:
+                from ib_iam.exceptions.custom_exceptions import \
+                    UserAccountDoesNotExist
+                raise UserAccountDoesNotExist
+        else:
+            converted_user_tokens_dto = self._convert_to_user_tokens_dto(
+                user_auth_tokens_dto
+            )
+            return converted_user_tokens_dto
 
     def user_log_out_from_a_device(self, user_id: int):
         pass
 
     def get_reset_password_token(self, email: str,
                                  expires_in_sec: int) -> str:
-        reset_password_token = self.interface.get_reset_password_token_for_reset_password(
-            email=email, token_expiry_in_seconds=expires_in_sec
-        )
-        return reset_password_token
+        try:
+            reset_password_token = self.interface.get_reset_password_token_for_reset_password(
+                email=email, token_expiry_in_seconds=expires_in_sec
+            )
+            return reset_password_token
+        except CustomException as err:
+            from ib_users.exceptions.custom_exception_constants import \
+                INVALID_EMAIL
+            if err.error_type == INVALID_EMAIL.code:
+                from ib_iam.exceptions.custom_exceptions import InvalidEmail
+                raise InvalidEmail
 
     def update_user_password_with_reset_password_token(
             self, reset_password_token: str, password: str
     ):
-        self.interface.reset_password_for_given_user_password_reset_token(
-            token=reset_password_token, new_password=password
-        )
+        from ib_users.interactors.exceptions.user_credentials_exceptions import \
+            InvalidTokenException
+        from ib_users.interactors.exceptions.user_credentials_exceptions import \
+            TokenExpiredException
+        try:
+            self.interface.reset_password_for_given_user_password_reset_token(
+                token=reset_password_token, new_password=password
+            )
+        except CustomException as err:
+            from ib_users.exceptions.custom_exception_constants import \
+                PASSWORD_AT_LEAST_1_SPECIAL_CHARACTER
+            if err.error_type == PASSWORD_AT_LEAST_1_SPECIAL_CHARACTER.code:
+                from ib_iam.interactors.update_user_password_interactor import \
+                    PasswordAtLeastOneSpecialCharacter
+                raise PasswordAtLeastOneSpecialCharacter
+            from ib_users.exceptions.custom_exception_constants import \
+                PASSWORD_MIN_LENGTH_IS
+            if err.error_type == PASSWORD_MIN_LENGTH_IS.code:
+                from ib_iam.interactors.update_user_password_interactor import \
+                    PasswordMinLength
+                raise PasswordMinLength
+        except InvalidTokenException:
+            from ib_iam.interactors.update_user_password_interactor import \
+                TokenDoesNotExist
+            raise TokenDoesNotExist
+        except TokenExpiredException:
+            from ib_iam.interactors.update_user_password_interactor import \
+                TokenHasExpired
+            raise TokenHasExpired
 
     @staticmethod
     def _convert_to_user_tokens_dto(user_auth_tokens_dto):
@@ -59,7 +119,7 @@ class AuthService:
             access_token=user_auth_tokens_dto.access_token,
             refresh_token=user_auth_tokens_dto.refresh_token,
             expires_in_seconds=(
-                    user_auth_tokens_dto.expires_in-datetime.now()
+                    user_auth_tokens_dto.expires_in - datetime.now()
             ).total_seconds()
         )
         return converted_user_tokens_dto
---
+++ ib_iam/tests/adapters/test_get_reset_password_token.py
@@ -0,0 +1,59 @@
+from unittest.mock import patch
+
+import pytest
+
+
+class TestGetResetPasswordToken:
+
+    @patch(
+        "ib_users.interfaces.service_interface.ServiceInterface.get_reset_password_token_for_reset_password"
+    )
+    def test_get_reset_password_token(
+            self, get_reset_password_token_for_reset_password_mock
+    ):
+        # Arrange
+        email = "test@gmail.com"
+        expires_in_sec = 5647665599
+        expected_token = "string"
+
+        get_reset_password_token_for_reset_password_mock.return_value \
+            = expected_token
+
+        from ib_iam.adapters.service_adapter import ServiceAdapter
+        service_adapter = ServiceAdapter()
+        auth_service = service_adapter.auth_service
+
+        # Act
+        response = auth_service.get_reset_password_token(
+            email=email, expires_in_sec=expires_in_sec
+        )
+
+        # Assert
+        assert response == expected_token
+
+    @patch(
+        "ib_users.interfaces.service_interface.ServiceInterface.get_reset_password_token_for_reset_password"
+    )
+    def test_with_invalid_email_raise_exception_for_reset_password_token(
+            self, get_reset_password_token_for_reset_password_mock
+    ):
+        # Arrange
+        email = "test@gmail.com"
+        expires_in_sec = 5647665599
+
+        from ib_users.constants.custom_exception_messages import INVALID_EMAIL
+        from ib_users.validators.base_validator import CustomException
+        get_reset_password_token_for_reset_password_mock.side_effect \
+            = CustomException(exception_type=INVALID_EMAIL.code,
+                              message=INVALID_EMAIL.message)
+
+        from ib_iam.adapters.service_adapter import ServiceAdapter
+        service_adapter = ServiceAdapter()
+        auth_service = service_adapter.auth_service
+
+        # Assert
+        from ib_iam.exceptions.custom_exceptions import InvalidEmail
+        with pytest.raises(InvalidEmail):
+            auth_service.get_reset_password_token(
+                email=email, expires_in_sec=expires_in_sec
+            )
