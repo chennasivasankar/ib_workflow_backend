2594fa70dd7d3169404fd1aa86619adff91dba90
DurgaPrime <durgaprasad@ibhubs.co>
2020-07-22 20:11:27 +0530

    Red state -> delete User -> valid user_id -> delete user roles from user roles db

--- ib_iam/tests/interactors/test_delete_user_interactor.py
+++ ib_iam/tests/interactors/test_delete_user_interactor.py
@@ -40,4 +40,20 @@ class TestDeleteUserInteractor:
             user_id=delete_user_id)
         presenter_mock.get_delete_user_response.assert_called_once()
 
+    def test_delete_user_given_valid_user_id_then_delete_user_roles_from_user_roles_db(
+            self, storage_mock, presenter_mock):
+        admin_user_id = "1"
+        delete_user_id = "2"
+        interactor = DeleteUserInteractor(storage=storage_mock)
 
+        presenter_mock.get_delete_user_response.return_value = Mock()
+
+        interactor.delete_user_wrapper(user_id=admin_user_id,
+                                       delete_user_id=delete_user_id,
+                                       presenter=presenter_mock)
+
+        storage_mock.delete_user.assert_called_once_with(
+            user_id=delete_user_id)
+        storage_mock.delete_user_roles.assert_called_once_with(
+            user_id=delete_user_id)
+        presenter_mock.get_delete_user_response.assert_called_once()
