a2b56115f522a267135c25f6ca271d21c8078545
Spidey03 <sayhai2naveenkumar@gmail.com>
2020-07-23 16:25:52 +0530

    Split storages for each api

--- ib_iam/storages/storage_implementation.py
+++ ib_iam/storages/storage_implementation.py
@@ -1,11 +1,8 @@
 from typing import List
 
-from ib_iam.interactors.storage_interfaces.dtos import UserTeamDTO, \
-    UserRoleDTO, UserCompanyDTO, UserDTO, CompanyDTO, \
-    TeamDTO, RoleIdAndNameDTO, RoleDTO
+from ib_iam.interactors.storage_interfaces.dtos import RoleDTO
 from ib_iam.interactors.storage_interfaces.storage_interface import \
     StorageInterface
-from ib_iam.models import Role
 
 
 class StorageImplementation(StorageInterface):
@@ -15,8 +12,8 @@ class StorageImplementation(StorageInterface):
         user = UserDetails.objects.get(user_id=user_id)
         return user.is_admin
 
-
     def create_roles(self, role_dtos: List[RoleDTO]):
+        from ib_iam.models import Role
         role_objects = [
             Role(role_id=role_dto.role_id, name=role_dto.name,
                  description=role_dto.description)
@@ -24,7 +21,7 @@ class StorageImplementation(StorageInterface):
         Role.objects.bulk_create(role_objects)
 
     def get_valid_role_ids(self, role_ids: List[str]):
-        valid_role_ids = Role.objects.filter(role_id__in=role_ids).values_list(
-            "role_id", flat=True
-        )
+        from ib_iam.models import Role
+        valid_role_ids = Role.objects.filter(role_id__in=role_ids).\
+            values_list("role_id", flat=True)
         return list(valid_role_ids)
