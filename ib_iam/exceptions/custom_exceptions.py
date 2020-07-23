bb16c232e11df7b40c38e5eece593f9d7e34db5a
Spidey03 <sayhai2naveenkumar@gmail.com>
2020-07-23 09:38:20 +0530

    Fix controller test cases except get configuration test

---
+++ ib_iam/exceptions/custom_exceptions.py
@@ -0,0 +1,62 @@
+class UserIsNotAdmin(Exception):
+    pass
+
+
+class InvalidOffsetValue(Exception):
+    pass
+
+
+class InvalidLimitValue(Exception):
+    pass
+
+
+class OffsetValueIsGreaterthanLimitValue(Exception):
+    pass
+
+
+class UserAccountAlreadyExistWithThisEmail(Exception):
+    pass
+
+
+class GivenNameIsEmpty(Exception):
+    pass
+
+
+class InvalidEmailAddressException(Exception):
+    pass
+
+
+class NameShouldNotContainsNumbersSpecCharactersException(Exception):
+    pass
+
+
+class RoleIdsAreInvalidException(Exception):
+    pass
+
+
+class InvalidCompanyIdException(Exception):
+    pass
+
+
+class TeamIdsAreInvalidException(Exception):
+    pass
+
+
+class UserDoesNotExist(Exception):
+    pass
+
+
+class RoleNameIsEmptyException(Exception):
+    pass
+
+
+class RoleDescriptionIsEmptyException(Exception):
+    pass
+
+
+class RoleIdFormatIsInvalidException(Exception):
+    pass
+
+
+class DuplicateRoleIdsException(Exception):
+    pass
