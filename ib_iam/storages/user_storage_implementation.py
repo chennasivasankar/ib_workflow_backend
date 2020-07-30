49dd3efb2cee2cd518f5627d7e6d1071b47e2f98
DurgaPrime <durgaprasad@ibhubs.co>
2020-07-30 18:07:19 +0530

    refactoring user related storage implementation

--- ib_iam/storages/user_storage_implementation.py
+++ ib_iam/storages/user_storage_implementation.py
@@ -83,15 +83,11 @@ class UserStorageImplementation(UserStorageInterface):
 
     def get_users_who_are_not_admins(self, offset, limit) -> List[UserDTO]:
         from ib_iam.models import UserDetails
-        user_dtos = []
-        users = UserDetails.objects. \
-                    filter(is_admin=False)[offset: offset + limit]
-        for user in users:
-            user_dtos.append(UserDTO(
-                user_id=user.user_id,
-                is_admin=user.is_admin,
-                company_id=str(user.company_id)
-            ))
+        users = UserDetails.objects.filter(is_admin=False)[
+                offset: offset + limit]
+        user_dtos = [UserDTO(
+            user_id=user_object.user_id, is_admin=user_object.is_admin,
+            company_id=str(user_object.company_id)) for user_object in users]
         return user_dtos
 
     def get_total_count_of_users_for_query(self):
@@ -121,69 +117,54 @@ class UserStorageImplementation(UserStorageInterface):
         from ib_iam.models import UserRole
         user_roles = UserRole.objects.filter(user_id__in=user_ids) \
             .select_related('role')
-        role_dtos = []
-        for user_role in user_roles:
-            role = user_role.role
-            role_dto = UserRoleDTO(
-                user_id=user_role.user_id,
-                role_id=str(role.role_id),
-                name=role.name,
-                description=role.description
-            )
-            role_dtos.append(role_dto)
+        role_dtos = [self._convert_to_user_role_dto(user_role)
+                     for user_role in user_roles]
         return role_dtos
+
+    @staticmethod
+    def _convert_to_user_role_dto(user_role):
+        role = user_role.role
+        return UserRoleDTO(
+            user_id=user_role.user_id, role_id=role.role_id,
+            name=role.name, description=role.description)
 
     def get_company_details_of_users_bulk(
             self, user_ids: List[str]) -> List[UserCompanyDTO]:
         from ib_iam.models import UserDetails
         user_companies = UserDetails.objects.filter(user_id__in=user_ids) \
             .select_related('company')
-        company_dtos = []
-        for user_company in user_companies:
-            company = user_company.company
-            company_dto = UserCompanyDTO(
-                user_id=user_company.user_id,
-                company_id=str(company.company_id),
-                company_name=company.name
-            )
-            company_dtos.append(company_dto)
-        return company_dtos
+        user_company_dtos = [self._convert_user_company_dto(user_company)
+                             for user_company in user_companies]
+        return user_company_dtos
+
+    @staticmethod
+    def _convert_user_company_dto(user_company) -> UserCompanyDTO:
+        company = user_company.company
+        return UserCompanyDTO(
+            user_id=user_company.user_id,
+            company_id=str(company.company_id), company_name=company.name)
 
     def get_companies(self) -> List[CompanyIdAndNameDTO]:
         from ib_iam.models import Company
-        companies = []
         company_query_set = Company.objects.values('company_id', 'name')
-        for company in company_query_set:
-            companies.append(
-                CompanyIdAndNameDTO(
-                    company_id=str(company['company_id']),
-                    company_name=company['name']
-                )
-            )
-        return companies
+        company_dtos = [CompanyIdAndNameDTO(
+            company_id=str(company_object['company_id']),
+            company_name=company_object['name']) for company_object in
+            company_query_set]
+        return company_dtos
 
     def get_teams(self) -> List[TeamIdAndNameDTO]:
-        teams = []
         from ib_iam.models import Team
         team_query_set = Team.objects.values('team_id', 'name')
-        for team in team_query_set:
-            teams.append(
-                TeamIdAndNameDTO(
-                    team_id=str(team['team_id']),
-                    team_name=team['name']
-                )
-            )
-        return teams
+        team_dtos = [TeamIdAndNameDTO(
+            team_id=str(team_object['team_id']),
+            team_name=team_object['name']) for team_object in team_query_set]
+        return team_dtos
 
     def get_roles(self) -> List[RoleIdAndNameDTO]:
-        roles = []
         from ib_iam.models import Role
         role_query_set = Role.objects.values('role_id', 'name')
-        for role in role_query_set:
-            roles.append(
-                RoleIdAndNameDTO(
-                    role_id=str(role['role_id']),
-                    name=role['name']
-                )
-            )
-        return roles
+        role_dtos = [RoleIdAndNameDTO(
+            role_id=str(role_object['role_id']),
+            name=role_object['name']) for role_object in role_query_set]
+        return role_dtos
