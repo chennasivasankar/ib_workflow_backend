447e36e7d87b1c018b9f6b58352f05abb6261000
Mukunda <mukunda.pedda@ibhubs.co.in>
2020-07-31 18:20:48 +0530

    adds models to admin panel

--- ib_tasks/admin.py
+++ ib_tasks/admin.py
@@ -7,6 +7,10 @@ from ib_tasks.models.field import Field
 from ib_tasks.models.task_template import TaskTemplate
 from ib_tasks.models.gof_role import GoFRole
 from ib_tasks.models.field_role import FieldRole
+from ib_tasks.models.task import Task
+from ib_tasks.models.task_gof import TaskGoF
+from ib_tasks.models.task_gof_field import TaskGoFField
+from ib_tasks.models.task_stage import TaskStage
 
 
 admin.site.register(GoF)
@@ -18,3 +22,7 @@ admin.site.register(Stage)
 admin.site.register(TaskTemplateStatusVariable)
 admin.site.register(StageAction)
 admin.site.register(TaskTemplateInitialStage)
+admin.site.register(Task)
+admin.site.register(TaskGoF)
+admin.site.register(TaskGoFField)
+admin.site.register(TaskStage)
--- ib_tasks/tests/storages/snapshots/snap_test_create_or_update_task_storage_implementation.py
+++ ib_tasks/tests/storages/snapshots/snap_test_create_or_update_task_storage_implementation.py
@@ -2,15 +2,15 @@
 # snapshottest: v1 - https://goo.gl/zC4yUc
 from __future__ import unicode_literals
 
-from snapshottest import GenericRepr, Snapshot
+from snapshottest import Snapshot, GenericRepr
 
 
 snapshots = Snapshot()
 
 snapshots['TestCreateOrUpdateTaskStorageImplementation.test_given_task_id_returns_task_gof_dtos task_gof_dtos'] = [
-    GenericRepr("TaskGoFDTO(task_gof_id=1, gof_id='gof_id_0', same_gof_order=1)"),
-    GenericRepr("TaskGoFDTO(task_gof_id=2, gof_id='gof_id_1', same_gof_order=1)"),
-    GenericRepr("TaskGoFDTO(task_gof_id=3, gof_id='gof_id_2', same_gof_order=1)")
+    GenericRepr("TaskGoFDTO(task_gof_id=1, gof_id='gof_1', same_gof_order=1)"),
+    GenericRepr("TaskGoFDTO(task_gof_id=2, gof_id='gof_2', same_gof_order=1)"),
+    GenericRepr("TaskGoFDTO(task_gof_id=3, gof_id='gof_3', same_gof_order=1)")
 ]
 
 snapshots['TestCreateOrUpdateTaskStorageImplementation.test_given_task_gof_ids_returns_task_gof_field_dtos task_gof_field_dtos'] = [
@@ -22,10 +22,10 @@ snapshots['TestCreateOrUpdateTaskStorageImplementation.test_given_task_gof_ids_r
 ]
 
 snapshots['TestCreateOrUpdateTaskStorageImplementation.test_given_gof_ids_and_user_roles_returns_gof_ids_having_permission_for_roles gof_ids_having_permission'] = [
+    'gof_13',
     'gof_15',
-    'gof_16',
-    'gof_7',
-    'gof_9'
+    'gof_21',
+    'gof_22'
 ]
 
 snapshots['TestCreateOrUpdateTaskStorageImplementation.test_given_field_ids_and_user_roles_returns_field_ids_having_permission_for_roles field_ids_having_permission'] = [
