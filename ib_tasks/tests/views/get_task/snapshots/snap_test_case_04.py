5dcc7d09ec0b8a724355e84d4c7b570785c6bb6d
Mukunda <mukunda.pedda@ibhubs.co.in>
2020-08-01 16:23:16 +0530

    adds snapshot test cases

--- ib_tasks/tests/views/get_task/snapshots/snap_test_case_04.py
+++ ib_tasks/tests/views/get_task/snapshots/snap_test_case_04.py
@@ -7,50 +7,10 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestCase02GetTaskAPITestCase.test_case status_code'] = '200'
+snapshots['TestCase04GetTaskAPITestCase.test_case status_code'] = '200'
 
-snapshots['TestCase02GetTaskAPITestCase.test_case body'] = {
+snapshots['TestCase04GetTaskAPITestCase.test_case body'] = {
     'gofs': [
-        {
-            'gof_fields': [
-                {
-                    'field_id': 'FIELD_ID-0',
-                    'field_response': 'field_response_0'
-                },
-                {
-                    'field_id': 'FIELD_ID-3',
-                    'field_response': 'field_response_3'
-                },
-                {
-                    'field_id': 'FIELD_ID-6',
-                    'field_response': 'field_response_6'
-                },
-                {
-                    'field_id': 'FIELD_ID-9',
-                    'field_response': 'field_response_9'
-                }
-            ],
-            'gof_id': 'gof_1',
-            'same_gof_order': 1.0
-        },
-        {
-            'gof_fields': [
-                {
-                    'field_id': 'FIELD_ID-1',
-                    'field_response': 'field_response_1'
-                },
-                {
-                    'field_id': 'FIELD_ID-4',
-                    'field_response': 'field_response_4'
-                },
-                {
-                    'field_id': 'FIELD_ID-7',
-                    'field_response': 'field_response_7'
-                }
-            ],
-            'gof_id': 'gof_2',
-            'same_gof_order': 1.0
-        }
     ],
     'stages_with_actions': [
         {
