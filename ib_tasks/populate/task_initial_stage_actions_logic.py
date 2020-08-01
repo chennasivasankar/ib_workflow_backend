1c913a4d08ea38e19b9e670e380dd6915f6eaf58
prathyushakl <jonathan@notmyemail.com>
2020-08-01 14:40:57 +0530

    updated all roles id case in create gofs

--- ib_tasks/populate/task_initial_stage_actions_logic.py
+++ ib_tasks/populate/task_initial_stage_actions_logic.py
@@ -208,3 +208,13 @@ def VENDOR_DRAFTS_Save_as_Draft(task_dict, global_constants, stage_value_dict):
 def VENDOR_DRAFTS_Submit_for_Approval(task_dict, global_constants, stage_value_dict):
 	a = '9'
 	return task_dict
+
+
+def PR_CREATE_VENDOR_Save_as_Draft(task_dict, global_constants, stage_value_dict):
+	a = '9'
+	return task_dict
+
+
+def PR_CREATE_VENDOR_Submit_for_Approval(task_dict, global_constants, stage_value_dict):
+	a = '9'
+	return task_dict
