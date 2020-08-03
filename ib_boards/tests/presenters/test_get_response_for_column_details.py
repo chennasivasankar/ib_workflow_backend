1a9d1ed0ee2f51ab048c3e83b76caa340377ca7d
pavankumar <pavankumar.pamuru@ibhubs.com.in>
2020-08-03 20:00:30 +0530

    add test case for the duplicate fields

--- ib_boards/tests/presenters/test_get_response_for_column_details.py
+++ ib_boards/tests/presenters/test_get_response_for_column_details.py
@@ -32,6 +32,22 @@ class TestGetColumnDetails:
         )]
 
     @pytest.fixture()
+    def get_task_actions_dtos_with_duplicate_fields(self):
+        TaskActionsDTOFactory.reset_sequence()
+        return TaskActionsDTOFactory.create_batch(size=3) + [
+            TaskActionsDTOFactory(
+                task_id='task_id_0'
+            )]
+
+    @pytest.fixture()
+    def get_task_fields_dtos_with_duplicates_fields(self):
+        TaskFieldsDTOFactory.reset_sequence()
+        return TaskFieldsDTOFactory.create_batch(size=3) + [
+            TaskFieldsDTOFactory(
+                task_id='task_id_0'
+            )]
+
+    @pytest.fixture()
     def get_task_actions_dtos(self):
         TaskActionsDTOFactory.reset_sequence()
         return TaskActionsDTOFactory.create_batch(size=3)
@@ -58,6 +74,29 @@ class TestGetColumnDetails:
         # Arrange
         task_fields_dtos = get_task_fields_dtos_with_duplicates
         task_actions_dtos = get_task_actions_dtos_with_duplicates
+        task_details = get_column_task_details_with_duplicates
+        column_details = get_column_details
+        presenter = PresenterImplementation()
+
+        # Act
+        response = presenter.get_response_for_column_details(
+            column_details=column_details,
+            column_tasks=task_details, task_fields_dtos=task_fields_dtos,
+            task_actions_dtos=task_actions_dtos)
+
+        # Assert
+        import json
+        result = json.loads(response.content)
+
+        snapshot.assert_match(result, "column_details_with_duplicates")
+
+    def test_with_duplicate_tasks_in_same_column_and_duplicate_fields(
+            self, get_task_fields_dtos_with_duplicates_fields,
+            get_column_task_details_with_duplicates,
+            get_task_actions_dtos_with_duplicates_fields, get_column_details, snapshot):
+        # Arrange
+        task_fields_dtos = get_task_fields_dtos_with_duplicates_fields
+        task_actions_dtos = get_task_actions_dtos_with_duplicates_fields
         task_details = get_column_task_details_with_duplicates
         column_details = get_column_details
         presenter = PresenterImplementation()
