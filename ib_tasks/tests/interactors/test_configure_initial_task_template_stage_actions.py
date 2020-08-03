f499db6bff1b497d12d761786b3f2485b4a393cb
raviteja <raviteja.kotipalli@ibhubs.co.in>
2020-08-03 20:30:23 +0530

    add testcase to configure initial task template interactor

--- ib_tasks/tests/interactors/test_configure_initial_task_template_stage_actions.py
+++ ib_tasks/tests/interactors/test_configure_initial_task_template_stage_actions.py
@@ -18,6 +18,7 @@ class TestConfigureInitialTaskTemplateStageActions:
 
     @staticmethod
     def test_given_invalid_task_template_ids_raises_exception():
+
         # Arrange
         import pytest
         import json
@@ -49,6 +50,42 @@ class TestConfigureInitialTaskTemplateStageActions:
             .assert_called_once_with(task_template_ids=task_template_ids)
 
     @staticmethod
+    def test_given_more_than_one_stage_to_task_template_raises_exception():
+        # Arrange
+        import pytest
+        import json
+        expected_task_template_stages_dict = json.dumps(
+            {"task_template_1": ['stage_1', 'stage_3']}
+        )
+        TaskTemplateStageActionDTOFactory.reset_sequence(0)
+        tasks_dto = TaskTemplateStageActionDTOFactory.create_batch(size=2)
+        tasks_dto.append(TaskTemplateStageActionDTOFactory(
+            task_template_id='task_template_1')
+        )
+
+        task_template_ids = ["task_template_1", "task_template_2"]
+        storage = create_autospec(StorageInterface)
+        storage.get_valid_task_template_ids.return_value = task_template_ids
+
+        interactor = ConfigureInitialTaskTemplateStageActions(
+            storage=storage,
+            tasks_dto=tasks_dto
+        )
+        from ib_tasks.exceptions.task_custom_exceptions \
+            import ManyStagesToInitialTaskTemplate
+
+        # Act
+        with pytest.raises(ManyStagesToInitialTaskTemplate) as err:
+            assert interactor \
+                .create_update_delete_stage_actions_to_task_template()
+
+        # Assert
+        assert err.value.task_template_stages_dict == \
+               expected_task_template_stages_dict
+        storage.get_valid_task_template_ids \
+            .assert_called_once_with(task_template_ids=task_template_ids)
+
+    @staticmethod
     def test_given_valid_details_creates_stage_actions_to_task_template(mocker):
 
         # Arrange
@@ -68,8 +105,9 @@ class TestConfigureInitialTaskTemplateStageActions:
                 stage_id="stage_2"
             )
         ]
-
-        mock_obj = mocker.patch('ib_tasks.interactors.create_update_delete_stage_actions.CreateUpdateDeleteStageActionsInteractor.create_update_delete_stage_actions')
+        path = 'ib_tasks.interactors.create_update_delete_stage_actions.CreateUpdateDeleteStageActionsInteractor' \
+               '.create_update_delete_stage_actions'
+        mock_obj = mocker.patch(path)
 
         interactor = ConfigureInitialTaskTemplateStageActions(
             storage=storage,
