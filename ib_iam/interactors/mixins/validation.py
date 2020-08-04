f407451a0be5e32bd4c7cf2d4e9347c44a607a5f
Your Name <jaswanth.mamidipudi@ibhubs.co.in>
2020-08-04 14:53:56 +0530

    Modify some import statements

--- ib_iam/interactors/mixins/validation.py
+++ ib_iam/interactors/mixins/validation.py
@@ -65,6 +65,7 @@ class ValidationMixin:
                 for user_id, user_id_count in user_ids_with_count_dict.items()
                 if user_id_count > 1
             ]
+            from ib_iam.exceptions.custom_exceptions import DuplicateUserIds
             raise DuplicateUserIds(user_ids=duplicate_user_ids_list)
 
     def _validate_is_invalid_users_exists(self, user_ids: List[str]):
@@ -74,4 +75,5 @@ class ValidationMixin:
         is_invalid_users_found = len(user_ids) != len(user_ids_from_db)
         if is_invalid_users_found:
             invalid_user_ids_list = list(set(user_ids) - set(user_ids_from_db))
+            from ib_iam.exceptions.custom_exceptions import InvalidUserIds
             raise InvalidUserIds(user_ids=invalid_user_ids_list)
