c918f389dca0966c0d0cf50c16b77a7ab9911898
geetha.k <geetha.k@ibhubs.co.in>
2020-08-04 12:03:02 +0530

    add test cases for task in two stages

--- ib_tasks/storages/fields_storage_implementation.py
+++ ib_tasks/storages/fields_storage_implementation.py
@@ -36,7 +36,7 @@ class FieldsStorageImplementation(FieldsStorageInterface):
                             stage_ids: List[str]) -> \
             List[ActionDetailsDTO]:
         action_objs = StageAction.objects.filter(stage__stage_id__in=stage_ids)
-        print("StageAction",StageAction.objects.all().values())
+        print("StageAction", StageAction.objects.all().values())
         action_dtos = self._convert_action_objs_to_dtos(action_objs)
         return action_dtos
 
@@ -129,6 +129,7 @@ class FieldsStorageImplementation(FieldsStorageInterface):
                         stage_id=stage.stage_id,
                         field_ids=field_ids))
         return task_fields_dtos
+
     def validate_task_related_stage_ids(self,
                                         task_dtos: List[GetTaskDetailsDTO]) -> \
             List[GetTaskDetailsDTO]:
--- ib_tasks/tests/storages/test_get_stage_fields.py
+++ ib_tasks/tests/storages/test_get_stage_fields.py
@@ -17,12 +17,38 @@ class TestGetFieldIds:
     @pytest.fixture()
     def populate_data(self):
         StageModelFactory.reset_sequence()
-        StageModelFactory.create_batch(size=4)
+        StageModelFactory()
+        import json
+        StageModelFactory(card_info_kanban=json.dumps(['field_id_3', 'field_id_4']))
+        StageModelFactory(card_info_kanban=json.dumps(['field_id_5', 'field_id_6']))
         TaskFactory.reset_sequence()
         TaskFactory.create_batch(size=3)
         TaskTemplateFactory.reset_sequence()
         TaskTemplateFactory.create_batch(size=3)
         TaskStageModelFactory.reset_sequence()
+        TaskStageModelFactory.create_batch(size=4)
+
+    @pytest.fixture()
+    def get_task_template_stage_dtos_with_one_task_with_two_stages(self):
+        TemplateStagesDTOFactory.reset_sequence()
+        templates = TemplateStagesDTOFactory.create_batch(size=2, task_id=1, task_template_id="task_template_id_0")
+        templates.append(TemplateStagesDTOFactory(task_id=2))
+        templates.append(TemplateStagesDTOFactory(task_id=2))
+        return templates
+
+    @pytest.fixture()
+    def populate_data_for_one_task_in_two_stages(self):
+        StageModelFactory.reset_sequence()
+        StageModelFactory(task_template_id="task_template_id_0")
+        import json
+        StageModelFactory(card_info_kanban=json.dumps(['field_id_3', 'field_id_4']),
+                          task_template_id="task_template_id_0")
+        StageModelFactory(card_info_kanban=json.dumps(['field_id_5', 'field_id_6']))
+        StageModelFactory(card_info_kanban=json.dumps(['field_id_7', 'field_id_8']))
+        TaskFactory.reset_sequence()
+        TaskFactory.create_batch(size=3)
+        TaskTemplateFactory.reset_sequence()
+        TaskTemplateFactory.create_batch(size=3)
         TaskStageModelFactory.create_batch(size=4)
 
     def test_get_field_ids(self, get_task_template_stage_dtos,
@@ -33,6 +59,20 @@ class TestGetFieldIds:
 
         # Act
         response = storage.get_field_ids(get_task_template_stage_dtos)
+
+        # Assert
+        snapshot.assert_match(response, "response")
+
+    def test_get_field_ids_when_one_task_is_in_two_stages(
+            self, get_task_template_stage_dtos_with_one_task_with_two_stages,
+            populate_data_for_one_task_in_two_stages,
+            snapshot):
+        # Arrange
+        storage = FieldsStorageImplementation()
+
+        # Act
+        response = storage.get_field_ids(
+            get_task_template_stage_dtos_with_one_task_with_two_stages)
 
         # Assert
         snapshot.assert_match(response, "response")
