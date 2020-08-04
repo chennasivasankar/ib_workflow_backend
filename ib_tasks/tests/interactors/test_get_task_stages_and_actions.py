be977febfd723f7df93861f4a119c3d4d2a24462
geetha.k <geetha.k@ibhubs.co.in>
2020-08-04 16:51:02 +0530

    get user permitted actions in task stage and actions interactor

--- ib_tasks/tests/interactors/test_get_task_stages_and_actions.py
+++ ib_tasks/tests/interactors/test_get_task_stages_and_actions.py
@@ -17,11 +17,16 @@ class TestGetTaskStagesAndActions:
     @pytest.fixture()
     def get_stage_actions(self):
         ActionDetailsDTOFactory.reset_sequence()
-        return ActionDetailsDTOFactory.create_batch(size=4)
+        actions = ActionDetailsDTOFactory.create_batch(size=3, stage_id="stage_id_1")
+        actions.append(ActionDetailsDTOFactory(stage_id="stage_id_2"))
+        actions.append(ActionDetailsDTOFactory(stage_id="stage_id_2"))
+        actions.append(ActionDetailsDTOFactory(stage_id="stage_id_3"))
+        actions.append(ActionDetailsDTOFactory(stage_id="stage_id_3"))
+        return actions
 
     @pytest.fixture()
     def get_stage_details(self):
-        StageDetailsDTOFactory.reset_sequence()
+        StageDetailsDTOFactory.reset_sequence(1)
         return StageDetailsDTOFactory.create_batch(size=4)
 
     def test_when_user_has_permissions_get_stage_actions(
@@ -40,8 +45,35 @@ class TestGetTaskStagesAndActions:
         user_roles_mock.return_value = user_roles
         storage = create_autospec(FieldsStorageInterface)
         storage.get_task_stages.return_value = [
-            "stage_id_0", "stage_id_1", "stage_id_2"]
+            "stage_id_1", "stage_id_2", "stage_id_3"]
         storage.get_actions_details.return_value = get_stage_actions
+        storage.get_stage_complete_details.return_value = get_stage_details
+        interactor = GetTaskStagesAndActions(storage=storage)
+
+        # Act
+        response = interactor.get_task_stages_and_actions(task_id=task_id,
+                                                          user_id="user_id_1")
+
+        # Assert
+        snapshot.assert_match(response, "response")
+
+    def test_when_user_has_no_permissions_returns_empty_actions(
+            self, snapshot,
+            mocker,
+            get_stage_details):
+        # Arrange
+        task_id = 1
+        user_roles = ["FIN_PAYMENT_REQUESTER",
+                      "FIN_PAYMENT_POC",
+                      "FIN_PAYMENT_APPROVER",
+                      "FIN_PAYMENTS_RP",
+                      "FIN_FINANCE_RP"]
+        user_roles_mock = get_user_role_ids(mocker)
+        user_roles_mock.return_value = user_roles
+        storage = create_autospec(FieldsStorageInterface)
+        storage.get_task_stages.return_value = [
+            "stage_id_1", "stage_id_2", "stage_id_3"]
+        storage.get_actions_details.return_value = []
         storage.get_stage_complete_details.return_value = get_stage_details
         interactor = GetTaskStagesAndActions(storage=storage)
 
