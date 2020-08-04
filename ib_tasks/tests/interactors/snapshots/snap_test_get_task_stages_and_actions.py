2bb866e42ba70dd2b942d90b822a62851d23b0aa
geetha.k <geetha.k@ibhubs.co.in>
2020-08-04 12:48:27 +0530

    add test case when a task is in one stage

--- ib_tasks/tests/interactors/snapshots/snap_test_get_task_stages_and_actions.py
+++ ib_tasks/tests/interactors/snapshots/snap_test_get_task_stages_and_actions.py
@@ -7,14 +7,19 @@ from snapshottest import GenericRepr, Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestGetTaskStagesAndActions.test_given_valid_details_get_details response'] = [
+
+snapshots['TestGetTaskStagesAndActions.test_given_task_id_returns_task_related_stages_and_their_actions response'] = [
     GenericRepr("StageAndActionsDetailsDTO(stage_id='stage_id_0', name='name_0', actions_dtos=[ActionDetailsDTO(action_id=1, name='name_1', stage_id='stage_id_0', button_text='button_text_1', button_color=None), ActionDetailsDTO(action_id=2, name='name_2', stage_id='stage_id_0', button_text='button_text_2', button_color=None)])"),
     GenericRepr("StageAndActionsDetailsDTO(stage_id='stage_id_1', name='name_1', actions_dtos=[ActionDetailsDTO(action_id=3, name='name_3', stage_id='stage_id_1', button_text='button_text_3', button_color=None)])"),
     GenericRepr("StageAndActionsDetailsDTO(stage_id='stage_id_2', name='name_2', actions_dtos=[ActionDetailsDTO(action_id=4, name='name_4', stage_id='stage_id_2', button_text='button_text_4', button_color=None)])")
 ]
 
-snapshots['TestGetTaskStagesAndActions.test_given_valid_details_but_task_has_no_actions_returns_actions_as_empty response'] = [
+snapshots['TestGetTaskStagesAndActions.test_given_task_id_but_task_has_no_actions_returns_actions_as_empty_list response'] = [
     GenericRepr("StageAndActionsDetailsDTO(stage_id='stage_id_0', name='name_0', actions_dtos=[])"),
     GenericRepr("StageAndActionsDetailsDTO(stage_id='stage_id_1', name='name_1', actions_dtos=[])"),
     GenericRepr("StageAndActionsDetailsDTO(stage_id='stage_id_2', name='name_2', actions_dtos=[])")
+]
+
+snapshots['TestGetTaskStagesAndActions.test_given_task_id_returns_task_related_stages_and_their_actions_when_a_task_has_only_one_stage response'] = [
+    GenericRepr("StageAndActionsDetailsDTO(stage_id='stage_id_0', name='name_0', actions_dtos=[ActionDetailsDTO(action_id=1, name='name_1', stage_id='stage_id_0', button_text='button_text_1', button_color=None), ActionDetailsDTO(action_id=2, name='name_2', stage_id='stage_id_0', button_text='button_text_2', button_color=None)])")
 ]
--- ib_tasks/tests/interactors/test_get_task_stages_and_actions.py
+++ ib_tasks/tests/interactors/test_get_task_stages_and_actions.py
@@ -3,9 +3,12 @@ from unittest.mock import create_autospec
 import pytest
 
 from ib_tasks.exceptions.task_custom_exceptions import InvalidTaskIdException
-from ib_tasks.interactors.get_task_stages_and_actions import GetTaskStagesAndActions
-from ib_tasks.interactors.storage_interfaces.fields_storage_interface import FieldsStorageInterface
-from ib_tasks.tests.factories.storage_dtos import ActionDetailsDTOFactory, StageDetailsDTOFactory
+from ib_tasks.interactors.get_task_stages_and_actions import \
+    GetTaskStagesAndActions
+from ib_tasks.interactors.storage_interfaces.fields_storage_interface import \
+    FieldsStorageInterface
+from ib_tasks.tests.factories.storage_dtos import ActionDetailsDTOFactory, \
+    StageDetailsDTOFactory
 
 
 class TestGetTaskStagesAndActions:
@@ -23,9 +26,21 @@ class TestGetTaskStagesAndActions:
         StageDetailsDTOFactory.reset_sequence()
         return StageDetailsDTOFactory.create_batch(size=3)
 
-    def test_given_valid_details_get_details(self, snapshot,
-                                             get_stage_actions,
-                                             get_stage_details):
+    @pytest.fixture()
+    def get_stage_actions_for_one_stage(self):
+        ActionDetailsDTOFactory.reset_sequence()
+        actions = ActionDetailsDTOFactory.create_batch(size=2, stage_id="stage_id_0")
+        return actions
+
+    @pytest.fixture()
+    def get_stage_details_for_one_stage(self):
+        StageDetailsDTOFactory.reset_sequence()
+        return [StageDetailsDTOFactory()]
+
+    def test_given_task_id_returns_task_related_stages_and_their_actions(
+            self, snapshot,
+            get_stage_actions,
+            get_stage_details):
         # Arrange
         task_id = 1
         storage = create_autospec(FieldsStorageInterface)
@@ -36,12 +51,35 @@ class TestGetTaskStagesAndActions:
         interactor = GetTaskStagesAndActions(storage=storage)
 
         # Act
-        response = interactor.get_task_stages_and_actions(task_id=task_id, user_id=1)
+        response = interactor.get_task_stages_and_actions(
+            task_id=task_id,
+            user_id="123e4567-e89b-12d3-a456-426614174000")
 
         # Assert
         snapshot.assert_match(response, "response")
 
-    def test_given_valid_details_but_task_has_no_actions_returns_actions_as_empty_list(
+    def test_given_task_id_returns_task_related_stages_and_their_actions_when_a_task_has_only_one_stage(
+            self, snapshot,
+            get_stage_actions_for_one_stage,
+            get_stage_details_for_one_stage):
+        # Arrange
+        task_id = 1
+        storage = create_autospec(FieldsStorageInterface)
+        storage.validate_task_id.return_value = True
+        storage.get_task_stages.return_value = ["stage_id_0"]
+        storage.get_actions_details.return_value = get_stage_actions_for_one_stage
+        storage.get_stage_complete_details.return_value = get_stage_details_for_one_stage
+        interactor = GetTaskStagesAndActions(storage=storage)
+
+        # Act
+        response = interactor.get_task_stages_and_actions(
+            task_id=task_id,
+            user_id="123e4567-e89b-12d3-a456-426614174000")
+
+        # Assert
+        snapshot.assert_match(response, "response")
+
+    def test_given_task_id_but_task_has_no_actions_returns_actions_as_empty_list(
             self, snapshot, get_stage_details):
         # Arrange
         task_id = 1
@@ -53,7 +91,9 @@ class TestGetTaskStagesAndActions:
         interactor = GetTaskStagesAndActions(storage=storage)
 
         # Act
-        response = interactor.get_task_stages_and_actions(task_id=task_id, user_id=1)
+        response = interactor.get_task_stages_and_actions(
+            task_id=task_id,
+            user_id="123e4567-e89b-12d3-a456-426614174000")
 
         # Assert
         snapshot.assert_match(response, "response")
@@ -67,7 +107,8 @@ class TestGetTaskStagesAndActions:
 
         # Act
         with pytest.raises(InvalidTaskIdException) as error:
-            interactor.get_task_stages_and_actions(task_id=task_id, user_id=1)
+            interactor.get_task_stages_and_actions(
+                task_id=task_id, user_id="123e4567-e89b-12d3-a456-426614174000")
 
         # Assert
         storage.validate_task_id.assert_called_once_with(task_id)
