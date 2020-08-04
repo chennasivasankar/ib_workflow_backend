49aa9b4d6caa2dae523e0c152082b04892a66043
geetha.k <geetha.k@ibhubs.co.in>
2020-08-04 12:51:54 +0530

    add test case when a task is in one stage

--- ib_tasks/tests/interactors/snapshots/snap_test_get_task_stages_and_actions.py
+++ ib_tasks/tests/interactors/snapshots/snap_test_get_task_stages_and_actions.py
@@ -7,7 +7,6 @@ from snapshottest import GenericRepr, Snapshot
 
 snapshots = Snapshot()
 
-
 snapshots['TestGetTaskStagesAndActions.test_given_task_id_returns_task_related_stages_and_their_actions response'] = [
     GenericRepr("StageAndActionsDetailsDTO(stage_id='stage_id_0', name='name_0', actions_dtos=[ActionDetailsDTO(action_id=1, name='name_1', stage_id='stage_id_0', button_text='button_text_1', button_color=None), ActionDetailsDTO(action_id=2, name='name_2', stage_id='stage_id_0', button_text='button_text_2', button_color=None)])"),
     GenericRepr("StageAndActionsDetailsDTO(stage_id='stage_id_1', name='name_1', actions_dtos=[ActionDetailsDTO(action_id=3, name='name_3', stage_id='stage_id_1', button_text='button_text_3', button_color=None)])"),
@@ -20,6 +19,10 @@ snapshots['TestGetTaskStagesAndActions.test_given_task_id_but_task_has_no_action
     GenericRepr("StageAndActionsDetailsDTO(stage_id='stage_id_2', name='name_2', actions_dtos=[])")
 ]
 
-snapshots['TestGetTaskStagesAndActions.test_given_task_id_returns_task_related_stages_and_their_actions_when_a_task_has_only_one_stage response'] = [
+snapshots['TestGetTaskStagesAndActions.test_given_task_id_with_one_stage_returns_stage_and_their_actions response'] = [
     GenericRepr("StageAndActionsDetailsDTO(stage_id='stage_id_0', name='name_0', actions_dtos=[ActionDetailsDTO(action_id=1, name='name_1', stage_id='stage_id_0', button_text='button_text_1', button_color=None), ActionDetailsDTO(action_id=2, name='name_2', stage_id='stage_id_0', button_text='button_text_2', button_color=None)])")
+]
+
+snapshots['TestGetTaskStagesAndActions.test_given_task_id_with_one_stage_without_no_actions_returns_actions_as_empty_list response'] = [
+    GenericRepr("StageAndActionsDetailsDTO(stage_id='stage_id_0', name='name_0', actions_dtos=[])")
 ]
