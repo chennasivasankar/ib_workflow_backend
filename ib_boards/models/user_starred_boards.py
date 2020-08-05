ba315468cbd2f3232b60704591f2f523241cf167
geetha.k <geetha.k@ibhubs.co.in>
2020-08-05 20:25:26 +0530

    add unique constraint to the model

--- ib_boards/models/user_starred_boards.py
+++ ib_boards/models/user_starred_boards.py
@@ -6,3 +6,6 @@ from ib_boards.models import Board
 class UserStarredBoard(models.Model):
     user_id = models.CharField(max_length=100)
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user_id', 'board')
