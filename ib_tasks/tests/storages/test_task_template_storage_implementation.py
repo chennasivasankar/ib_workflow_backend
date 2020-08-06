a7f4ac2866b6f11f1a6e6476f00ef9e7c6185264
rajesh.ganjikunta <rajesh.ganjikunta@ibhubs.co.in>
2020-08-06 15:52:47 +0530

    add population script for transition templates

--- ib_tasks/tests/storages/test_task_template_storage_implementation.py
+++ ib_tasks/tests/storages/test_task_template_storage_implementation.py
@@ -213,53 +213,48 @@ class TestTaskTemplateStorageImplementation:
         # Assert
         assert is_template_exists is True
 
-    def test_get_task_template_name(self, storage):
-        # Arrange
-        task_template = TaskTemplateFactory()
-        template_id = task_template.template_id
-        expected_template_name = task_template.name
-
-        # Act
-        template_name = \
-            storage.get_task_template_name(template_id=template_id)
-
-        # Assert
-        assert template_name == expected_template_name
-
-    def test_create_task_template(self, storage):
+    def test_create_template(self, storage):
         # Arrange
         template_id = "FIN_VENDOR"
         template_name = "Task Template 1"
+        is_transition_template = True
 
         # Act
-        storage.create_task_template(
-            template_id=template_id, template_name=template_name
+        storage.create_template(
+            template_id=template_id, template_name=template_name,
+            is_transition_template=is_transition_template
         )
 
         # Assert
         from ib_tasks.models.task_template import TaskTemplate
-        task_template = TaskTemplate.objects.get(template_id=template_id)
+        template = TaskTemplate.objects.get(template_id=template_id)
 
-        assert task_template.template_id == template_id
-        assert task_template.name == template_name
+        assert template.template_id == template_id
+        assert template.name == template_name
+        assert template.is_transition_template == is_transition_template
 
-    def test_update_task_template(self, storage):
+    def test_update_template(self, storage):
         # Arrange
         template_id = "FIN_VENDOR"
         template_name = "iB Template"
-        TaskTemplateFactory(template_id=template_id, name=template_name)
+        is_transition_template = True
+        TaskTemplateFactory(
+            template_id=template_id, name=template_name
+        )
 
         # Act
-        storage.update_task_template(
-            template_id=template_id, template_name=template_name
+        storage.update_template(
+            template_id=template_id, template_name=template_name,
+            is_transition_template=is_transition_template
         )
 
         # Assert
         from ib_tasks.models.task_template import TaskTemplate
-        task_template = TaskTemplate.objects.get(template_id=template_id)
+        template = TaskTemplate.objects.get(template_id=template_id)
 
-        assert task_template.template_id == template_id
-        assert task_template.name == template_name
+        assert template.template_id == template_id
+        assert template.name == template_name
+        assert template.is_transition_template == is_transition_template
 
     def test_get_existing_gof_ids_of_template(self, storage):
         # Arrange
---
+++ ib_tasks/tests/views/get_task_templates_fields_details/snapshots/snap_test_case_01.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['TestCase01GetTaskTemplatesFieldsDetailsAPITestCase.test_case status_code'] = '200'
+
+snapshots['TestCase01GetTaskTemplatesFieldsDetailsAPITestCase.test_case body'] = {
+    'operators': [
+        'GTE'
+    ],
+    'task_template_fields_details': [
+        {
+            'fields': [
+                {
+                    'field_id': 'string',
+                    'name': 'string'
+                }
+            ],
+            'name': 'string',
+            'task_template_id': 'string'
+        }
+    ]
+}
