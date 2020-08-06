b85e6dc3ad3ea81fecab2e92da6e0690a41b27ed
Your Name <jaswanth.mamidipudi@ibhubs.co.in>
2020-08-06 13:00:50 +0530

    Add Dtos for Entity and Checklist Item

--- ib_utility_tools/interactors/dtos/dtos.py
+++ ib_utility_tools/interactors/dtos/dtos.py
@@ -4,8 +4,19 @@ from ib_utility_tools.constants.enum import EntityType
 
 
 @dataclass
-class ChecklistItemWithEntityDetailsDTO:
-    user_id: str
+class EntityDetailsDTO:
     entity_id: str
     entity_type: EntityType
+
+
+@dataclass
+class ChecklistItemWithEntityDetailsDTO(EntityDetailsDTO):
     text: str
+    is_checked: bool
+
+
+@dataclass
+class ChecklistItemWithItemIdDTO:
+    checklist_item_id: str
+    text: str
+    is_checked: bool
