ba4a1f28520bd1f3b0643273437ff1e4669be55b
prathyushakl <jonathan@notmyemail.com>
2020-08-06 18:45:56 +0530

    added stage assignee in task stage model

--- ib_tasks/models/task_stage.py
+++ ib_tasks/models/task_stage.py
@@ -7,7 +7,7 @@ from ib_tasks.models.task import Task
 class TaskStage(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
-    stage_assignee = models.CharField(max_length=50)
+    stage_assignee = models.CharField(max_length=50, null=True)
 
     class Meta:
         unique_together = ('stage', 'task')
