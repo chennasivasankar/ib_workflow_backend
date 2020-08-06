085c9a3715c36254e3ea8b5a7067dc4974fd64bb
geetha.k <geetha.k@ibhubs.co.in>
2020-08-06 13:47:41 +0530

    controller get board details

--- ib_boards/tests/views/get_boards_details/snapshots/snap_test_case_01.py
+++ ib_boards/tests/views/get_boards_details/snapshots/snap_test_case_01.py
@@ -10,21 +10,21 @@ snapshots = Snapshot()
 snapshots['TestCase01GetBoardsDetailsAPITestCase.test_case status_code'] = '200'
 
 snapshots['TestCase01GetBoardsDetailsAPITestCase.test_case body'] = {
-    'boards_details': [
+    'all_boards': [
+        {
+            'board_id': 'BOARD_ID_1',
+            'name': 'BOARD_DISPLAY_NAME'
+        },
+        {
+            'board_id': 'BOARD_ID_10',
+            'name': 'BOARD_DISPLAY_NAME'
+        },
         {
             'board_id': 'BOARD_ID_11',
             'name': 'BOARD_DISPLAY_NAME'
         },
         {
-            'board_id': 'BOARD_ID_2',
-            'name': 'BOARD_DISPLAY_NAME'
-        },
-        {
             'board_id': 'BOARD_ID_3',
-            'name': 'BOARD_DISPLAY_NAME'
-        },
-        {
-            'board_id': 'BOARD_ID_4',
             'name': 'BOARD_DISPLAY_NAME'
         },
         {
@@ -33,6 +33,24 @@ snapshots['TestCase01GetBoardsDetailsAPITestCase.test_case body'] = {
         },
         {
             'board_id': 'BOARD_ID_6',
+            'name': 'BOARD_DISPLAY_NAME'
+        },
+        {
+            'board_id': 'BOARD_ID_7',
+            'name': 'BOARD_DISPLAY_NAME'
+        },
+        {
+            'board_id': 'BOARD_ID_9',
+            'name': 'BOARD_DISPLAY_NAME'
+        }
+    ],
+    'starred_boards': [
+        {
+            'board_id': 'BOARD_ID_2',
+            'name': 'BOARD_DISPLAY_NAME'
+        },
+        {
+            'board_id': 'BOARD_ID_4',
             'name': 'BOARD_DISPLAY_NAME'
         }
     ],
