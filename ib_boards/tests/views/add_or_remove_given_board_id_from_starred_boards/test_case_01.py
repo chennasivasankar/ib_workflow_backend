c416a6163810e7b5ca70c1b1d809aea214971f4d
Vedavidh Budimuri <vedavidhbudimuri@gmail.com>
2020-08-06 17:51:34 +0530

    updated test cases

--- ib_boards/tests/views/add_or_remove_given_board_id_from_starred_boards/test_case_01.py
+++ ib_boards/tests/views/add_or_remove_given_board_id_from_starred_boards/test_case_01.py
@@ -2,7 +2,7 @@
 # given action UNSTAR removes board from starred board
 """
 import pytest
-from django_swagger_utils.utils.test_v1 import TestUtils
+from django_swagger_utils.utils.test_utils import TestUtils
 
 from ib_boards.models import UserStarredBoard
 from . import APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX
--- ib_iam/tests/views/delete_company/test_case_02.py
+++ ib_iam/tests/views/delete_company/test_case_02.py
@@ -2,7 +2,7 @@
 UserHasNoAccess exception Response will be given as user is not admin
 """
 import pytest
-from django_swagger_utils.utils.test_v1 import TestUtils
+from django_swagger_utils.utils.test_utils import TestUtils
 from . import APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX
 
 
@@ -19,7 +19,7 @@ class TestCase02DeleteCompanyAPITestCase(TestUtils):
         path_params = {"company_id": "413642ff-1272-4990-b878-6607a5e02bc1"}
         query_params = {}
         headers = {}
-        response = self.default_test_case(
+        response = self.make_api_call(
             body=body, path_params=path_params,
             query_params=query_params, headers=headers, snapshot=snapshot
         )
