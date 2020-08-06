e992d6a9ad0c0e4656f1cbbbfed70dd578aedf33
geetha.k <geetha.k@ibhubs.co.in>
2020-08-06 12:01:15 +0530

    controller

--- ib_boards/views/add_or_remove_given_board_id_from_starred_boards/api_wrapper.py
+++ ib_boards/views/add_or_remove_given_board_id_from_starred_boards/api_wrapper.py
@@ -1,10 +1,24 @@
 from django_swagger_utils.drf_server.utils.decorator.interface_decorator \
     import validate_decorator
 from .validator_class import ValidatorClass
+from ...interactors.dtos import StarOrUnstarParametersDTO
+from ...interactors.star_or_unstar_given_board_interactor import StarOrUnstarBoardInteractor
+from ...presenters.presenter_implementation import PresenterImplementation
+from ...storages.storage_implementation import StorageImplementation
 
 
 @validate_decorator(validator_class=ValidatorClass)
 def api_wrapper(*args, **kwargs):
     user = kwargs["user"]
     board_id = kwargs["board_id"]
-    action = kwargs['']
+    request_params = kwargs['request_query_params']
+    action = request_params['action']
+
+    parameters = StarOrUnstarParametersDTO(
+        board_id=board_id,
+        user_id=user.id,
+        action=action
+    )
+    storage = StorageImplementation()
+    presenter = PresenterImplementation()
+    interactor = StarOrUnstarBoardInteractor(storage=storage)
