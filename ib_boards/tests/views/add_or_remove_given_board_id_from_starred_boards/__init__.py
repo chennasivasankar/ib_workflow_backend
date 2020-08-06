e599a11edee7291d38a193a79413bd40673b5e6d
geetha.k <geetha.k@ibhubs.co.in>
2020-08-06 11:45:18 +0530

    controllers

---
+++ ib_boards/tests/views/add_or_remove_given_board_id_from_starred_boards/__init__.py
@@ -0,0 +1,12 @@
+# pylint: disable=wrong-import-position
+
+APP_NAME = "ib_boards"
+OPERATION_NAME = "add_or_remove_given_board_id_from_starred_boards"
+REQUEST_METHOD = "post"
+URL_SUFFIX = "boards/{board_id}/star/v1/"
+
+from .test_case_01 import TestCase01AddOrRemoveGivenBoardIdFromStarredBoardsAPITestCase
+
+__all__ = [
+    "TestCase01AddOrRemoveGivenBoardIdFromStarredBoardsAPITestCase"
+]
