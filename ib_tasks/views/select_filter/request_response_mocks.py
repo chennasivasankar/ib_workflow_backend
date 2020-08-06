d3a1199c39f47852bf9ee0b9dd9fd07020662734
rajesh.ganjikunta <rajesh.ganjikunta@ibhubs.co.in>
2020-08-06 14:11:40 +0530

    add spec for get transition template

---
+++ ib_tasks/views/select_filter/request_response_mocks.py
@@ -0,0 +1,33 @@
+
+
+REQUEST_BODY_JSON = """
+{
+    "filter_id": 1,
+    "is_selected": true
+}
+"""
+
+
+RESPONSE_200_JSON = """
+{
+    "filter_id": 1,
+    "action": "ENABLED"
+}
+"""
+
+RESPONSE_403_JSON = """
+{
+    "response": "string",
+    "http_status_code": 1,
+    "res_status": "INVALID_FILTER_ID"
+}
+"""
+
+RESPONSE_404_JSON = """
+{
+    "response": "string",
+    "http_status_code": 1,
+    "res_status": "USER_DO_NOT_HAVE_PERMISSION_TO_FILTER"
+}
+"""
+
