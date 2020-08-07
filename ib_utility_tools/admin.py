bf38e011f042e6578ea6d622025e522edeb2ff16
Your Name <jaswanth.mamidipudi@ibhubs.co.in>
2020-08-07 12:40:16 +0530

    Add models into admin.py in order to see data in admin panel

--- ib_utility_tools/admin.py
+++ ib_utility_tools/admin.py
@@ -1,1 +1,7 @@
 # your django admin
+from django.contrib import admin
+
+from ib_utility_tools.models import Checklist, ChecklistItem
+
+admin.site.register(Checklist)
+admin.site.register(ChecklistItem)
