4d2a1817729f7e5ed185793576c4edcdbca93c90
raviteja <raviteja.kotipalli@ibhubs.co.in>
2020-08-07 21:12:01 +0530

    add elastic search task create and update in create and update task implementation

--- ib_tasks/interactors/create_or_update_task/update_task_interactor.py
+++ ib_tasks/interactors/create_or_update_task/update_task_interactor.py
@@ -1,6 +1,7 @@
 from typing import Optional, List, Union
 
 from ib_tasks.constants.config import TIME_FORMAT
+from ib_tasks.documents.elastic_task import ElasticFieldDTO, ElasticTaskDTO
 from ib_tasks.exceptions.datetime_custom_exceptions import \
     DueTimeHasExpiredForToday, InvalidDueTimeFormat, \
     StartDateIsAheadOfDueDate, \
@@ -31,6 +32,7 @@ from ib_tasks.interactors.presenter_interfaces.update_task_presenter import \
 from ib_tasks.interactors.storage_interfaces. \
     create_or_update_task_storage_interface import \
     CreateOrUpdateTaskStorageInterface
+from ib_tasks.interactors.storage_interfaces.elastic_storage_interface import ElasticSearchStorageInterface
 from ib_tasks.interactors.storage_interfaces.fields_storage_interface import \
     FieldsStorageInterface
 from ib_tasks.interactors.storage_interfaces.get_task_dtos import \
@@ -45,7 +47,7 @@ from ib_tasks.interactors.storage_interfaces.task_dtos import \
     TaskGoFWithTaskIdDTO, TaskGoFDetailsDTO
 from ib_tasks.interactors.storage_interfaces.task_storage_interface import \
     TaskStorageInterface
-from ib_tasks.interactors.task_dtos import UpdateTaskDTO, CreateTaskDTO
+from ib_tasks.interactors.task_dtos import UpdateTaskDTO, CreateTaskDTO, FieldValuesDTO
 
 
 class UpdateTaskInteractor:
@@ -55,7 +57,8 @@ class UpdateTaskInteractor:
             gof_storage: GoFStorageInterface,
             create_task_storage: CreateOrUpdateTaskStorageInterface,
             storage: StorageInterface, field_storage: FieldsStorageInterface,
-            stage_storage: StageStorageInterface
+            stage_storage: StageStorageInterface,
+            elastic_storage: ElasticSearchStorageInterface
     ):
         self.gof_storage = gof_storage
         self.task_storage = task_storage
@@ -63,6 +66,7 @@ class UpdateTaskInteractor:
         self.storage = storage
         self.field_storage = field_storage
         self.stage_storage = stage_storage
+        self.elastic_storage = elastic_storage
 
     def update_task_wrapper(
             self, presenter: UpdateTaskPresenterInterface,
@@ -162,6 +166,8 @@ class UpdateTaskInteractor:
             task_template_id, action_type=None)
         self.create_task_storage.update_task_with_given_task_details(
             task_dto=task_dto)
+        elastic_dto = self._get_elastic_task_dto(task_dto)
+        self.elastic_storage.update_task(task_dto=elastic_dto)
         existing_gofs = \
             self.create_task_storage \
                 .get_gof_ids_with_same_gof_order_related_to_a_task(task_id)
@@ -193,6 +199,36 @@ class UpdateTaskInteractor:
                 task_gof_dtos_for_updation, task_dto, existing_fields)
         if task_gof_dtos_for_creation:
             self._create_task_gofs(task_gof_dtos_for_creation, task_dto)
+
+    def _get_elastic_task_dto(self, task_dto: UpdateTaskDTO):
+
+        fields_dto = self._get_fields_dto(task_dto)
+        elastic_task_dto = ElasticTaskDTO(
+            template_id=None,
+            task_id=task_dto.task_id,
+            title=task_dto.title,
+            fields=fields_dto
+        )
+        return elastic_task_dto
+
+    def _get_fields_dto(
+            self, task_dto: UpdateTaskDTO) -> List[ElasticFieldDTO]:
+
+        fields_dto = []
+        gof_fields_dtos = task_dto.gof_fields_dtos
+        for gof_fields_dto in gof_fields_dtos:
+            for field_value_dto in gof_fields_dto.field_values_dtos:
+                fields_dto.append(self._get_elastic_field_dto(field_value_dto))
+
+        return fields_dto
+
+    @staticmethod
+    def _get_elastic_field_dto(field_dto: FieldValuesDTO) -> ElasticFieldDTO:
+        return ElasticFieldDTO(
+            field_id=field_dto.field_id,
+            value=field_dto.field_response
+        )
+
 
     def _validate_task_id(
             self, task_id: int) -> Optional[InvalidTaskException]:
--- ib_tasks/storages/elasticsearch_storage_implementation.py
+++ ib_tasks/storages/elasticsearch_storage_implementation.py
@@ -4,16 +4,52 @@ Author: Pavankumar Pamuru
 
 """
 
+from ib_tasks.documents.elastic_task import ElasticTaskDTO, Task, QueryTasksDTO
+from ib_tasks.interactors.storage_interfaces.elastic_storage_interface import ElasticSearchStorageInterface
 
-class ElasticSearchStorageImplementation:
-    def create_task(self):
-        pass
 
-    def update_task(self):
+class ElasticSearchStorageImplementation(ElasticSearchStorageInterface):
+
+    def create_task(self, elastic_task_dto: ElasticTaskDTO) -> str:
+
+        task_obj = Task(
+            template_id=elastic_task_dto.template_id,
+            task_id=elastic_task_dto.task_id,
+            title=elastic_task_dto.title
+        )
+        field_dtos = elastic_task_dto.fields
+        task_obj.add_fields(field_dtos=field_dtos)
+        task_obj.save()
+        elastic_task_id = task_obj.meta.id
+        return elastic_task_id
+
+    def update_task(self, task_dto: ElasticTaskDTO):
         pass
 
     def filter_tasks(self):
         pass
 
-    def query_tasks(self):
-        pass
+    def query_tasks(
+            self, offset: int, limit: int, search_query: str
+    ) -> QueryTasksDTO:
+        from elasticsearch_dsl import Q, Search
+
+        search = Search(index='task')
+        search = search.query(
+            Q(
+                "match",
+                title={
+                    "query": search_query,
+                    "fuzziness": "2"
+                }
+            )
+        )
+        total_tasks_count = search.count()
+        task_ids = [
+            hit.task_id
+            for hit in search[offset: offset+limit]
+        ]
+        return QueryTasksDTO(
+            total_tasks_count=total_tasks_count,
+            task_ids=task_ids
+        )
