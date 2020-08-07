acb744dcc8580550ea2ef593e9be5ce80384d615
Your Name <jaswanth.mamidipudi@ibhubs.co.in>
2020-08-07 23:16:48 +0530

    Add get timer controller

---
+++ ib_utility_tools/views/get_timer/request_response_mocks.py
@@ -0,0 +1,17 @@
+
+
+REQUEST_BODY_JSON = """
+{
+    "entity_id": "9acfeb4d-88fc-4c64-8f4b-83aa4134c72a",
+    "entity_type": "STAGE_TASK"
+}
+"""
+
+
+RESPONSE_200_JSON = """
+{
+    "duration_in_seconds": 1,
+    "is_running": true
+}
+"""
+
