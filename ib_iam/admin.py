9267457a915a8c20e08bb00bcf866785fb007150
DurgaPrime <durgaprasad@ibhubs.co>
2020-08-07 21:48:44 +0530

    added admin panels

--- ib_iam/admin.py
+++ ib_iam/admin.py
@@ -6,6 +6,19 @@ from ib_iam.models import UserDetails, UserTeam, UserRole, Company, Role, Team
 admin.site.register(UserDetails)
 admin.site.register(UserTeam)
 admin.site.register(UserRole)
-admin.site.register(Company)
+# admin.site.register(Company)
 admin.site.register(Role)
-admin.site.register(Team)
+# admin.site.register(Team)
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("company_id", "name", "description", "logo_url")
+    search_fields = ["name"]
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ("team_id", "name", "description")
+
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Team, TeamAdmin)
