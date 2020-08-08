ec98a3abf0b606a115d5b7c02ca29ca6b82feeee
geetha.k <geetha.k@ibhubs.co.in>
2020-08-08 09:22:50 +0530

    changes in specs of boards

--- ib_boards/tests/views/get_columns_details/__init__.py
+++ ib_boards/tests/views/get_columns_details/__init__.py
@@ -2,7 +2,7 @@
 
 APP_NAME = "ib_boards"
 OPERATION_NAME = "get_columns_details"
-REQUEST_METHOD = "get"
+REQUEST_METHOD = "post"
 URL_SUFFIX = "boards/{board_id}/columns/v1/"
 
 from .test_case_01 import TestCase01GetColumnsDetailsAPITestCase
