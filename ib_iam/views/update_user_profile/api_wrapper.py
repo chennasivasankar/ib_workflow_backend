03af6772821700c527624685b8eb1ca095ce196e
Your Name <jaswanth.mamidipudi@ibhubs.co.in>
2020-08-08 16:23:36 +0530

    Add controller for update user profile

---
+++ ib_iam/views/update_user_profile/api_wrapper.py
@@ -0,0 +1,44 @@
+from django_swagger_utils.drf_server.utils.decorator.interface_decorator \
+    import validate_decorator
+
+from .validator_class import ValidatorClass
+
+
+@validate_decorator(validator_class=ValidatorClass)
+def api_wrapper(*args, **kwargs):
+    user_object = kwargs["user"]
+    request_data = kwargs["request_data"]
+    user_id_name_email_and_profile_pic_url_dto = \
+        _prepare_user_id_name_email_and_profile_pic_url_dto(
+            request_data=request_data, user_object=user_object)
+
+    from ib_iam.storages.user_storage_implementation import \
+        UserStorageImplementation
+    user_storage = UserStorageImplementation()
+    from ib_iam.presenters \
+        .update_user_profile_presenter_implementation import \
+        UpdateUserProfilePresenterImplementation
+    presenter = UpdateUserProfilePresenterImplementation()
+    from ib_iam.interactors.update_user_profile_interactor import \
+        UpdateUserProfileInteractor
+    interactor = UpdateUserProfileInteractor(user_storage=user_storage)
+
+    response_data = interactor.update_user_profile_wrapper(
+        user_id_name_email_and_profile_pic_url_dto=
+        user_id_name_email_and_profile_pic_url_dto,
+        presenter=presenter)
+    return response_data
+
+
+def _prepare_user_id_name_email_and_profile_pic_url_dto(request_data,
+                                                        user_object):
+    from ib_iam.interactors.storage_interfaces.dtos import \
+        UserIdNameEmailAndProfilePicUrlDTO
+    user_id_name_email_and_profile_pic_url_dto = \
+        UserIdNameEmailAndProfilePicUrlDTO(
+            user_id=user_object.user_id,
+            name=request_data["name"],
+            email=request_data["email"],
+            profile_pic_url=request_data["profile_pic_url"]
+        )
+    return user_id_name_email_and_profile_pic_url_dto
