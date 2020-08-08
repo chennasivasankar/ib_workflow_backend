10b3a4534439c31ac9b90023c0a475d0bb6d6014
Mukunda <mukunda.pedda@ibhubs.co.in>
2020-08-08 12:25:46 +0530

    adds fixes of dsu settings

---
+++ ib_tasks/tests/views/get_reasons_for_missing_task_due_date_time/__init__.py
@@ -0,0 +1,6 @@
+# pylint: disable=wrong-import-position
+
+APP_NAME = "ib_tasks"
+OPERATION_NAME = "get_reasons_for_missing_task_due_date_time"
+REQUEST_METHOD = "get"
+URL_SUFFIX = "task/{task_id}/reasons/v1/"
