d1b20443b9e46d4cef20dcc3bbd08b6ca7fd0857
prathyushakl <jonathan@notmyemail.com>
2020-08-08 15:39:20 +0530

    added test case for integration test

--- ib_tasks/tests/views/get_next_stages_random_assignees_of_a_task/test_case_01.py
+++ ib_tasks/tests/views/get_next_stages_random_assignees_of_a_task/test_case_01.py
@@ -1,9 +1,17 @@
 """
 # TODO: Update test case description
 """
+import json
+
+import factory
 import pytest
-from django_swagger_utils.utils.test_v1 import TestUtils
+from django_swagger_utils.utils.test_utils import TestUtils
 from . import APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX
+from ...common_fixtures.adapters.auth_service import \
+    prepare_permitted_user_details_mock
+from ...factories.models import StagePermittedRolesFactory, TaskFactory, \
+    StageModelFactory, TaskStageModelFactory, StageActionFactory, \
+    TaskStatusVariableFactory, ActionPermittedRolesFactory
 
 
 class TestCase01GetNextStagesRandomAssigneesOfATaskAPITestCase(TestUtils):
@@ -12,13 +20,67 @@ class TestCase01GetNextStagesRandomAssigneesOfATaskAPITestCase(TestUtils):
     REQUEST_METHOD = REQUEST_METHOD
     URL_SUFFIX = URL_SUFFIX
     SECURITY = {'oauth': {'scopes': ['read']}}
+
+    @pytest.fixture(autouse=True)
+    def setup(self, mocker, api_user):
+        user_obj = api_user
+        user_id = str(user_obj.user_id)
+        StagePermittedRolesFactory.reset_sequence()
+
+        TaskFactory.reset_sequence()
+        StageModelFactory.reset_sequence()
+        TaskStageModelFactory.reset_sequence()
+
+        user_details_mock = prepare_permitted_user_details_mock(mocker)
+        stage1 = StageModelFactory(
+            task_template_id='template_1',
+            display_logic="variable0==stage_id_0",
+            card_info_kanban=json.dumps(["FIELD_ID-1", "FIELD_ID-2"]),
+            card_info_list=json.dumps(["FIELD_ID-1", "FIELD_ID-2"]),
+        )
+        stage2 = StageModelFactory(
+            task_template_id='template_1',
+            display_logic="variable1==stage_id_1",
+            card_info_kanban=json.dumps(["FIELD_ID-1", "FIELD_ID-2"]),
+            card_info_list=json.dumps(["FIELD_ID-1", "FIELD_ID-2"])
+        )
+        stage3 = StageModelFactory(
+            task_template_id='template_1',
+            display_logic="variable2==stage_id_2",
+            card_info_kanban=json.dumps(["FIELD_ID-1", "FIELD_ID-2"]),
+            card_info_list=json.dumps(["FIELD_ID-1", "FIELD_ID-2"])
+        )
+        stages = [stage1, stage2, stage3]
+        path = 'ib_tasks.tests.populate.stage_actions_logic.stage_1_action_name_1'
+        action = StageActionFactory(stage=stage1, py_function_import_path=path)
+        actions = StageActionFactory.create_batch(6, stage=factory.Iterator(
+            stages))
+
+        task = TaskFactory(template_id='template_1')
+        TaskStatusVariableFactory(task_id=1, variable='variable0',
+                                  value="stage_id_0")
+        TaskStatusVariableFactory(task_id=1, variable='variable1',
+                                  value="stage_id_1")
+        TaskStatusVariableFactory(task_id=1, variable='variable2',
+                                  value="stage_id_2")
+        TaskStatusVariableFactory(task_id=1, variable='variable3',
+                                  value="stage_id_1")
+        TaskStageModelFactory.create_batch(
+            3, task=task,
+            stage=factory.Iterator(stages)
+        )
+        ActionPermittedRolesFactory.create_batch(3, action=action)
+
+
+
     @pytest.mark.django_db
-    def test_case(self, snapshot):
+    def test_case(self, snapshot, setup):
         body = {}
-        path_params = {"task_id": "1234", "action_id": "1234"}
+        path_params = {"task_id": 1, "action_id": 1}
         query_params = {}
         headers = {}
-        response = self.default_test_case(
-            body=body, path_params=path_params,
-            query_params=query_params, headers=headers, snapshot=snapshot
-        )
+        response = self.make_api_call(body=body,
+                                      path_params=path_params,
+                                      query_params=query_params,
+                                      headers=headers,
+                                      snapshot=snapshot)
--- ib_tasks/tests/views/get_task/__init__.py
+++ ib_tasks/tests/views/get_task/__init__.py
@@ -4,17 +4,3 @@ APP_NAME = "ib_tasks"
 OPERATION_NAME = "get_task"
 REQUEST_METHOD = "get"
 URL_SUFFIX = "task/v1/"
-
-from .test_case_01 import TestCase01GetTaskAPITestCase
-from .test_case_02 import TestCase02GetTaskAPITestCase
-from .test_case_03 import TestCase03GetTaskAPITestCase
-from .test_case_04 import TestCase04GetTaskAPITestCase
-from .test_case_05 import TestCase05GetTaskAPITestCase
-
-__all__ = [
-    "TestCase01GetTaskAPITestCase",
-    "TestCase02GetTaskAPITestCase",
-    "TestCase03GetTaskAPITestCase",
-    "TestCase04GetTaskAPITestCase",
-    "TestCase05GetTaskAPITestCase"
-]
