888eebf6caf48fb5e1425ce39d21bcc4af99b46d
geetha.k <geetha.k@ibhubs.co.in>
2020-08-08 09:31:31 +0530

    add a test

--- ib_tasks/interactors/get_task_due_missing_ reasons_interactor.py
+++ ib_tasks/interactors/get_task_due_missing_ reasons_interactor.py
@@ -1,14 +1,20 @@
+from ib_tasks.exceptions.task_custom_exceptions import InvalidTaskIdException
 from ib_tasks.interactors.presenter_interfaces.get_task_presenter_interface import \
     GetTaskPresenterInterface
 from ib_tasks.interactors.storage_interfaces.task_storage_interface import TaskStorageInterface
 
 
-class GetTaskDueMissingReasons:
+class GetTaskDueMissingReasonsInteractor:
     def __init__(self, task_storage: TaskStorageInterface):
         self.task_storage = task_storage
 
     def get_task_due_missing_reasons_wrapper(self,
                                              presenter: GetTaskPresenterInterface,
-                                             task_id: int):
+                                             task_id: int, user_id: str):
         try:
-            self.get_task_due_missing_reasons()
+            self.get_task_due_missing_reasons(task_id, user_id)
+        except InvalidTaskIdException:
+            return presenter.raise_exception_for_invalid_task_id()
+
+    def get_task_due_missing_reasons(self, task_id: int, user_id: str):
+        pass
--- ib_tasks/tests/interactors/test_get_task_reasons_for_missing_due_date_time.py
+++ ib_tasks/tests/interactors/test_get_task_reasons_for_missing_due_date_time.py
@@ -1,9 +1,21 @@
+from unittest.mock import create_autospec
 
+from ib_tasks.interactors.presenter_interfaces.get_task_presenter_interface import \
+    GetTaskPresenterInterface
+from ib_tasks.interactors.storage_interfaces.task_storage_interface import \
+    TaskStorageInterface
+from ib_tasks.interactors.
 
 class TestGetTaskReasons:
 
     def test_given_invalid_task_id_raises_exception(self):
         # Arrange
-
+        task_id = 1
+        user_id = "user_id_1"
+        storage = create_autospec(TaskStorageInterface)
+        presenter = create_autospec(GetTaskPresenterInterface)
+        interactor = GetTaskDueMissingReasonsInteractor(
+            task_storage=storage
+        )
         # Act
         # Assert
