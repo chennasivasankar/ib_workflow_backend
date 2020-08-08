deb5a33e3cd483f8a62a353e1c6abc9047cfcdbf
DurgaPrime <durgaprasad@ibhubs.co>
2020-08-08 15:48:31 +0530

    added migrations for discussion

--- ib_discussions/admin.py
+++ ib_discussions/admin.py
@@ -1,1 +1,37 @@
 # your django admin
+from django.contrib import admin
+
+from ib_discussions.models import Discussion, DiscussionSet, Comment
+
+
+class DiscussionAdmin(admin.ModelAdmin):
+    list_display = ("user_id", "description", "title", "is_clarified")
+    search_fields = ["user_id"]
+    list_filter = ["is_clarified"]
+
+
+class DiscussionInline(admin.TabularInline):
+    model = Discussion
+    extra = 0
+
+
+class DiscussionSetAdmin(admin.ModelAdmin):
+    list_display = ("id", "entity_id", "entity_type")
+    list_filter = ["entity_type"]
+    search_fields = ["entity_id"]
+    inlines = [DiscussionInline]
+
+
+class RepliesInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "user_id", "discussion_id", "content")
+    inlines = [RepliesInline]
+
+
+admin.site.register(Discussion, DiscussionAdmin)
+admin.site.register(DiscussionSet, DiscussionSetAdmin)
+admin.site.register(Comment, CommentAdmin)
