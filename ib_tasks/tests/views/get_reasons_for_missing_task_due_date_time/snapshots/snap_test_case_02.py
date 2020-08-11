b6b4f715f7666b0ccbe280872ea60d75e3d4b890
geetha.k <geetha.k@ibhubs.co.in>
2020-08-11 14:27:48 +0530

    controllers

--- ib_tasks/tests/views/get_reasons_for_missing_task_due_date_time/snapshots/snap_test_case_02.py
+++ ib_tasks/tests/views/get_reasons_for_missing_task_due_date_time/snapshots/snap_test_case_02.py
@@ -7,10 +7,51 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestCase01GetReasonsForMissingTaskDueDateTimeAPITestCase.test_case status_code'] = '404'
+snapshots['TestCase01GetReasonsForMissingTaskDueDateTimeAPITestCase.test_case status_code'] = '200'
 
-snapshots['TestCase01GetReasonsForMissingTaskDueDateTimeAPITestCase.test_case body'] = {
-    'http_status_code': 404,
-    'res_status': 'INVALID_TASK_ID',
-    'response': 'invalid task id is: 1, please send valid task id'
-}
+snapshots['TestCase01GetReasonsForMissingTaskDueDateTimeAPITestCase.test_case body'] = [
+    {
+        'due_date_time': '2020-08-11 14:19:33',
+        'due_missed_count': 1,
+        'reason': 'Missed reiterating objective',
+        'task_id': 1,
+        'user': {
+            'name': 'name_0',
+            'profile_pic': 'https://www.google.com/search?q=ibhubs&client=ubuntu&hs=DI7&channel=fs&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjZqYjthYfrAhUF4zgGHevjDZUQ_AUoA3oECAsQBQ&biw=1848&bih=913#imgrc=Kg3TRY0jmx3udM',
+            'user_id': '814ea30d-6736-41a0-877e-96b6d8278755'
+        }
+    },
+    {
+        'due_date_time': '2020-08-11 14:19:33',
+        'due_missed_count': 2,
+        'reason': 'wrong estimation of time',
+        'task_id': 1,
+        'user': {
+            'name': 'name_1',
+            'profile_pic': 'https://www.google.com/search?q=ibhubs&client=ubuntu&hs=DI7&channel=fs&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjZqYjthYfrAhUF4zgGHevjDZUQ_AUoA3oECAsQBQ&biw=1848&bih=913#imgrc=Kg3TRY0jmx3udM',
+            'user_id': '123e4567-e89b-12d3-a456-426614174001'
+        }
+    },
+    {
+        'due_date_time': '2020-08-11 14:19:33',
+        'due_missed_count': 3,
+        'reason': 'wrong estimation of time',
+        'task_id': 1,
+        'user': {
+            'name': 'name_2',
+            'profile_pic': 'https://www.google.com/search?q=ibhubs&client=ubuntu&hs=DI7&channel=fs&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjZqYjthYfrAhUF4zgGHevjDZUQ_AUoA3oECAsQBQ&biw=1848&bih=913#imgrc=Kg3TRY0jmx3udM',
+            'user_id': '123e4567-e89b-12d3-a456-426614174002'
+        }
+    },
+    {
+        'due_date_time': '2020-08-11 14:19:33',
+        'due_missed_count': 4,
+        'reason': 'wrong estimation of time',
+        'task_id': 1,
+        'user': {
+            'name': 'name_3',
+            'profile_pic': 'https://www.google.com/search?q=ibhubs&client=ubuntu&hs=DI7&channel=fs&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjZqYjthYfrAhUF4zgGHevjDZUQ_AUoA3oECAsQBQ&biw=1848&bih=913#imgrc=Kg3TRY0jmx3udM',
+            'user_id': '123e4567-e89b-12d3-a456-426614174003'
+        }
+    }
+]
