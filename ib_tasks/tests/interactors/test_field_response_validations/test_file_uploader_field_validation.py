3efeeb25e1f753ad981479d3ec9634ecd5661963
Mukunda <mukunda.pedda@ibhubs.co.in>
2020-08-11 12:01:31 +0530

    adds test cases for field_response_validation interactor

--- ib_tasks/tests/interactors/test_field_response_validations/test_file_uploader_field_validation.py
+++ ib_tasks/tests/interactors/test_field_response_validations/test_file_uploader_field_validation.py
@@ -2,10 +2,10 @@ import pytest
 
 from ib_tasks.interactors.create_or_update_task.field_response_validations \
     import \
-    ImageUploaderFieldValidationInteractor
+    FileUploaderFieldValidationInteractor
 
 
-class TestImageUploaderFieldValidationInteractor:
+class TestFileUploaderFieldValidationInteractor:
 
     @pytest.fixture
     def allowed_formats(self):
@@ -18,48 +18,54 @@ class TestImageUploaderFieldValidationInteractor:
 
     @pytest.mark.parametrize(
         "field_response",
-        ["hps://www.google.com/search?q=images&client=ubuntu&hs=f83&channel=fs&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjm_L6voJLrAhVYyDgGHccbCaYQ_AUoAXoECA0QAw&biw=1848&bih=913#imgrc=a9B7raWE3PxoBM",
-         ""])
-    def test_given_invalid_image_uploader_value_in_field_response_raise_exception(
+        [
+            "hps://www.google.com/search?q=images&client=ubuntu&hs=f83"
+            "&channel=fs&source=lnms&tbm=isch&sa=X&ved"
+            "=2ahUKEwjm_L6voJLrAhVYyDgGHccbCaYQ_AUoAXoECA0QAw&biw=1848&bih"
+            "=913#imgrc=a9B7raWE3PxoBM",
+            ""])
+    def test_given_invalid_file_uploader_value_in_field_response_raise_exception(
             self, allowed_formats, field_response
     ):
-
         # Arrange
         from ib_tasks.exceptions.field_values_custom_exceptions import \
-            InvalidUrlForImage
+            InvalidUrlForFile
         field_id = "FIN_VENDOR_COMPANY_DETAILS"
-        field_response = "hps://www.google.com/search?q=images&client=ubuntu&hs=f83&channel=fs&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjm_L6voJLrAhVYyDgGHccbCaYQ_AUoAXoECA0QAw&biw=1848&bih=913#imgrc=a9B7raWE3PxoBM"
-        interactor = ImageUploaderFieldValidationInteractor(
+        field_response = "hps://www.google.com/search?q=images&client=ubuntu" \
+                         "&hs=f83&channel=fs&source=lnms&tbm=isch&sa=X&ved" \
+                         "=2ahUKEwjm_L6voJLrAhVYyDgGHccbCaYQ_AUoAXoECA0QAw" \
+                         "&biw=1848&bih=913#imgrc=a9B7raWE3PxoBM"
+        interactor = FileUploaderFieldValidationInteractor(
             field_id=field_id,
             field_response=field_response,
             allowed_formats=allowed_formats
         )
 
         # Act
-
-        with pytest.raises(InvalidUrlForImage) as err:
+        with pytest.raises(InvalidUrlForFile) as err:
             interactor.validate_field_response()
 
         # Assert
         exception_object = err.value
         assert exception_object.field_id == field_id
-        assert exception_object.image_url == field_response
+        assert exception_object.file_url == field_response
 
-    def test_invalid_image_format_in_field_response_raise_exception(self, allowed_formats):
+    def test_invalid_file_format_in_field_response_raise_exception(self, allowed_formats):
         # Arrange
         from ib_tasks.exceptions.field_values_custom_exceptions import \
-            InvalidImageFormat
+            InvalidFileFormat
         field_id = "FIN_VENDOR_COMPANY_DETAILS"
-        field_response = "https://www.google.com/bright-spring-view-cameo-island-260nw-1048185397.jqa"
+        field_response = "https://www.google.com/bright-spring-view-cameo" \
+                         "-island-260nw-1048185397.jqa"
         given_image_format = ".jqa"
-        interactor = ImageUploaderFieldValidationInteractor(
+        interactor = FileUploaderFieldValidationInteractor(
             field_id=field_id,
             field_response=field_response,
             allowed_formats=allowed_formats
         )
 
         # Act
-        with pytest.raises(InvalidImageFormat) as err:
+        with pytest.raises(InvalidFileFormat) as err:
             interactor.validate_field_response()
 
         # Assert
@@ -73,8 +79,9 @@ class TestImageUploaderFieldValidationInteractor:
     ):
         # Arrange
         field_id = "FIN_VENDOR_COMPANY_DETAILS"
-        field_response = "https://www.google.com/bright-spring-view-cameo-island-260nw-1048185397.jpg"
-        interactor = ImageUploaderFieldValidationInteractor(
+        field_response = "https://www.google.com/bright-spring-view-cameo" \
+                         "-island-260nw-1048185397.jpg"
+        interactor = FileUploaderFieldValidationInteractor(
             field_id=field_id,
             field_response=field_response,
             allowed_formats=allowed_formats
@@ -84,4 +91,3 @@ class TestImageUploaderFieldValidationInteractor:
         interactor.validate_field_response()
 
         # Assert
-
