e2d62d8b2388ace0d5c296bfe02324ec8fd5c4af
chennasivasankar <siva.chenna@ibhubs.co.in>
2020-08-11 20:37:15 +0530

    Add interface for refresh auth tokens

--- ib_iam/adapters/auth_service.py
+++ ib_iam/adapters/auth_service.py
@@ -21,6 +21,18 @@ class AccessTokenNotFound(Exception):
     pass
 
 
+class UserAccountNotFound(Exception):
+    pass
+
+
+class RefreshTokenHasExpired(Exception):
+    pass
+
+
+class RefreshTokenHasNotFound(Exception):
+    pass
+
+
 class AuthService:
     @property
     def interface(self):
@@ -31,32 +43,33 @@ class AuthService:
     def get_user_tokens_dto_for_given_email_and_password_dto(
             self, email_and_password_dto: EmailAndPasswordDTO
     ) -> UserTokensDTO:
+        from ib_users.exceptions.custom_exception_constants import \
+            INVALID_EMAIL
+        from ib_iam.interactors.user_login_interactor import \
+            IncorrectPassword
+        from ib_iam.exceptions.custom_exceptions import InvalidEmail
+        from ib_users.exceptions.custom_exception_constants import \
+            PASSWORD_AT_LEAST_1_SPECIAL_CHARACTER
+        from ib_users.exceptions.custom_exception_constants import \
+            PASSWORD_MIN_LENGTH_IS
+        from ib_users.exceptions.custom_exception_constants import \
+            USER_ACCOUNT_IS_DEACTIVATED
+        from ib_iam.exceptions.custom_exceptions import \
+            UserAccountDoesNotExist
+
         try:
             user_auth_tokens_dto = self.interface. \
                 get_user_auth_tokens_for_login_with_email_and_password(
                 email=email_and_password_dto.email,
                 password=email_and_password_dto.password)
         except CustomException as err:
-            from ib_users.exceptions.custom_exception_constants import \
-                INVALID_EMAIL
-            from ib_iam.interactors.user_login_interactor import \
-                IncorrectPassword
             if err.error_type == INVALID_EMAIL.code:
-                from ib_iam.exceptions.custom_exceptions import InvalidEmail
                 raise InvalidEmail
-            from ib_users.exceptions.custom_exception_constants import \
-                PASSWORD_AT_LEAST_1_SPECIAL_CHARACTER
             if err.error_type == PASSWORD_AT_LEAST_1_SPECIAL_CHARACTER.code:
                 raise IncorrectPassword
-            from ib_users.exceptions.custom_exception_constants import \
-                PASSWORD_MIN_LENGTH_IS
             if err.error_type == PASSWORD_MIN_LENGTH_IS.code:
                 raise IncorrectPassword
-            from ib_users.exceptions.custom_exception_constants import \
-                USER_ACCOUNT_IS_DEACTIVATED
             if err.error_type == USER_ACCOUNT_IS_DEACTIVATED.code:
-                from ib_iam.exceptions.custom_exceptions import \
-                    UserAccountDoesNotExist
                 raise UserAccountDoesNotExist
         else:
             converted_user_tokens_dto = self._convert_to_user_tokens_dto(
@@ -69,20 +82,22 @@ class AuthService:
     def get_reset_password_token(self, email: str, expires_in_sec: int) -> str:
         from ib_users.interactors.exceptions.user_credentials_exceptions import \
             AccountWithEmailDoesntExistException
+        from ib_users.exceptions.custom_exception_constants import \
+            INVALID_EMAIL
+        from ib_iam.exceptions.custom_exceptions import InvalidEmail
+        from ib_iam.exceptions.custom_exceptions import \
+            UserAccountDoesNotExist
+
         try:
             reset_password_token = self.interface. \
                 get_reset_password_token_for_reset_password(
                 email=email, token_expiry_in_seconds=expires_in_sec)
             return reset_password_token
         except CustomException as err:
-            from ib_users.exceptions.custom_exception_constants import \
-                INVALID_EMAIL
             if err.error_type == INVALID_EMAIL.code:
-                from ib_iam.exceptions.custom_exceptions import InvalidEmail
                 raise InvalidEmail
         except AccountWithEmailDoesntExistException:
-            from ib_iam.exceptions.custom_exceptions import \
-                UserAccountDoesNotExist
+
             raise UserAccountDoesNotExist
 
     def update_user_password_with_reset_password_token(
@@ -91,21 +106,22 @@ class AuthService:
             InvalidTokenException
         from ib_users.interactors.exceptions.user_credentials_exceptions import \
             TokenExpiredException
+        from ib_iam.interactors.reset_user_password_interactor import \
+            PasswordAtLeastOneSpecialCharacter
+        from ib_users.exceptions.custom_exception_constants import \
+            PASSWORD_AT_LEAST_1_SPECIAL_CHARACTER
+        from ib_iam.interactors.reset_user_password_interactor import \
+            PasswordMinLength
+
         try:
             self.interface.reset_password_for_given_user_password_reset_token(
                 token=reset_password_token, new_password=password)
         except CustomException as err:
-            from ib_users.exceptions.custom_exception_constants import \
-                PASSWORD_AT_LEAST_1_SPECIAL_CHARACTER
             if err.error_type == PASSWORD_AT_LEAST_1_SPECIAL_CHARACTER.code:
-                from ib_iam.interactors.reset_user_password_interactor import \
-                    PasswordAtLeastOneSpecialCharacter
                 raise PasswordAtLeastOneSpecialCharacter
             from ib_users.exceptions.custom_exception_constants import \
                 PASSWORD_MIN_LENGTH_IS
             if err.error_type == PASSWORD_MIN_LENGTH_IS.code:
-                from ib_iam.interactors.reset_user_password_interactor import \
-                    PasswordMinLength
                 raise PasswordMinLength
         except InvalidTokenException:
             from ib_iam.interactors.reset_user_password_interactor import \
@@ -126,14 +142,32 @@ class AuthService:
         )
         return converted_user_tokens_dto
 
-    def get_refresh_auth_tokens(self, access_token: str, refresh_token: str):
-        from ib_users.exceptions.oauth2_exceptions import \
-            AccessTokenNotFoundException
+    def get_refresh_auth_tokens_dto(self, access_token: str,
+                                    refresh_token: str):
+        from ib_users.constants.custom_exception_messages import \
+            INVALID_ACCESS_TOKEN
+        from django_swagger_utils.drf_server.exceptions import NotFound
+        from ib_users.exceptions.custom_exception_constants import \
+            USER_ACCOUNT_IS_DEACTIVATED
+        from ib_users.exceptions.oauth2_exceptions import RefreshTokenExpired
+        from ib_users.exceptions.oauth2_exceptions import RefreshTokenNotFound
+
         try:
             user_auth_tokens_dto = \
                 self.interface.refresh_auth_tokens(
                     access_token=access_token, refresh_token=refresh_token
                 )
-        except AccessTokenNotFoundException:
-            raise AccessTokenNotFound
-
+        except NotFound as err:
+            if err.res_status == INVALID_ACCESS_TOKEN.code:
+                raise AccessTokenNotFound
+        except RefreshTokenExpired:
+            raise RefreshTokenHasExpired
+        except RefreshTokenNotFound:
+            raise RefreshTokenHasNotFound
+        except CustomException as err:
+            if err.error_type == USER_ACCOUNT_IS_DEACTIVATED.code:
+                raise UserAccountNotFound
+        else:
+            converted_user_tokens_dto = self._convert_to_user_tokens_dto(
+                user_auth_tokens_dto)
+            return converted_user_tokens_dto
--- ib_iam/tests/adapters/test_get_refresh_auth_tokens.py
+++ ib_iam/tests/adapters/test_get_refresh_auth_tokens.py
@@ -1,6 +1,139 @@
+import pytest
+
+
 class TestGetRefreshAuthTokens:
 
-    def prepare_get_refresh_auth_tokens_mock(self, mocker):
+    @staticmethod
+    def prepare_get_refresh_auth_tokens_mock(mocker):
         mock = mocker.patch(
-
+            "ib_users.interfaces.service_interface.ServiceInterface.refresh_auth_tokens"
         )
+        return mock
+
+    def test_with_access_token_not_found_raise_exception(self, mocker):
+        # Arrange
+        access_token = "rNYAlle5thjiWD5MIt63GkhAws5suQ"
+        refresh_token = "pNYAlle5thjiWD5MIt63GkhAws5suQ"
+
+        get_refresh_tokens_mock = self.prepare_get_refresh_auth_tokens_mock(
+            mocker=mocker)
+        from django_swagger_utils.drf_server.exceptions import NotFound
+
+        from ib_users.constants.custom_exception_messages import \
+            INVALID_ACCESS_TOKEN
+        get_refresh_tokens_mock.side_effect = NotFound(
+            message=INVALID_ACCESS_TOKEN.message,
+            res_status=INVALID_ACCESS_TOKEN.code
+        )
+
+        from ib_iam.adapters.service_adapter import ServiceAdapter
+        service_adapter = ServiceAdapter()
+
+        # Assert
+        from ib_iam.adapters.auth_service import AccessTokenNotFound
+        with pytest.raises(AccessTokenNotFound):
+            service_adapter.auth_service.get_refresh_auth_tokens_dto(
+                access_token=access_token, refresh_token=refresh_token
+            )
+
+    def test_with_user_account_not_found_raise_exception(self, mocker):
+        # Arrange
+        access_token = "rNYAlle5thjiWD5MIt63GkhAws5suQ"
+        refresh_token = "pNYAlle5thjiWD5MIt63GkhAws5suQ"
+
+        get_refresh_tokens_mock = self.prepare_get_refresh_auth_tokens_mock(
+            mocker=mocker)
+        from ib_users.validators.base_validator import CustomException
+
+        from ib_users.constants.custom_exception_messages import \
+            USER_ACCOUNT_IS_DEACTIVATED
+        get_refresh_tokens_mock.side_effect = CustomException(
+            exception_type=USER_ACCOUNT_IS_DEACTIVATED.code,
+            message=USER_ACCOUNT_IS_DEACTIVATED.message
+        )
+
+        from ib_iam.adapters.service_adapter import ServiceAdapter
+        service_adapter = ServiceAdapter()
+
+        # Assert
+        from ib_iam.adapters.auth_service import UserAccountNotFound
+        with pytest.raises(UserAccountNotFound):
+            service_adapter.auth_service.get_refresh_auth_tokens_dto(
+                access_token=access_token, refresh_token=refresh_token
+            )
+
+    def test_with_refresh_token_has_expired_raise_exception(self, mocker):
+        # Arrange
+        access_token = "rNYAlle5thjiWD5MIt63GkhAws5suQ"
+        refresh_token = "pNYAlle5thjiWD5MIt63GkhAws5suQ"
+
+        get_refresh_tokens_mock = self.prepare_get_refresh_auth_tokens_mock(
+            mocker=mocker)
+        from ib_users.exceptions.oauth2_exceptions import RefreshTokenExpired
+        get_refresh_tokens_mock.side_effect = RefreshTokenExpired
+
+        from ib_iam.adapters.service_adapter import ServiceAdapter
+        service_adapter = ServiceAdapter()
+
+        # Assert
+        from ib_iam.adapters.auth_service import RefreshTokenHasExpired
+        with pytest.raises(RefreshTokenHasExpired):
+            service_adapter.auth_service.get_refresh_auth_tokens_dto(
+                access_token=access_token, refresh_token=refresh_token
+            )
+
+    def test_with_refresh_token_not_found_raise_exception(self, mocker):
+        # Arrange
+        access_token = "rNYAlle5thjiWD5MIt63GkhAws5suQ"
+        refresh_token = "pNYAlle5thjiWD5MIt63GkhAws5suQ"
+
+        get_refresh_tokens_mock = self.prepare_get_refresh_auth_tokens_mock(
+            mocker=mocker)
+        from ib_users.exceptions.oauth2_exceptions import RefreshTokenNotFound
+        get_refresh_tokens_mock.side_effect = RefreshTokenNotFound
+
+        from ib_iam.adapters.service_adapter import ServiceAdapter
+        service_adapter = ServiceAdapter()
+
+        # Assert
+        from ib_iam.adapters.auth_service import RefreshTokenHasNotFound
+        with pytest.raises(RefreshTokenHasNotFound):
+            service_adapter.auth_service.get_refresh_auth_tokens_dto(
+                access_token=access_token, refresh_token=refresh_token
+            )
+
+    def test_with_valid_details_return_response(self, mocker):
+        # Arrange
+        access_token = "rNYAlle5thjiWD5MIt63GkhAws5suQ"
+        refresh_token = "pNYAlle5thjiWD5MIt63GkhAws5suQ"
+        from ib_users.interactors.third_party.user_tokens_generator import \
+            UserAuthTokensDTO
+        user_auth_tokens_dto = UserAuthTokensDTO(
+            access_token="asdfaldskfjdfdlsdkf",
+            refresh_token="sadfenkljkdfeller",
+            expires_in=5647665599,
+            user_id="11"
+        )
+
+        from ib_iam.adapters.auth_service import UserTokensDTO
+        expected_user_tokens_dto = UserTokensDTO(
+            access_token='asdfaldskfjdfdlsdkf',
+            refresh_token='sadfenkljkdfeller',
+            expires_in_seconds=5647665599,
+            user_id='11'
+        )
+
+        get_refresh_tokens_mock = self.prepare_get_refresh_auth_tokens_mock(
+            mocker=mocker)
+        get_refresh_tokens_mock.return_value = user_auth_tokens_dto
+
+        from ib_iam.adapters.service_adapter import ServiceAdapter
+        service_adapter = ServiceAdapter()
+
+        # Act
+        response = service_adapter.auth_service.get_refresh_auth_tokens_dto(
+            access_token=access_token, refresh_token=refresh_token
+        )
+
+        # Assert
+        assert response == expected_user_tokens_dto
