fcfdc0921a51cc09f707862e55d1497ee7534b39
geetha.k <geetha.k@ibhubs.co.in>
2020-08-11 10:40:22 +0530

    freeze time in test case

--- ib_tasks/tests/storages/snapshots/snap_test_add_due_delay_details.py
+++ ib_tasks/tests/storages/snapshots/snap_test_add_due_delay_details.py
@@ -7,4 +7,4 @@ from snapshottest import GenericRepr, Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestAddDueDelayDetails.test_add_due_delay_details_given_details reason'] = GenericRepr("<QuerySet [{'id': 1, 'task_id': 1, 'due_datetime': datetime.datetime(2020, 8, 11, 4, 53, 33, 630803, tzinfo=<UTC>), 'count': 1, 'reason_id': '1', 'reason': 'reason', 'user_id': '123e4567-e89b-12d3-a456-426614174000'}]>")
+snapshots['TestAddDueDelayDetails.test_add_due_delay_details_given_details reason'] = GenericRepr("<QuerySet [{'id': 1, 'task_id': 1, 'due_datetime': FakeDatetime(2020, 8, 10, 12, 30, tzinfo=<UTC>), 'count': 1, 'reason_id': 1, 'reason': 'reason', 'user_id': '123e4567-e89b-12d3-a456-426614174000'}]>")
