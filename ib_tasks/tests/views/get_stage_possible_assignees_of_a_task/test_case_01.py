61e187da9790eb09cdd88e4baea43ead08bd1164
rajesh.ganjikunta <rajesh.ganjikunta@ibhubs.co.in>
2020-08-12 11:48:59 +0530

    add spec for get stage possible assignees

---
+++ ib_tasks/tests/views/get_stage_possible_assignees_of_a_task/test_case_01.py
@@ -0,0 +1,31 @@
+"""
+# TODO: Update test case description
+"""
+import pytest
+from django_swagger_utils.utils.test_utils import TestUtils
+from . import APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX
+
+
+class TestCase01GetStagePossibleAssigneesOfATaskAPITestCase(TestUtils):
+    APP_NAME = APP_NAME
+    OPERATION_NAME = OPERATION_NAME
+    REQUEST_METHOD = REQUEST_METHOD
+    URL_SUFFIX = URL_SUFFIX
+    SECURITY = {'oauth': {'scopes': ['read']}}
+
+    @pytest.mark.django_db
+    def test_case(self, snapshot):
+        body = {}
+        path_params = {"task_id": "1234", "stage_id": "ibgroup"}
+        query_params = {
+            'user_search_type': 'USER',
+            'limit': 271,
+            'offset': 162,
+            'search_query': 'string'
+        }
+        headers = {}
+        response = self.make_api_call(body=body,
+                                      path_params=path_params,
+                                      query_params=query_params,
+                                      headers=headers,
+                                      snapshot=snapshot)
