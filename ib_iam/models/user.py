803d4db579e72f0578209dd7dacd94f66331ea60
Your Name <jaswanth.mamidipudi@ibhubs.co.in>
2020-08-12 22:45:55 +0530

    Add related name in user team model

--- ib_iam/models/user.py
+++ ib_iam/models/user.py
@@ -5,13 +5,15 @@ class UserDetails(models.Model):
     user_id = models.CharField(max_length=1000)
     name = models.CharField(max_length=1000, null=True)
     is_admin = models.BooleanField(default=False)
-    company = models.ForeignKey('Company', on_delete=models.SET_NULL, null=True,
+    company = models.ForeignKey('Company', on_delete=models.SET_NULL,
+                                null=True,
                                 blank=True, related_name="users")
 
 
 class UserTeam(models.Model):
     user_id = models.CharField(max_length=1000)
-    team = models.ForeignKey('Team', on_delete=models.CASCADE)
+    team = models.ForeignKey('Team', on_delete=models.CASCADE,
+                             related_name="users")
 
 
 class UserRole(models.Model):
