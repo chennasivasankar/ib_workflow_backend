a9294a98d5a2eb2ebeecf112cbcf4c9fad5c37d3
rajesh.ganjikunta <rajesh.ganjikunta@ibhubs.co.in>
2020-08-12 12:05:21 +0530

    add spec for get stage possible assignees

---
+++ ib_tasks/tests/views/get_stage_searchable_possible_assignees_of_a_task/__init__.py
@@ -0,0 +1,6 @@
+# pylint: disable=wrong-import-position
+
+APP_NAME = "ib_tasks"
+OPERATION_NAME = "get_stage_searchable_possible_assignees_of_a_task"
+REQUEST_METHOD = "get"
+URL_SUFFIX = "task/stage/stage_assignees/v1/"
