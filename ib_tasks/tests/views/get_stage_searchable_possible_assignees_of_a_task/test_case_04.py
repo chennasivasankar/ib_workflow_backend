75ba1205edaa26ae3b5ca34974399027a02b441d
rajesh.ganjikunta <rajesh.ganjikunta@ibhubs.co.in>
2020-08-13 16:58:26 +0530

    add all tests for get stage searchable possible assignees

--- ib_tasks/tests/views/get_stage_searchable_possible_assignees_of_a_task/test_case_04.py
+++ ib_tasks/tests/views/get_stage_searchable_possible_assignees_of_a_task/test_case_04.py
@@ -1,5 +1,5 @@
 """
-test with invalid limit value raises exception
+test with invalid stage id raises exception
 """
 
 import pytest
@@ -8,7 +8,7 @@ from django_swagger_utils.utils.test_utils import TestUtils
 from . import APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX
 
 
-class TestCase03GetStageSearchablePossibleAssigneesOfATaskAPITestCase(
+class TestCase04GetStageSearchablePossibleAssigneesOfATaskAPITestCase(
         TestUtils):
     APP_NAME = APP_NAME
     OPERATION_NAME = OPERATION_NAME
@@ -16,12 +16,11 @@ class TestCase03GetStageSearchablePossibleAssigneesOfATaskAPITestCase(
     URL_SUFFIX = URL_SUFFIX
     SECURITY = {'oauth': {'scopes': ['read']}}
 
-    @pytest.mark.parametrize('limit', [-1, 0, -5])
     @pytest.mark.django_db
-    def test_case(self, snapshot, limit):
+    def test_case(self, snapshot):
         body = {}
         path_params = {"stage_id": 1}
-        query_params = {'limit': limit, 'offset': 0, 'search_query': 'string'}
+        query_params = {'limit': 10, 'offset': 0, 'search_query': 'string'}
         headers = {}
         response = self.make_api_call(body=body,
                                       path_params=path_params,
