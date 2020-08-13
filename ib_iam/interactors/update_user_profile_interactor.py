dbbc0639f89fc7a815c52bef184b8204e86d6711
Your Name <jaswanth.mamidipudi@ibhubs.co.in>
2020-08-13 20:21:51 +0530

    Add role ids in interactor and exceptions are in middle Add a test in update user profile

--- ib_iam/interactors/update_user_profile_interactor.py
+++ ib_iam/interactors/update_user_profile_interactor.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from ib_iam.exceptions.custom_exceptions import (
     InvalidNameLength,
     NameShouldNotContainsNumbersSpecCharacters, InvalidEmail,
-    UserAccountAlreadyExistWithThisEmail)
+    UserAccountAlreadyExistWithThisEmail, RoleIdsAreInvalid)
 from ib_iam.interactors.mixins.validation import ValidationMixin
 from ib_iam.interactors.presenter_interfaces \
     .update_user_profile_presenter_interface import \
@@ -18,30 +20,38 @@ class UpdateUserProfileInteractor(ValidationMixin):
 
     def update_user_profile_wrapper(
             self,
-            user_profile_dto:
-            UserProfileDTO,
+            user_profile_dto: UserProfileDTO,
+            role_ids: List[str],
             presenter: UpdateUserProfilePresenterInterface):
         try:
-            self.update_user_profile(user_profile_dto=user_profile_dto)
+            self.update_user_profile(user_profile_dto=user_profile_dto,
+                                     role_ids=role_ids)
             response = presenter.get_success_response_for_update_user_profile()
         except InvalidNameLength:
             response = presenter.raise_invalid_name_length_exception_for_update_user_profile()
         except NameShouldNotContainsNumbersSpecCharacters:
             response = presenter \
                 .raise_name_should_not_contain_special_chars_and_numbers_exception_for_update_user_profile()
+        except RoleIdsAreInvalid:
+            response = presenter.raise_role_ids_are_invalid()
         except InvalidEmail:
             response = presenter.raise_invalid_email_exception_for_update_user_profile()
         except UserAccountAlreadyExistWithThisEmail:
             response = presenter.raise_email_already_in_use_exception_for_update_user_profile()
         return response
 
-    def update_user_profile(self, user_profile_dto: UserProfileDTO):
+    def update_user_profile(
+            self, user_profile_dto: UserProfileDTO, role_ids: List[str]):
         name = user_profile_dto.name
         user_id = user_profile_dto.user_id
         self._validate_name_and_throw_exception(name=name)
+        self._validate_roles(role_ids=role_ids)
         self._update_user_profile_in_ib_users(
             user_profile_dto=user_profile_dto)
         self.user_storage.update_user_name(user_id=user_id, name=name)
+        is_user_admin = self.user_storage.is_user_admin(user_id=user_id)
+        if is_user_admin:
+            self._update_user_roles(role_ids=role_ids, user_id=user_id)
 
     def _update_user_profile_in_ib_users(self,
                                          user_profile_dto: UserProfileDTO):
@@ -64,3 +74,16 @@ class UpdateUserProfileInteractor(ValidationMixin):
                                           email=user_profile_dto.email,
                                           profile_pic_url=profile_pic_url)
         return user_profile_dto
+
+    def _update_user_roles(self, role_ids: List[str], user_id: str):
+        self.user_storage.remove_roles_for_user(user_id)
+        ids_of_role_objects = self.user_storage.get_role_objs_ids(role_ids)
+        self.user_storage.add_roles_to_the_user(user_id=user_id,
+                                                role_ids=ids_of_role_objects)
+
+    def _validate_roles(self, role_ids: List[str]):
+        valid_role_ids = self.user_storage.get_valid_role_ids(role_ids=role_ids)
+        # todo check whether duplicate ids sent or invalid role ids sent
+        #  then exceptions accordingly and modify the interactors accordingly
+        if are_not_valid:
+            raise RoleIdsAreInvalid()
--- ib_iam/tests/interactors/test_update_user_profile_interactor.py
+++ ib_iam/tests/interactors/test_update_user_profile_interactor.py
@@ -35,6 +35,7 @@ class TestUpdateUserProfileInteractor:
     def test_validate_name_minimum_name_length_exception(
             self, presenter_mock, interactor, name):
         # Arrange
+        role_ids = ["1"]
         user_id_name_email_and_profile_pic_url_dto = \
             UserIdNameEmailAndProfilePicUrlDTOFactory(name=name)
         presenter_mock.raise_invalid_name_length_exception_for_update_user_profile \
@@ -42,8 +43,8 @@ class TestUpdateUserProfileInteractor:
 
         # Act
         interactor.update_user_profile_wrapper(
-            user_profile_dto=
-            user_id_name_email_and_profile_pic_url_dto,
+            user_profile_dto=user_id_name_email_and_profile_pic_url_dto,
+            role_ids=role_ids,
             presenter=presenter_mock)
 
         # Assert
@@ -55,6 +56,7 @@ class TestUpdateUserProfileInteractor:
     def test_validate_name_when_contains_special_characters_and_numbers_throw_exception(
             self, presenter_mock, interactor, name):
         # Arrange
+        role_ids = ["1"]
         user_id_name_email_and_profile_pic_url_dto = \
             UserIdNameEmailAndProfilePicUrlDTOFactory(name=name)
         presenter_mock \
@@ -63,8 +65,8 @@ class TestUpdateUserProfileInteractor:
 
         # Act
         interactor.update_user_profile_wrapper(
-            user_profile_dto=
-            user_id_name_email_and_profile_pic_url_dto,
+            user_profile_dto=user_id_name_email_and_profile_pic_url_dto,
+            role_ids=role_ids,
             presenter=presenter_mock)
 
         # Assert
@@ -75,6 +77,7 @@ class TestUpdateUserProfileInteractor:
     def test_given_invalid_email_returns_invalid_email_exception_response(
             self, mocker, presenter_mock, interactor):
         # Arrange
+        role_ids = ["1"]
         user_id_name_email_and_profile_pic_url_dto = \
             UserIdNameEmailAndProfilePicUrlDTOFactory()
         from ib_iam.tests.common_fixtures.adapters.user_service \
@@ -87,17 +90,37 @@ class TestUpdateUserProfileInteractor:
 
         # Act
         interactor.update_user_profile_wrapper(
-            user_profile_dto=
-            user_id_name_email_and_profile_pic_url_dto,
+            user_profile_dto=user_id_name_email_and_profile_pic_url_dto,
+            role_ids=role_ids,
             presenter=presenter_mock)
 
         # Assert
         presenter_mock.raise_invalid_email_exception_for_update_user_profile \
             .assert_called_once()
 
+    def test_given_invalid_role_ids_returns_invalid_role_ids_response(
+            self, storage_mock, presenter_mock, interactor):
+        # Arrange
+        role_ids = ["1"]
+        user_id_name_email_and_profile_pic_url_dto = \
+            UserIdNameEmailAndProfilePicUrlDTOFactory()
+        storage_mock.check_are_valid_role_ids.return_value = False
+        presenter_mock.raise_role_ids_are_invalid.return_value = mock.Mock()
+
+        # Act
+        interactor.update_user_profile_wrapper(
+            user_profile_dto=user_id_name_email_and_profile_pic_url_dto,
+            role_ids=role_ids,
+            presenter=presenter_mock)
+
+        # Assert
+        presenter_mock.raise_role_ids_are_invalid.assert_called_once()
+
+
     def test_given_email_already_in_use_returns_email_already_in_use_response(
             self, mocker, presenter_mock, interactor):
         # Arrange
+        role_ids = ["1"]
         user_id_name_email_and_profile_pic_url_dto = \
             UserIdNameEmailAndProfilePicUrlDTOFactory()
         from ib_iam.tests.common_fixtures.adapters.user_service \
@@ -111,19 +134,20 @@ class TestUpdateUserProfileInteractor:
 
         # Act
         interactor.update_user_profile_wrapper(
-            user_profile_dto=
-            user_id_name_email_and_profile_pic_url_dto,
+            user_profile_dto=user_id_name_email_and_profile_pic_url_dto,
+            role_ids=role_ids,
             presenter=presenter_mock)
 
         # Assert
         presenter_mock.raise_email_already_in_use_exception_for_update_user_profile \
             .assert_called_once()
 
-    def test_given_valid_details_returns_success_response(
+    def test_given_valid_details_returns_success_response_for_user(
             self, mocker, storage_mock, presenter_mock, interactor):
         # Arrange
         name = "username"
         user_id = "user_id1"
+        role_ids = ["1"]
         user_id_name_email_and_profile_pic_url_dto = \
             UserIdNameEmailAndProfilePicUrlDTOFactory(user_id=user_id,
                                                       name=name)
@@ -135,12 +159,51 @@ class TestUpdateUserProfileInteractor:
 
         # Act
         interactor.update_user_profile_wrapper(
-            user_profile_dto=
-            user_id_name_email_and_profile_pic_url_dto,
+            user_profile_dto=user_id_name_email_and_profile_pic_url_dto,
+            role_ids=role_ids,
             presenter=presenter_mock)
 
         # Assert
         adapter_mock.assert_called_once()
+        storage_mock.is_user_admin.assert_called_once_with(user_id=user_id)
+        storage_mock.update_user_name.assert_called_once_with(user_id=user_id,
+                                                              name=name)
+        presenter_mock.get_success_response_for_update_user_profile \
+            .assert_called_once()
+
+    def test_given_valid_details_returns_success_response_for_admin(
+            self, mocker, storage_mock, presenter_mock, interactor):
+        # Arrange
+        name = "username"
+        user_id = "user_id1"
+        role_ids = ["1"]
+        ids_of_role_objects = ["role_1"]
+        user_id_name_email_and_profile_pic_url_dto = \
+            UserIdNameEmailAndProfilePicUrlDTOFactory(user_id=user_id,
+                                                      name=name)
+        from ib_iam.tests.common_fixtures.adapters.user_service \
+            import prepare_update_user_profile_adapter_mock
+        adapter_mock = prepare_update_user_profile_adapter_mock(mocker=mocker)
+        storage_mock.is_user_admin.return_value = True
+        storage_mock.get_role_objs_ids.return_value = ids_of_role_objects
+        presenter_mock.get_success_response_for_update_user_profile \
+            .return_value = mock.Mock()
+
+        # Act
+        interactor.update_user_profile_wrapper(
+            user_profile_dto=user_id_name_email_and_profile_pic_url_dto,
+            role_ids=role_ids,
+            presenter=presenter_mock)
+
+        # Assert
+        adapter_mock.assert_called_once()
+        storage_mock.is_user_admin.assert_called_once_with(user_id=user_id)
+        storage_mock.remove_roles_for_user.assert_called_once_with(
+            user_id=user_id)
+        storage_mock.get_role_objs_ids.assert_called_once_with(
+            role_ids=role_ids)
+        storage_mock.add_roles_to_the_user.assert_called_once_with(
+            user_id=user_id, role_ids=ids_of_role_objects)
         storage_mock.update_user_name.assert_called_once_with(user_id=user_id,
                                                               name=name)
         presenter_mock.get_success_response_for_update_user_profile \
