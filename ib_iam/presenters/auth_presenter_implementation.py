35d2b79ceecb36d318a4fde05092053fe269e644
DurgaPrime <durgaprasad@ibhubs.co>
2020-08-18 14:55:07 +0530

    presenter implementation and test cases for create user account presenter implementation

--- ib_iam/presenters/auth_presenter_implementation.py
+++ ib_iam/presenters/auth_presenter_implementation.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from ib_iam.adapters.auth_service import UserTokensDTO
 from ib_iam.constants.enums import StatusCode
 from ib_iam.interactors.presenter_interfaces.auth_presenter_interface import \
-    AuthPresenterInterface
+    AuthPresenterInterface, CreateUserAccountPresenterInterface
 
 INVALID_EMAIL = (
     "Please send valid email",
@@ -146,3 +146,88 @@ class AuthPresenterImplementation(AuthPresenterInterface, HTTPResponseMixin):
     def get_success_response_for_reset_password_link_to_user_email(self) \
             -> HttpResponse:
         return self.prepare_200_success_response(response_dict={})
+
+
+ACCOUNT_ALREADY_EXISTS = (
+    "The given email has already account, try with another email",
+    "ACCOUNT_ALREADY_EXISTS"
+)
+
+PASSWORD_DOES_NOT_MATCH_CRITERIA = (
+    'not a valid password, try with valid password',
+    "PASSWORD_DOES_NOT_MATCH_CRITERIA"
+)
+
+INVALID_DOMAIN = (
+    "Currently, you can sign up to the portal only with iB Hubs and related companies email IDs",
+    "INVALID_DOMAIN"
+)
+
+
+class CreateUserAccountPresenterImplementation(
+    CreateUserAccountPresenterInterface, HTTPResponseMixin):
+    def raise_account_already_exists_exception(self):
+        response_dict = {
+            "response": ACCOUNT_ALREADY_EXISTS[0],
+            "http_status_code": StatusCode.BAD_REQUEST.value,
+            "res_status": ACCOUNT_ALREADY_EXISTS[1]
+        }
+        return self.prepare_400_bad_request_response(
+            response_dict=response_dict)
+
+    def raise_password_not_matched_with_criteria_exception(self):
+        response_dict = {
+            "response": PASSWORD_DOES_NOT_MATCH_CRITERIA[0],
+            "http_status_code": StatusCode.BAD_REQUEST.value,
+            "res_status": PASSWORD_DOES_NOT_MATCH_CRITERIA[1]
+        }
+        return self.prepare_400_bad_request_response(
+            response_dict=response_dict)
+
+    def raise_invalid_email_exception(self):
+        response_dict = {
+            "response": INVALID_EMAIL[0],
+            "http_status_code": StatusCode.BAD_REQUEST.value,
+            "res_status": INVALID_EMAIL[1]
+        }
+        return self.prepare_400_bad_request_response(
+            response_dict=response_dict)
+
+    def raise_invalid_domain_exception(self):
+        response_dict = {
+            "response": INVALID_DOMAIN[0],
+            "http_status_code": StatusCode.BAD_REQUEST.value,
+            "res_status": INVALID_DOMAIN[1]
+        }
+        return self.prepare_400_bad_request_response(
+            response_dict=response_dict)
+
+    def get_response_for_create_user_account(self):
+        return self.prepare_200_success_response(response_dict={})
+
+    def raise_invalid_name_length_exception(self):
+        from ib_iam.constants.exception_messages import \
+            INVALID_NAME_LENGTH
+        from ib_iam.constants.config import MINIMUM_USER_NAME_LENGTH
+        response_dict = {
+            "response": INVALID_NAME_LENGTH[0].format(
+                minimum_name_length=MINIMUM_USER_NAME_LENGTH
+            ),
+            "http_status_code": StatusCode.BAD_REQUEST.value,
+            "res_status": INVALID_NAME_LENGTH[1]
+        }
+        return self.prepare_400_bad_request_response(
+            response_dict=response_dict)
+
+    def raise_name_should_not_contain_special_characters_exception(self):
+        from ib_iam.constants.exception_messages import \
+            NAME_SHOULD_NOT_CONTAIN_SPECIAL_CHARACTERS_AND_NUMBERS
+        response_dict = {
+            "response":
+                NAME_SHOULD_NOT_CONTAIN_SPECIAL_CHARACTERS_AND_NUMBERS[0],
+            "http_status_code": StatusCode.BAD_REQUEST.value,
+            "res_status":
+                NAME_SHOULD_NOT_CONTAIN_SPECIAL_CHARACTERS_AND_NUMBERS[1]
+        }
+        return self.prepare_400_bad_request_response(
+            response_dict=response_dict)
