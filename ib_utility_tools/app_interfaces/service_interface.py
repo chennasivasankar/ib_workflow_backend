61cf5f30127641c4e71616ea537717b6b0ec1f03
Your Name <jaswanth.mamidipudi@ibhubs.co.in>
2020-08-19 18:14:48 +0530

    Add service method for get timers bulk

---
+++ ib_utility_tools/app_interfaces/service_interface.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from ib_utility_tools.interactors.storage_interfaces.dtos import TimerEntityDTO
+
+
+class ServiceInterface:
+
+    @staticmethod
+    def get_bulk_timer_details_for_given_entities(
+            timer_entity_dtos: List[TimerEntityDTO]):
+        from ib_utility_tools.storages.timer_storage_implementation import \
+            TimerStorageImplementation
+        timer_storage = TimerStorageImplementation()
+
+        from ib_utility_tools.interactors.get_timers_bulk_interactor import \
+            GetTimersBulkInteractor
+        interactor = GetTimersBulkInteractor(timer_storage=timer_storage)
+        timer_details_dtos = interactor.get_timers_bulk(
+            timer_entity_dtos=timer_entity_dtos)
+        return timer_details_dtos
