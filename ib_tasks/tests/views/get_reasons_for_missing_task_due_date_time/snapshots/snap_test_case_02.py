aaad0f24fe634e74cf674992ea7eb2591afbdc03
geetha.k <geetha.k@ibhubs.co.in>
2020-08-20 10:38:10 +0530

    fix test cases for add reason for task due miss

--- ib_tasks/tests/views/get_reasons_for_missing_task_due_date_time/snapshots/snap_test_case_02.py
+++ ib_tasks/tests/views/get_reasons_for_missing_task_due_date_time/snapshots/snap_test_case_02.py
@@ -14,18 +14,18 @@ snapshots['TestCase02GetReasonsForMissingTaskDueDateTimeAPITestCase.test_case bo
         'due_date_time': '2020-08-11 14:19:33',
         'due_missed_count': 1,
         'reason': 'Missed reiterating objective',
-        'task_id': 'iBWF-0',
+        'task_id': 'IBWF-1',
         'user': {
             'name': 'name_0',
             'profile_pic': 'https://www.google.com/search?q=ibhubs&client=ubuntu&hs=DI7&channel=fs&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjZqYjthYfrAhUF4zgGHevjDZUQ_AUoA3oECAsQBQ&biw=1848&bih=913#imgrc=Kg3TRY0jmx3udM',
-            'user_id': '5ce65234-c0b9-4d4a-8785-88aa2d39bd2f'
+            'user_id': '881148bd-72c9-4095-b3af-a5217cc58de2'
         }
     },
     {
         'due_date_time': '2020-08-11 14:19:33',
         'due_missed_count': 2,
         'reason': 'wrong estimation of time',
-        'task_id': 'iBWF-0',
+        'task_id': 'IBWF-1',
         'user': {
             'name': 'name_1',
             'profile_pic': 'https://www.google.com/search?q=ibhubs&client=ubuntu&hs=DI7&channel=fs&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjZqYjthYfrAhUF4zgGHevjDZUQ_AUoA3oECAsQBQ&biw=1848&bih=913#imgrc=Kg3TRY0jmx3udM',
@@ -36,7 +36,7 @@ snapshots['TestCase02GetReasonsForMissingTaskDueDateTimeAPITestCase.test_case bo
         'due_date_time': '2020-08-11 14:19:33',
         'due_missed_count': 3,
         'reason': 'wrong estimation of time',
-        'task_id': 'iBWF-0',
+        'task_id': 'IBWF-1',
         'user': {
             'name': 'name_2',
             'profile_pic': 'https://www.google.com/search?q=ibhubs&client=ubuntu&hs=DI7&channel=fs&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjZqYjthYfrAhUF4zgGHevjDZUQ_AUoA3oECAsQBQ&biw=1848&bih=913#imgrc=Kg3TRY0jmx3udM',
@@ -47,7 +47,7 @@ snapshots['TestCase02GetReasonsForMissingTaskDueDateTimeAPITestCase.test_case bo
         'due_date_time': '2020-08-11 14:19:33',
         'due_missed_count': 4,
         'reason': 'wrong estimation of time',
-        'task_id': 'iBWF-0',
+        'task_id': 'IBWF-1',
         'user': {
             'name': 'name_3',
             'profile_pic': 'https://www.google.com/search?q=ibhubs&client=ubuntu&hs=DI7&channel=fs&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjZqYjthYfrAhUF4zgGHevjDZUQ_AUoA3oECAsQBQ&biw=1848&bih=913#imgrc=Kg3TRY0jmx3udM',
