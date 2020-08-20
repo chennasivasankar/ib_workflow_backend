a04dcff055d0d511e318e46b7f5421294bf06a9b
raviteja <raviteja.kotipalli@ibhubs.co.in>
2020-08-20 16:14:21 +0530

    add utility service interface and get entity id duration

--- ib_tasks/tests/interactors/test_get_task_stages_history.py
+++ ib_tasks/tests/interactors/test_get_task_stages_history.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 import pytest
 
 from ib_tasks.interactors.stages_dtos import TaskStageCompleteDetailsDTO
-from ib_tasks.tests.factories.adapter_dtos import AssigneeDetailsDTOFactory
+from ib_tasks.tests.factories.adapter_dtos import AssigneeDetailsDTOFactory, EntityTypeDTOFactory
 from ib_tasks.tests.factories.storage_dtos import TaskStageHistoryDTOFactory, LogDurationDTOFactory, \
     StageMinimalDTOFactory
 
@@ -43,12 +43,15 @@ class TestGetTaskStagesHistory:
             task_storage=task_storage, stage_storage=stage_storage
         )
         AssigneeDetailsDTOFactory.reset_sequence(1)
+        EntityTypeDTOFactory.reset_sequence(1)
         stage_dtos = StageMinimalDTOFactory.create_batch(1)
+        entity_dtos = EntityTypeDTOFactory.create_batch(1)
         stage_storage.get_stage_details.return_value = stage_dtos
         StageMinimalDTOFactory.reset_sequence(1)
         user_dtos = [
             AssigneeDetailsDTOFactory(assignee_id="1")
         ]
+        assignee_ids = ['1']
         task_display_id = "1"
         task_id = 1
         TaskStageHistoryDTOFactory.reset_sequence(1)
@@ -60,7 +63,7 @@ class TestGetTaskStagesHistory:
         task_stages = [task_stage_1]
         log_dtos = LogDurationDTOFactory.create_batch(1)
         stage_storage.get_task_stage_dtos.return_value = task_stages
-        path1 = 'ib_tasks.adapters.assignees_details_service.AssigneeDetailsService.get_log_duration_dtos'
+        path1 = 'ib_tasks.adapters.utility_tools_service.UtilityToolsService.get_log_duration_dtos'
         log_mock = mocker.patch(path1)
         log_mock.return_value = log_dtos
         path2 = 'ib_tasks.adapters.assignees_details_service.AssigneeDetailsService.get_assignees_details_dtos'
@@ -81,8 +84,8 @@ class TestGetTaskStagesHistory:
         # Assert
         task_storage.check_is_task_exists\
             .assert_called_once_with(task_id=task_id)
-        log_mock.called_once()
-        user_mock_mock.called_once()
+        log_mock.assert_called_once_with(entity_dtos=entity_dtos)
+        user_mock_mock.assert_called_once_with(assignee_ids=assignee_ids)
         presenter.get_task_stages_history_response.assert_called_once_with(
             task_stages_details_dto=task_stage_details_dto
         )
@@ -97,9 +100,12 @@ class TestGetTaskStagesHistory:
             task_storage=task_storage, stage_storage=stage_storage
         )
         AssigneeDetailsDTOFactory.reset_sequence(1)
+        EntityTypeDTOFactory.reset_sequence(1)
+        entity_dtos = EntityTypeDTOFactory.create_batch(1)
         user_dtos = [
             AssigneeDetailsDTOFactory(assignee_id="1")
         ]
+        assignee_ids = ['1']
         AssigneeDetailsDTOFactory.reset_sequence(1)
         stage_dtos = StageMinimalDTOFactory.create_batch(1)
         stage_storage.get_stage_details.return_value = stage_dtos
@@ -116,7 +122,7 @@ class TestGetTaskStagesHistory:
         task_stages = [task_stage_1]
         log_dtos = LogDurationDTOFactory.create_batch(1)
         stage_storage.get_task_stage_dtos.return_value = task_stages
-        path1 = 'ib_tasks.adapters.assignees_details_service.AssigneeDetailsService.get_log_duration_dtos'
+        path1 = 'ib_tasks.adapters.utility_tools_service.UtilityToolsService.get_log_duration_dtos'
         log_mock = mocker.patch(path1)
         log_mock.return_value = log_dtos
         path2 = 'ib_tasks.adapters.assignees_details_service.AssigneeDetailsService.get_assignees_details_dtos'
@@ -140,8 +146,8 @@ class TestGetTaskStagesHistory:
         # Assert
         task_storage.check_is_task_exists\
             .assert_called_once_with(task_id=task_id)
-        log_mock.called_once()
-        user_mock_mock.called_once()
+        log_mock.assert_called_once_with(entity_dtos=entity_dtos)
+        user_mock_mock.assert_called_once_with(assignee_ids=assignee_ids)
         presenter.get_task_stages_history_response.assert_called_once_with(
             task_stages_details_dto=task_stage_details_dto
         )
