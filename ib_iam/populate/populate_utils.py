de31f5cc1834364ecc68b11191f0e4b2e528b040
Vedavidh Budimuri <vedavidhbudimuri@gmail.com>
2020-08-21 16:27:56 +0530

    added elasticsearch populate function

--- ib_iam/populate/populate_utils.py
+++ ib_iam/populate/populate_utils.py
@@ -25,6 +25,9 @@ def populate(spread_sheet_name: str):
     #role.add_roles_details_to_database(spread_sheet_name, ROLES_SUBSHEET_NAME)
     populate_test_users()
 
+    from ib_iam.populate.populate_elastic_search_data import populate_data
+    populate_data()
+
 
 def populate_admin_users():
     admin_users = [
