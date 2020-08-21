28198c59ab6d5d68eea876c0dfa5ed54a172c886
Your Name <jaswanth.mamidipudi@ibhubs.co.in>
2020-08-21 20:40:45 +0530

    Modify controller test of get teams success case to check sorting of teams with respect to team names and updated its snapshot

--- ib_iam/tests/views/get_list_of_teams/snapshots/snap_test_case_01.py
+++ ib_iam/tests/views/get_list_of_teams/snapshots/snap_test_case_01.py
@@ -12,29 +12,6 @@ snapshots['TestCase01GetListOfTeamsAPITestCase.test_case status_code'] = '200'
 snapshots['TestCase01GetListOfTeamsAPITestCase.test_case body'] = {
     'teams': [
         {
-            'description': 'team_description 1',
-            'members': [
-                {
-                    'member_id': '2bdb417e-4632-419a-8ddd-085ea272c6eb',
-                    'name': 'name1',
-                    'profile_pic_url': 'url1'
-                },
-                {
-                    'member_id': '548a803c-7b48-47ba-a700-24f2ea0d1280',
-                    'name': 'name2',
-                    'profile_pic_url': 'url2'
-                },
-                {
-                    'member_id': '4b8fb6eb-fa7d-47c1-8726-cd917901104e',
-                    'name': 'name3',
-                    'profile_pic_url': 'url3'
-                }
-            ],
-            'name': 'team 1',
-            'no_of_members': 3,
-            'team_id': 'f2c02d98-f311-4ab2-8673-3daa00757002'
-        },
-        {
             'description': 'team_description 2',
             'members': [
                 {
@@ -48,7 +25,7 @@ snapshots['TestCase01GetListOfTeamsAPITestCase.test_case body'] = {
                     'profile_pic_url': 'url4'
                 }
             ],
-            'name': 'team 2',
+            'name': 'Arogya',
             'no_of_members': 2,
             'team_id': 'aa66c40f-6d93-484a-b418-984716514c7b'
         },
@@ -71,9 +48,32 @@ snapshots['TestCase01GetListOfTeamsAPITestCase.test_case body'] = {
                     'profile_pic_url': 'url4'
                 }
             ],
-            'name': 'team 3',
+            'name': 'Daru',
             'no_of_members': 3,
             'team_id': 'c982032b-53a7-4dfa-a627-4701a5230765'
+        },
+        {
+            'description': 'team_description 1',
+            'members': [
+                {
+                    'member_id': '2bdb417e-4632-419a-8ddd-085ea272c6eb',
+                    'name': 'name1',
+                    'profile_pic_url': 'url1'
+                },
+                {
+                    'member_id': '548a803c-7b48-47ba-a700-24f2ea0d1280',
+                    'name': 'name2',
+                    'profile_pic_url': 'url2'
+                },
+                {
+                    'member_id': '4b8fb6eb-fa7d-47c1-8726-cd917901104e',
+                    'name': 'name3',
+                    'profile_pic_url': 'url3'
+                }
+            ],
+            'name': 'Proyuga',
+            'no_of_members': 3,
+            'team_id': 'f2c02d98-f311-4ab2-8673-3daa00757002'
         }
     ],
     'total_teams_count': 3
--- ib_iam/tests/views/get_list_of_teams/test_case_01.py
+++ ib_iam/tests/views/get_list_of_teams/test_case_01.py
@@ -57,6 +57,7 @@ class TestCase01GetListOfTeamsAPITestCase(TestUtils):
         teams = [
             {
                 "team_id": "f2c02d98-f311-4ab2-8673-3daa00757002",
+                "name": "Proyuga",
                 "user_ids": [
                     '2bdb417e-4632-419a-8ddd-085ea272c6eb',
                     '548a803c-7b48-47ba-a700-24f2ea0d1280',
@@ -65,6 +66,7 @@ class TestCase01GetListOfTeamsAPITestCase(TestUtils):
             },
             {
                 "team_id": "aa66c40f-6d93-484a-b418-984716514c7b",
+                "name": "Arogya",
                 "user_ids": [
                     '2bdb417e-4632-419a-8ddd-085ea272c6eb',
                     '7ee2c7b4-34c8-4d65-a83a-f87da75db24e'
@@ -72,6 +74,7 @@ class TestCase01GetListOfTeamsAPITestCase(TestUtils):
             },
             {
                 "team_id": "c982032b-53a7-4dfa-a627-4701a5230765",
+                "name": "Daru",
                 "user_ids": [
                     '548a803c-7b48-47ba-a700-24f2ea0d1280',
                     '4b8fb6eb-fa7d-47c1-8726-cd917901104e',
@@ -81,6 +84,7 @@ class TestCase01GetListOfTeamsAPITestCase(TestUtils):
             }
         ]
         for team in teams:
-            team_object = TeamFactory(team_id=team["team_id"])
+            team_object = TeamFactory(team_id=team["team_id"],
+                                      name=team["name"])
             for user_id in team["user_ids"]:
                 UserTeamFactory(team=team_object, user_id=user_id)
