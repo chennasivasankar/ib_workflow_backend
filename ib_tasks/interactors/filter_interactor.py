5b0e462e6300ec91a161e2b00508f05bfe7d8868
pavankumar <pavankumar.pamuru@ibhubs.com.in>
2020-08-21 14:55:01 +0530

    filters bug fixed

--- ib_tasks/interactors/filter_interactor.py
+++ ib_tasks/interactors/filter_interactor.py
@@ -170,14 +170,6 @@ class FilterInteractor:
         )
         return response
 
-    @staticmethod
-    def _get_filter_ids(filters_dto: List[FilterDTO]):
-
-        return [
-            filter_dto.filter_id
-            for filter_dto in filters_dto
-        ]
-
     def _validate_filter_data(
             self, filter_dto: CreateFilterDTO,
             condition_dtos: List[CreateConditionDTO]):
@@ -195,14 +187,48 @@ class FilterInteractor:
         ]
         if invalid_field_ids:
             raise FieldIdsNotBelongsToTemplateId(field_ids=field_ids)
+        from ib_tasks.adapters.service_adapter import get_service_adapter
+        service_adapter = get_service_adapter()
+        user_roles = service_adapter.roles_service.get_user_role_ids(
+            user_id=filter_dto.user_id
+        )
+        self.filter_storage.validate_user_roles_with_field_ids_roles(
+            user_roles=user_roles, field_ids=field_ids
+        )
 
-        interactor = UserRoleValidationInteractor()
-        permitted_field_ids = interactor.get_field_ids_having_write_permission_for_user(
-            field_storage=self.field_storage,
-            user_id=filter_dto.user_id,
-            field_ids=field_ids)
-        if not set(valid_field_ids).issubset(permitted_field_ids):
-            raise UserNotHaveAccessToFields
+    @staticmethod
+    def _get_filter_ids(filters_dto: List[FilterDTO]):
+
+        return [
+            filter_dto.filter_id
+            for filter_dto in filters_dto
+        ]
+
+    # def _validate_filter_data(
+    #         self, filter_dto: CreateFilterDTO,
+    #         condition_dtos: List[CreateConditionDTO]):
+    #     template_id = filter_dto.template_id
+    #     field_ids = [condition_dto.field_id for condition_dto in condition_dtos]
+    #     self.filter_storage.validate_template_id(
+    #         template_id=filter_dto.template_id
+    #     )
+    #     valid_field_ids = self.filter_storage.get_field_ids_for_task_template(
+    #         template_id=template_id, field_ids=field_ids
+    #     )
+    #     invalid_field_ids = [
+    #         invalid_field_id for invalid_field_id in field_ids
+    #         if invalid_field_id not in valid_field_ids
+    #     ]
+    #     if invalid_field_ids:
+    #         raise FieldIdsNotBelongsToTemplateId(field_ids=field_ids)
+    #
+    #     interactor = UserRoleValidationInteractor()
+    #     permitted_field_ids = interactor.get_field_ids_having_write_permission_for_user(
+    #         field_storage=self.field_storage,
+    #         user_id=filter_dto.user_id,
+    #         field_ids=field_ids)
+    #     if not set(valid_field_ids).issubset(permitted_field_ids):
+    #         raise UserNotHaveAccessToFields
 
     def _validate_filter_id(self, filter_id: int):
         self.filter_storage.validate_filter_id(
