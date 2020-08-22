62a1b362e47d8d39e8a8753e04813538ed8576a3
DurgaPrime <durgaprasad@ibhubs.co>
2020-08-22 14:31:52 +0530

    add migrations and tested

--- ib_iam/models/project.py
+++ ib_iam/models/project.py
@@ -1,17 +1,11 @@
-import uuid
-
 from django.db import models
 
 
-def generate_uuid4():
-    return uuid.uuid4()
-
-
 class Project(models.Model):
-    project_id = models.UUIDField(
-        default=generate_uuid4,
+    project_id = models.CharField(
         editable=False,
-        primary_key=True
+        primary_key=True,
+        max_length=100
     )
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(null=True, blank=True)
