81b857213322265469d59aa1eeeeb97fe9455c96
Sunny <sanyasirao@ibhubs.co>
2020-08-22 12:33:55 +0530

    Update spec

--- ib_tasks/views/get_task_templates/request_response_mocks.py
+++ ib_tasks/views/get_task_templates/request_response_mocks.py
@@ -2,56 +2,47 @@
 
 
 RESPONSE_200_JSON = """
-{
-    "projects": [
-        {
-            "project_id": "string",
-            "project_name": "string",
-            "project_logo_url": "string"
-        }
-    ],
-    "task_templates": [
-        {
-            "template_id": "string",
-            "project_id": "string",
-            "template_name": "string",
-            "actions": [
-                {
-                    "action_id": 1,
-                    "action_type": "NO_VALIDATIONS",
-                    "transition_template_id": "string",
-                    "button_text": "string",
-                    "button_color": "string"
-                }
-            ],
-            "group_of_fields": [
-                {
-                    "gof_id": "string",
-                    "gof_display_name": "string",
-                    "max_columns": 1,
-                    "order": 1,
-                    "enable_add_another": true,
-                    "fields": [
-                        {
-                            "field_type": "PLAIN_TEXT",
-                            "field_id": "string",
-                            "display_name": "string",
-                            "is_field_required": true,
-                            "field_values": "string",
-                            "allowed_formats": "string",
-                            "validation_regex": "string",
-                            "error_msg": "string",
-                            "tooltip": "string",
-                            "help_text": "string",
-                            "placeholder_text": "string",
-                            "is_field_writable": true
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-}
+[
+    {
+        "template_id": "string",
+        "project_id": "string",
+        "template_name": "string",
+        "actions": [
+            {
+                "action_id": 1,
+                "action_type": "NO_VALIDATIONS",
+                "transition_template_id": "string",
+                "button_text": "string",
+                "button_color": "string"
+            }
+        ],
+        "group_of_fields": [
+            {
+                "gof_id": "string",
+                "gof_display_name": "string",
+                "max_columns": 1,
+                "order": 1,
+                "enable_add_another": true,
+                "fields": [
+                    {
+                        "field_type": "PLAIN_TEXT",
+                        "field_id": "string",
+                        "display_name": "string",
+                        "is_field_required": true,
+                        "field_values": "string",
+                        "allowed_formats": "string",
+                        "validation_regex": "string",
+                        "error_msg": "string",
+                        "tooltip": "string",
+                        "help_text": "string",
+                        "placeholder_text": "string",
+                        "is_field_writable": true
+                    }
+                ]
+            }
+        ]
+    }
+]
 """
 
 RESPONSE_404_JSON = """
