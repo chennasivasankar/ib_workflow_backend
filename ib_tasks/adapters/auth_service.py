ee22b4cea8d55fd999e2c6cbd105a9b7399ceccb
pavankumar <pavankumar.pamuru@ibhubs.com.in>
2020-08-25 14:29:37 +0530

    change in Operators LE to LT

--- ib_tasks/adapters/auth_service.py
+++ ib_tasks/adapters/auth_service.py
@@ -93,14 +93,14 @@ class AuthService:
 
     def validate_project_ids(self, project_ids: List[str]) -> \
             List[str]:
-        return project_ids
+        raise NotImplementedError
 
     def get_team_details(self, team_ids: List[str]) -> List[TeamDetailsDTO]:
         raise NotImplementedError
 
     def get_projects_info_for_given_ids(self, project_ids: List[str]) -> List[
         ProjectDetailsDTO]:
-        return project_ids
+        raise NotImplementedError
 
     def get_team_info_for_given_user_ids(self, user_ids: List[str],
                                          project_id: str) -> List[
