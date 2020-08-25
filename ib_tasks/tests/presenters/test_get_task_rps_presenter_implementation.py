8af913dc661e4cfa6c3461e2eb1b935231488a05
geetha.k <geetha.k@ibhubs.co.in>
2020-08-25 16:19:24 +0530

    presenter implementation for get task rps details

---
+++ ib_tasks/tests/presenters/test_get_task_rps_presenter_implementation.py
@@ -0,0 +1,52 @@
+from ib_tasks.presenters.get_task_rps_presenter_implementation import \
+    GetTaskRpsPresenterImplementation
+
+
+class TestGetRps:
+    def test_response_invalid_task_id(self, snapshot):
+        # Arrange
+        presenter = GetTaskRpsPresenterImplementation()
+        from ib_tasks.exceptions.task_custom_exceptions \
+            import InvalidTaskDisplayId
+        task_id = "iBWF-10"
+
+        err = InvalidTaskDisplayId(task_id)
+
+        # Act
+        response_object = presenter.response_for_invalid_task_id(err)
+
+        # Assert
+        snapshot.assert_match(response_object.content, "response")
+
+    def test_response_for_user_is_not_assignee_for_task(self, snapshot):
+        # Arrange
+        presenter = GetTaskRpsPresenterImplementation()
+
+        # Act
+        response_object = presenter.response_for_user_is_not_assignee_for_task()
+
+        # Assert
+        snapshot.assert_match(response_object.content, "response")
+
+    def test_response_for_invalid_stage_id(self, snapshot):
+        # Arrange
+        presenter = GetTaskRpsPresenterImplementation()
+
+        # Act
+        response_object = presenter.response_for_invalid_stage_id()
+
+        # Assert
+        snapshot.assert_match(response_object.content, "response")
+
+    def test_response_for_get_task_rps_details(self, snapshot):
+        # Arrange
+        from ib_tasks.tests.factories.interactor_dtos import UserDetailsDTOFactory
+        UserDetailsDTOFactory.reset_sequence()
+        rp_dtos = UserDetailsDTOFactory.create_batch(size=4)
+        presenter = GetTaskRpsPresenterImplementation()
+
+        # Act
+        response_object = presenter.response_for_get_rps_details(rp_dtos)
+
+        # Assert
+        snapshot.assert_match(response_object.content, "response")
--- ib_tasks/views/get_task_rps_in_given_stage/api_wrapper.py
+++ ib_tasks/views/get_task_rps_in_given_stage/api_wrapper.py
@@ -2,47 +2,32 @@ from django_swagger_utils.drf_server.utils.decorator.interface_decorator \
     import validate_decorator
 from .validator_class import ValidatorClass
 
+from ib_tasks.storages.storage_implementation import StorageImplementation
+
 
 @validate_decorator(validator_class=ValidatorClass)
 def api_wrapper(*args, **kwargs):
-    # ---------MOCK IMPLEMENTATION---------
+    user = kwargs['user']
+    request_params = kwargs['request_data']
+    task_id = request_params['task_id']
+    stage_id = request_params['stage_id']
 
-    try:
-        from ib_tasks.views.get_task_rps_in_given_stage.request_response_mocks \
-            import REQUEST_BODY_JSON
-        body = REQUEST_BODY_JSON
-    except ImportError:
-        body = {}
+    from ib_tasks.interactors.task_dtos import GetTaskRPsParametersDTO
+    parameters = GetTaskRPsParametersDTO(
+        task_id=task_id,
+        user_id=user.user_id,
+        stage_id=stage_id
+    )
+    storage = StorageImplementation()
+    from ib_tasks.storages.tasks_storage_implementation import \
+        TasksStorageImplementation
+    task_storage = TasksStorageImplementation()
+    from ib_tasks.presenters.get_task_rps_presenter_implementation import \
+        GetTaskRpsPresenterImplementation
+    presenter = GetTaskRpsPresenterImplementation()
+    from ib_tasks.interactors.get_task_related_rps_in_given_stage import \
+        GetTaskRPsInteractor
+    interactor = GetTaskRPsInteractor(storage=storage, task_storage=task_storage)
+    response = interactor.get_task_rps_wrapper(presenter, parameters)
 
-    test_case = {
-        "path_params": {},
-        "query_params": {},
-        "header_params": {},
-        "body": body,
-        "securities": [{'oauth': ['read']}]
-    }
-
-    from django_swagger_utils.drf_server.utils.server_gen.mock_response \
-        import mock_response
-    try:
-        response = ''
-        status_code = 200
-        if '200' in ['200', '404', '403']:
-            from ib_tasks.views.get_task_rps_in_given_stage.request_response_mocks \
-                import RESPONSE_200_JSON
-            response = RESPONSE_200_JSON
-            status_code = 200
-        elif '201' in ['200', '404', '403']:
-            from ib_tasks.views.get_task_rps_in_given_stage.request_response_mocks \
-                import RESPONSE_201_JSON
-            response = RESPONSE_201_JSON
-            status_code = 201
-    except ImportError:
-        response = ''
-        status_code = 200
-    response_tuple = mock_response(
-        app_name="ib_tasks", test_case=test_case,
-        operation_name="get_task_rps_in_given_stage",
-        kwargs=kwargs, default_response_body=response,
-        group_name="", status_code=status_code)
-    return response_tuple
+    return response
