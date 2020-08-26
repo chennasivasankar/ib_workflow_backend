287a1a12637f873ba30bf6bc3aa6e05a7faf5196
geetha.k <geetha.k@ibhubs.co.in>
2020-08-26 10:44:31 +0530

    test cases

--- ib_tasks/tests/storages/test_get_task_due_missing_details.py
+++ ib_tasks/tests/storages/test_get_task_due_missing_details.py
@@ -3,7 +3,7 @@ from freezegun import freeze_time
 
 from ib_tasks.storages.storage_implementation import StorageImplementation
 from ib_tasks.tests.factories.models import TaskFactory, TaskDueDetailsFactory, \
-    TaskStageHistoryModelFactory
+    TaskStageHistoryModelFactory, StageModelFactory
 from ib_tasks.tests.factories.models import TaskStageModelFactory
 
 
@@ -14,13 +14,15 @@ class TestGetTaskDueMissingDetails:
     @freeze_time("2020-08-10 12:30:56")
     def populate_data(self):
         TaskFactory.reset_sequence()
+        StageModelFactory.reset_sequence()
+        stage = StageModelFactory()
         tasks = TaskFactory.create_batch(size=3)
         TaskStageModelFactory.reset_sequence()
         TaskStageModelFactory(task=tasks[0])
         TaskDueDetailsFactory.reset_sequence()
         TaskStageHistoryModelFactory.reset_sequence()
-        TaskStageHistoryModelFactory.create_batch(size=5, task=tasks[0])
-        TaskDueDetailsFactory.create_batch(task=tasks[0], size=2, count=1, stage_id=1)
+        TaskStageHistoryModelFactory.create_batch(size=5, task=tasks[0], stage=stage)
+        TaskDueDetailsFactory.create_batch(task=tasks[0], size=2, count=1, stage=stage)
         TaskDueDetailsFactory.create_batch(task=tasks[0], size=2, reason_id=-1, count=1)
 
     def test_validate_task_id_given_valid_task_id(self, populate_data):
@@ -52,7 +54,7 @@ class TestGetTaskDueMissingDetails:
         # Arrange
         task_id = 1
         user_id = "123e4567-e89b-12d3-a456-426614174000"
-        stage_id = "stage_id_1"
+        stage_id = "stage_id_0"
         storage = StorageImplementation()
         expected_response = True
 
@@ -83,7 +85,7 @@ class TestGetTaskDueMissingDetails:
     def test_get_due_details_of_task_given_task_id(self, populate_data, snapshot):
         # Arrange
         task_id = 1
-        stage_id = "stage_id_1"
+        stage_id = "stage_id_0"
         storage = StorageImplementation()
 
         # Act
