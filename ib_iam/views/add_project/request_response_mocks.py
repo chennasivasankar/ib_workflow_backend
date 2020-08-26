346c3b1a2ab6625bcb323a91339a982a1b6c866e
Your Name <jaswanth.mamidipudi@ibhubs.co.in>
2020-08-26 14:09:50 +0530

    build

--- ib_iam/views/add_project/request_response_mocks.py
+++ ib_iam/views/add_project/request_response_mocks.py
@@ -14,7 +14,7 @@ REQUEST_BODY_JSON = """
 
 RESPONSE_201_JSON = """
 {
-    "project_id": "4c1552ab-6138-4bdf-8d2b-4798344b82c2"
+    "project_id": "string"
 }
 """
 
--- ib_iam/views/get_projects/request_response_mocks.py
+++ ib_iam/views/get_projects/request_response_mocks.py
@@ -6,14 +6,20 @@ RESPONSE_200_JSON = """
     "total_projects_count": 1,
     "projects": [
         {
-            "project_id": "string",
             "name": "string",
             "description": "string",
             "logo_url": "string",
+            "project_id": "string",
             "teams": [
                 {
                     "team_id": "89d96f4b-c19d-4e69-8eae-e818f3123b09",
                     "team_name": "string"
+                }
+            ],
+            "roles": [
+                {
+                    "role_id": "string",
+                    "role_name": "string"
                 }
             ]
         }
