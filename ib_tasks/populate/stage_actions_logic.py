334fe9d96fdfc992cd5a25f3ff957e174f4e0f3f
Ubuntu <ubuntu@ip-10-0-0-237.ap-south-1.compute.internal>
2020-08-27 13:50:41 +0000

    Load otg data

--- ib_tasks/populate/stage_actions_logic.py
+++ ib_tasks/populate/stage_actions_logic.py
@@ -1,79 +1,144 @@
-def VENDOR_DRAFTS_Save_Draft(task_dict, global_constants, stage_value_dict):
-    task_dict["status_variables"]["Status1"] = "VENDOR_DRAFTS"
+def EMP_DRAFTS_Save_Draft(task_dict, global_constants, stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "EMP_DRAFTS"
     return task_dict
 
 
-def VENDOR_DRAFTS_Submit_(task_dict, global_constants, stage_value_dict):
-    task_dict["status_variables"]["Status1"] = "VENDOR_PENDING_RP_APPROVAL"
+def EMP_DRAFTS_Submit_for_Enrolling(task_dict, global_constants,
+                                    stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "EMP_PENDING_SELECTION_PROCESS"
     return task_dict
 
 
-def VENDOR_DRAFTS_Discard(task_dict, global_constants, stage_value_dict):
-    task_dict["status_variables"]["Status1"] = "VENDOR_DISCARDED"
+def EMP_PENDING_BACKGROUND_CHECK_Cleared(task_dict, global_constants,
+                                         stage_value_dict):
+    task_dict["status_variables"][
+        "Status1"] = "PENDING_EMPLOYEE_ALIGNMENT_VALIDATION"
     return task_dict
 
 
-def VENDOR_PENDING_RP_APPROVAL_Reject(task_dict, global_constants,
-                                      stage_value_dict):
-    task_dict["status_variables"]["Status1"] = "VENDOR_REJECTED"
+def EMP_PENDING_BACKGROUND_CHECK_Not_Cleared(task_dict, global_constants,
+                                             stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "EMP_REJECTED"
     return task_dict
 
 
-def VENDOR_PENDING_RP_APPROVAL_Approve(task_dict, global_constants,
+def EMP_PENDING_FINAL_SELECTION_Reject(task_dict, global_constants,
                                        stage_value_dict):
-    task_dict["status_variables"][
-        "Status1"] = "VENDOR_PENDING_PAYMENTS_LEVEL5_VERIFICATION"
-    task_dict["status_variables"][
-        "Status2"] = "VENDOR_PENDING_PAYMENTS_LEVEL4_VERIFICATION"
-    task_dict["status_variables"][
-        "Status3"] = "VENDOR_PENDING_ACCOUNTS_LEVEL5_VERIFICATION"
-
+    task_dict["status_variables"]["Status1"] = "EMP_REJECTED"
     return task_dict
 
 
-def VENDOR_PENDING_PAYMENTS_LEVEL5_VERIFICATION_Verify(task_dict,
-                                                       global_constants,
-                                                       stage_value_dict):
+def EMP_PENDING_FINAL_SELECTION_Selected(task_dict, global_constants,
+                                         stage_value_dict):
     task_dict["status_variables"][
-        "Status1"] = "VENDOR_PAYMENTS_LEVEL5_VERIFICATION_COMPLETED"
-
-    if task_dict["status_variables"][
-            "Status2"] == "VENDOR_PAYMENTS_LEVEL4_VERIFICATION_COMPLETED" and task_dict[
-                "status_variables"][
-                    "Status3"] == "VENDOR_ACCOUNTS_LEVEL5_VERIFICATION_COMPLETED":
-        task_dict["status_variables"][
-            "Status4"] = "VENDOR_VERIFICATION_COMPLETED"
-
+        "Status1"] = "EMP_PENDING_ONBOARDING_SIGNING_AGREEMENTS"
     return task_dict
 
 
-def VENDOR_PENDING_PAYMENTS_LEVEL4_VERIFICATION_Verify(task_dict,
-                                                       global_constants,
-                                                       stage_value_dict):
+def EMP_PENDING_MENTOR_DISCUSSION_Interested(task_dict, global_constants,
+                                             stage_value_dict):
     task_dict["status_variables"][
-        "Status2"] = "VENDOR_PAYMENTS_LEVEL4_VERIFICATION_COMPLETED"
-
-    if task_dict["status_variables"][
-            "Status1"] == "VENDOR_PAYMENTS_LEVEL5_VERIFICATION_COMPLETED" and task_dict[
-                "status_variables"][
-                    "Status3"] == "VENDOR_ACCOUNTS_LEVEL5_VERIFICATION_COMPLETED":
-        task_dict["status_variables"][
-            "Status4"] = "VENDOR_VERIFICATION_COMPLETED"
+        "Status1"] = "EMP_PENDING_PAYMENT_INFORMATION"
     return task_dict
 
 
-def VENDOR_PENDING_ACCOUNTS_LEVEL5_VERIFICATION_Verify(task_dict,
-                                                       global_constants,
-                                                       stage_value_dict):
-    task_dict["status_variables"][
-        "Status3"] = "VENDOR_ACCOUNTS_LEVEL5_VERIFICATION_COMPLETED"
+def EMP_PENDING_MENTOR_DISCUSSION_Not_Interested(task_dict, global_constants,
+                                                 stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "EMP_REJECTED"
+    return task_dict
 
-    if task_dict["status_variables"][
-            "Status1"] == "VENDOR_PAYMENTS_LEVEL5_VERIFICATION_COMPLETED" and task_dict[
-                "status_variables"][
-                    "Status2"] == "VENDOR_PAYMENTS_LEVEL4_VERIFICATION_COMPLETED":
-        task_dict["status_variables"][
-            "Status4"] = "VENDOR_VERIFICATION_COMPLETED"
+
+def EMP_PENDING_ONBOARDING_SIGNING_AGREEMENTS_Submit(task_dict,
+                                                     global_constants,
+                                                     stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "EMP_PENDING_BACKGROUND_CHECK"
+    return task_dict
+
+
+def EMP_PENDING_PAYMENT_INFORMATION_Submit(task_dict, global_constants,
+                                           stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "EMP_PENDING_TRAINING"
+    return task_dict
+
+
+def EMP_PENDING_SELECTION_PROCESS_Reject(task_dict, global_constants,
+                                         stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "EMP_REJECTED"
+    return task_dict
+
+
+def EMP_PENDING_SELECTION_PROCESS_Selected(task_dict, global_constants,
+                                           stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "EMP_PENDING_MENTOR_DISCUSSION"
+    return task_dict
+
+
+def EMP_PENDING_TRAINING_Submit(task_dict, global_constants, stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "EMP_PENDING_FINAL_SELECTION"
+    return task_dict
+
+
+def PENDING_EMPLOYEE_ALIGNMENT_VALIDATION_Aligned(task_dict, global_constants,
+                                                  stage_value_dict):
+    task_dict["status_variables"][
+        "Status1"] = "PENDING_EMPLOYEE_ASSIGNMENT_COMPLETION"
+    return task_dict
+
+
+def PENDING_EMPLOYEE_ASSIGNMENT_COMPLETION_Submit(task_dict, global_constants,
+                                                  stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "PENDING_SALARY_DEPOSIT"
+    return task_dict
+
+
+def PENDING_EXTRA_ASSIGNMENT_COMPLETION_Submit(task_dict, global_constants,
+                                               stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "PENDING_LEARNINGS_FOR_WEEK"
+    return task_dict
+
+
+def PENDING_LEARNINGS_FOR_WEEK_Submit(task_dict, global_constants,
+                                      stage_value_dict):
+    task_dict["status_variables"][
+        "Status1"] = "PENDING_EMPLOYEE_ALIGNMENT_VALIDATION"
+    return task_dict
+
+
+def PENDING_SALARY_DEPOSIT_Submit(task_dict, global_constants,
+                                  stage_value_dict):
+    task_dict["status_variables"][
+        "Status1"] = "PENDING_WEEKLY_TARGET_COMPLETION"
+    return task_dict
+
+
+def PENDING_WEEKLY_TARGET_COMPLETION_Submit(task_dict, global_constants,
+                                            stage_value_dict):
+    task_dict["status_variables"][
+        "Status1"] = "PENDING_EXTRA_ASSIGNMENT_COMPLETION"
+    return task_dict
+
+
+def PR_CREATE_EMPLOYEE_FORM_Save_as_Draft(task_dict, global_constants,
+                                          stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "EMP_DRAFTS"
+    return task_dict
+
+
+def PR_CREATE_EMPLOYEE_FORM_Submit_for_Enrolling(task_dict, global_constants,
+                                                 stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "EMP_PENDING_SELECTION_PROCESS"
+    return task_dict
+
+
+def PR_CREATE_PAYMENT_REQUEST_Save_as_Draft(task_dict, global_constants,
+                                            stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "PR_PAYMENT_REQUEST_DRAFTS"
+    return task_dict
+
+
+def PR_CREATE_PAYMENT_REQUEST_Submit_for_Approval(task_dict, global_constants,
+                                                  stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "PR_PENDING_RP_APPROVAL"
     return task_dict
 
 
@@ -84,6 +149,24 @@ def PR_CREATE_VENDOR_Save_Draft(task_dict, global_constants, stage_value_dict):
 
 def PR_CREATE_VENDOR_Submit(task_dict, global_constants, stage_value_dict):
     task_dict["status_variables"]["Status1"] = "VENDOR_PENDING_RP_APPROVAL"
+    return task_dict
+
+
+def PR_NEED_CLARIFICATION_Discard(task_dict, global_constants,
+                                  stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "PR_DISCARDED"
+    return task_dict
+
+
+def PR_NEED_CLARIFICATION_Submit_for_Approval(task_dict, global_constants,
+                                              stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "PR_PENDING_RP_APPROVAL"
+    return task_dict
+
+
+def PR_PAYMENT_REQUEST_DRAFTS_Discard(task_dict, global_constants,
+                                      stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "PR_DISCARDED"
     return task_dict
 
 
@@ -99,55 +182,12 @@ def PR_PAYMENT_REQUEST_DRAFTS_Submit_for_Approval(task_dict, global_constants,
     return task_dict
 
 
-def PR_PAYMENT_REQUEST_DRAFTS_Discard(task_dict, global_constants,
-                                      stage_value_dict):
-    task_dict["status_variables"]["Status1"] = "PR_DISCARDED"
-    return task_dict
-
-
-def PR_PENDING_RP_APPROVAL_Need_Clarification(task_dict, global_constants,
-                                              stage_value_dict):
-    task_dict["status_variables"]["Status1"] = "PR_NEED_CLARIFICATION"
-    return task_dict
-
-
-def PR_PENDING_RP_APPROVAL_Reject(task_dict, global_constants,
-                                  stage_value_dict):
-    task_dict["status_variables"]["Status1"] = "PR_REJECTION_LIST"
-    return task_dict
-
-
-def PR_PENDING_RP_APPROVAL_Approve(task_dict, global_constants,
-                                   stage_value_dict):
-    task_dict["status_variables"][
-        "Status2"] = "PR_PENDING_PAYMENTS_LEVEL1_VERIFICATION"
-    task_dict["status_variables"][
-        "Status3"] = "PR_PENDING_PAYMENTS_LEVEL2_VERIFICATION"
-    task_dict["status_variables"][
-        "Status4"] = "PR_PENDING_PAYMENTS_LEVEL3_VERIFICATION"
-    task_dict["status_variables"][
-        "Status1"] = "PR_PENDING_ACCOUNTS_LEVEL1_VERIFICATION"
-    return task_dict
-
-
-def PR_NEED_CLARIFICATION_Submit_for_Approval(task_dict, global_constants,
-                                              stage_value_dict):
-    task_dict["status_variables"]["Status1"] = "PR_PENDING_RP_APPROVAL"
-    return task_dict
-
-
-def PR_NEED_CLARIFICATION_Discard(task_dict, global_constants,
-                                  stage_value_dict):
-    task_dict["status_variables"]["Status1"] = "PR_DISCARDED"
-    return task_dict
-
-
 def PR_PENDING_ACCOUNTS_LEVEL1_VERIFICATION_Verify(task_dict, global_constants,
                                                    stage_value_dict):
     task_dict["status_variables"][
         "Status1"] = "PR_PENDING_ACCOUNTS_LEVEL2_VERIFICATION"
     task_dict["status_variables"][
-        "Status9"] = "PR_ACCOUNTS_LEVEL1_VERIFICATION_DONE"
+        "Status9"] = PR_ACCOUNTS_LEVEL1_VERIFICATION_DONE
     return task_dict
 
 
@@ -158,7 +198,7 @@ def PR_PENDING_ACCOUNTS_LEVEL2_VERIFICATION_Verify(task_dict, global_constants,
     task_dict["status_variables"][
         "Status5"] = "PR_PENDING_ACCOUNTS_LEVEL4_VERIFICATION"
     task_dict["status_variables"][
-        "Status10"] = "PR_ACCOUNTS_LEVEL2_VERIFICATION_DONE"
+        "Status10"] = PR_ACCOUNTS_LEVEL2_VERIFICATION_DONE
     return task_dict
 
 
@@ -197,6 +237,20 @@ def PR_PENDING_ACCOUNTS_LEVEL4_VERIFICATION_Verify(task_dict, global_constants,
                     "Status6"] == "PR_ACCOUNTS_VERIFICATION_COMPLETED":
         task_dict["status_variables"][
             "Status7"] = "PR_PENDING_OVERALL_FINANCE_RP_APPROVAL"
+    return task_dict
+
+
+def PR_PENDING_COMPLIANCE_APPROVAL_Verify(task_dict, global_constants,
+                                          stage_value_dict):
+    task_dict["status_variables"][
+        "Status4"] = "PR_PENDING_PAYMENTS_LEVEL3_VERIFICATION"
+
+    return task_dict
+
+
+def PR_PENDING_OVERALL_FINANCE_RP_APPROVAL_Approve(task_dict, global_constants,
+                                                   stage_value_dict):
+    task_dict["status_variables"]["Status8"] = "PR_PENDING_PAYMENTS_PROCESSING"
     return task_dict
 
 
@@ -246,6 +300,13 @@ def PR_PENDING_PAYMENTS_LEVEL2_VERIFICATION_Verify(task_dict, global_constants,
     return task_dict
 
 
+def PR_PENDING_PAYMENTS_LEVEL3_VERIFICATION_Agreement_Mismatch(
+        task_dict, global_constants, stage_value_dict):
+    task_dict["status_variables"]["Status4"] = "PR_PENDING_COMPLIANCE_APPROVAL"
+
+    return task_dict
+
+
 def PR_PENDING_PAYMENTS_LEVEL3_VERIFICATION_Approve(task_dict,
                                                     global_constants,
                                                     stage_value_dict):
@@ -283,21 +344,6 @@ def PR_PENDING_PAYMENTS_LEVEL3_VERIFICATION_Reject(task_dict, global_constants,
     return task_dict
 
 
-def PR_PENDING_PAYMENTS_LEVEL3_VERIFICATION_Agreement_Mismatch(
-        task_dict, global_constants, stage_value_dict):
-    task_dict["status_variables"]["Status4"] = "PR_PENDING_COMPLIANCE_APPROVAL"
-
-    return task_dict
-
-
-def PR_PENDING_COMPLIANCE_APPROVAL_Verify(task_dict, global_constants,
-                                          stage_value_dict):
-    task_dict["status_variables"][
-        "Status4"] = "PR_PENDING_PAYMENTS_LEVEL3_VERIFICATION"
-
-    return task_dict
-
-
 def PR_PENDING_PAYMENTS_LEVEL5_APPROVAL_Approve(task_dict, global_constants,
                                                 stage_value_dict):
     task_dict["status_variables"][
@@ -330,19 +376,105 @@ def PR_PENDING_PAYMENTS_LEVEL5_APPROVAL_Reject(task_dict, global_constants,
     return task_dict
 
 
-def PR_PENDING_OVERALL_FINANCE_RP_APPROVAL_Approve(task_dict, global_constants,
-                                                   stage_value_dict):
-    task_dict["status_variables"]["Status8"] = "PR_PENDING_PAYMENTS_PROCESSING"
+def PR_PENDING_RP_APPROVAL_Approve(task_dict, global_constants,
+                                   stage_value_dict):
+    task_dict["status_variables"][
+        "Status2"] = "PR_PENDING_PAYMENTS_LEVEL1_VERIFICATION"
+    task_dict["status_variables"][
+        "Status3"] = "PR_PENDING_PAYMENTS_LEVEL2_VERIFICATION"
+    task_dict["status_variables"][
+        "Status4"] = "PR_PENDING_PAYMENTS_LEVEL3_VERIFICATION"
+    task_dict["status_variables"][
+        "Status1"] = "PR_PENDING_ACCOUNTS_LEVEL1_VERIFICATION"
     return task_dict
 
 
-def PR_CREATE_PAYMENT_REQUEST_Save_as_Draft(task_dict, global_constants,
-                                            stage_value_dict):
-    task_dict["status_variables"]["Status1"] = "PR_PAYMENT_REQUEST_DRAFTS"
+def PR_PENDING_RP_APPROVAL_Need_Clarification(task_dict, global_constants,
+                                              stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "PR_NEED_CLARIFICATION"
     return task_dict
 
 
-def PR_CREATE_PAYMENT_REQUEST_Submit_for_Approval(task_dict, global_constants,
-                                                  stage_value_dict):
-    task_dict["status_variables"]["Status1"] = "PR_PENDING_RP_APPROVAL"
+def PR_PENDING_RP_APPROVAL_Reject(task_dict, global_constants,
+                                  stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "PR_REJECTION_LIST"
+    return task_dict
+
+
+def VENDOR_DRAFTS_Discard(task_dict, global_constants, stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "VENDOR_DISCARDED"
+    return task_dict
+
+
+def VENDOR_DRAFTS_Save_Draft(task_dict, global_constants, stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "VENDOR_DRAFTS"
+    return task_dict
+
+
+def VENDOR_DRAFTS_Submit_(task_dict, global_constants, stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "VENDOR_PENDING_RP_APPROVAL"
+    return task_dict
+
+
+def VENDOR_PENDING_ACCOUNTS_LEVEL5_VERIFICATION_Verify(task_dict,
+                                                       global_constants,
+                                                       stage_value_dict):
+    task_dict["status_variables"][
+        "Status3"] = "VENDOR_ACCOUNTS_LEVEL5_VERIFICATION_COMPLETED"
+
+    if task_dict["status_variables"][
+            "Status1"] == "VENDOR_PAYMENTS_LEVEL5_VERIFICATION_COMPLETED" and task_dict[
+                "status_variables"][
+                    "Status2"] == "VENDOR_PAYMENTS_LEVEL4_VERIFICATION_COMPLETED":
+        task_dict["status_variables"][
+            "Status4"] = "VENDOR_VERIFICATION_COMPLETED"
+    return task_dict
+
+
+def VENDOR_PENDING_PAYMENTS_LEVEL4_VERIFICATION_Verify(task_dict,
+                                                       global_constants,
+                                                       stage_value_dict):
+    task_dict["status_variables"][
+        "Status2"] = "VENDOR_PAYMENTS_LEVEL4_VERIFICATION_COMPLETED"
+
+    if task_dict["status_variables"][
+            "Status1"] == "VENDOR_PAYMENTS_LEVEL5_VERIFICATION_COMPLETED" and task_dict[
+                "status_variables"][
+                    "Status3"] == "VENDOR_ACCOUNTS_LEVEL5_VERIFICATION_COMPLETED":
+        task_dict["status_variables"][
+            "Status4"] = "VENDOR_VERIFICATION_COMPLETED"
+    return task_dict
+
+
+def VENDOR_PENDING_PAYMENTS_LEVEL5_VERIFICATION_Verify(task_dict,
+                                                       global_constants,
+                                                       stage_value_dict):
+    task_dict["status_variables"][
+        "Status1"] = "VENDOR_PAYMENTS_LEVEL5_VERIFICATION_COMPLETED"
+
+    if task_dict["status_variables"][
+            "Status2"] == "VENDOR_PAYMENTS_LEVEL4_VERIFICATION_COMPLETED" and task_dict[
+                "status_variables"][
+                    "Status3"] == "VENDOR_ACCOUNTS_LEVEL5_VERIFICATION_COMPLETED":
+        task_dict["status_variables"][
+            "Status4"] = "VENDOR_VERIFICATION_COMPLETED"
+
+    return task_dict
+
+
+def VENDOR_PENDING_RP_APPROVAL_Approve(task_dict, global_constants,
+                                       stage_value_dict):
+    task_dict["status_variables"][
+        "Status1"] = "VENDOR_PENDING_PAYMENTS_LEVEL5_VERIFICATION"
+    task_dict["status_variables"][
+        "Status2"] = "VENDOR_PENDING_PAYMENTS_LEVEL4_VERIFICATION"
+    task_dict["status_variables"][
+        "Status3"] = "VENDOR_PENDING_ACCOUNTS_LEVEL5_VERIFICATION"
+
+    return task_dict
+
+
+def VENDOR_PENDING_RP_APPROVAL_Reject(task_dict, global_constants,
+                                      stage_value_dict):
+    task_dict["status_variables"]["Status1"] = "VENDOR_REJECTED"
     return task_dict
