954bed18d519ee96ee95f50985ffaa5ad45fe329
geetha.k <geetha.k@ibhubs.co.in>
2020-08-31 13:04:48 +0530

    change in populate utils

--- ib_boards/populate/population_utils.py
+++ ib_boards/populate/population_utils.py
@@ -2,18 +2,16 @@ from django.db import transaction
 
 from ib_boards.populate.get_data_from_sheet_for_boards_and_columns import \
     GetBoardsAndColumnsDataFromSheet
+
 from ib_boards.populate.get_data_from_sheet_for_project_boards import \
     GetSheetDataForProjectBoards
 
 
 @transaction.atomic()
-def populate_boards_and_columns_data(spread_sheet_name: str):
+def populate_data(spread_sheet_name: str,
+                  boards_columns_spread_sheet_name: str):
     boards_and_columns = GetBoardsAndColumnsDataFromSheet()
-    boards_and_columns.create_boards_and_columns(spread_sheet_name)
+    boards_and_columns.create_boards_and_columns(boards_columns_spread_sheet_name)
 
-
-@transaction.atomic()
-def populate_data(spread_sheet_name: str):
     project_boards = GetSheetDataForProjectBoards()
     project_boards.get_data_from_project_boards_sub_sheet(spread_sheet_name)
-
