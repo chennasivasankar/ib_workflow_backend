3366fe197962f5e4322e75624ef2a11075be94bb
geetha.k <geetha.k@ibhubs.co.in>
2020-08-31 11:45:38 +0530

    test population scripts

--- ib_boards/populate/population_utils.py
+++ ib_boards/populate/population_utils.py
@@ -7,9 +7,13 @@ from ib_boards.populate.get_data_from_sheet_for_project_boards import \
 
 
 @transaction.atomic()
-def populate_data(spread_sheet_name: str):
+def populate_boards_and_columns_data(spread_sheet_name: str):
     boards_and_columns = GetBoardsAndColumnsDataFromSheet()
     boards_and_columns.create_boards_and_columns(spread_sheet_name)
 
+
+@transaction.atomic()
+def populate_data(spread_sheet_name: str):
     project_boards = GetSheetDataForProjectBoards()
     project_boards.get_data_from_project_boards_sub_sheet(spread_sheet_name)
+
