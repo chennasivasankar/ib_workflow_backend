09dc9e843f32f915fd81bb03a15091a94ca66d6d
geetha.k <geetha.k@ibhubs.co.in>
2020-08-31 14:25:45 +0530

    change in populate utils

--- ib_boards/populate/population_utils.py
+++ ib_boards/populate/population_utils.py
@@ -8,10 +8,11 @@ from ib_boards.populate.get_data_from_sheet_for_project_boards import \
 
 
 @transaction.atomic()
-def populate_data(spread_sheet_name: str,
-                  boards_columns_spread_sheet_name: str):
+def populate_data(**kwargs):
     boards_and_columns = GetBoardsAndColumnsDataFromSheet()
-    boards_and_columns.create_boards_and_columns(boards_columns_spread_sheet_name)
+    boards_and_columns.create_boards_and_columns(
+        kwargs['boards_columns_spread_sheet_name'])
 
     project_boards = GetSheetDataForProjectBoards()
-    project_boards.get_data_from_project_boards_sub_sheet(spread_sheet_name)
+    project_boards.get_data_from_project_boards_sub_sheet(
+        kwargs['project_boards_spread_sheet_name'])
