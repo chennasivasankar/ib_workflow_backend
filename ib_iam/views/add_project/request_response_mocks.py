62e2549e68acf4aac846810c1ea8e80f142e9f1c
Your Name <jaswanth.mamidipudi@ibhubs.co.in>
2020-09-02 12:41:11 +0530

    Add exception enums in api_spec in add_project

--- ib_iam/views/add_project/request_response_mocks.py
+++ ib_iam/views/add_project/request_response_mocks.py
@@ -19,3 +19,19 @@ REQUEST_BODY_JSON = """
 """
 
 
+RESPONSE_400_JSON = """
+{
+    "response": "string",
+    "http_status_code": 1,
+    "res_status": "PROJECT_NAME_ALREADY_EXISTS"
+}
+"""
+
+RESPONSE_404_JSON = """
+{
+    "response": "string",
+    "http_status_code": 1,
+    "res_status": "INVALID_TEAM_IDS"
+}
+"""
+
