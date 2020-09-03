6a93334a0e56d2ed987890e751d530d1a20a7fa3
geetha.k <geetha.k@ibhubs.co.in>
2020-09-03 10:50:54 +0530

    change in storages

--- ib_tasks/tests/storages/test_fields_storage_implementation.py
+++ ib_tasks/tests/storages/test_fields_storage_implementation.py
@@ -6,7 +6,8 @@ import pytest
 from ib_tasks.constants.constants import ALL_ROLES_ID
 from ib_tasks.constants.enum import PermissionTypes
 from ib_tasks.interactors.storage_interfaces.task_dtos import TaskProjectRolesDTO
-from ib_tasks.tests.factories.models import FieldFactory, FieldRoleFactory, TaskGoFFieldFactory
+from ib_tasks.tests.factories.models import FieldFactory, FieldRoleFactory, TaskGoFFieldFactory, TaskGoFFactory, \
+    TaskFactory
 from ib_tasks.tests.factories.storage_dtos import \
     FieldCompleteDetailsDTOFactory
 
@@ -160,21 +161,25 @@ class TestFieldsStorageImplementation:
         fields = FieldFactory.create_batch(size=2)
         FieldRoleFactory.create_batch(
             size=2, permission_type=PermissionTypes.READ.value,
-            role=factory.Iterator(user_roles),
+            role=factory.Iterator(user_roles[0].roles),
             field=factory.Iterator(fields)
         )
+        TaskGoFFactory.reset_sequence()
+        TaskFactory.reset_sequence()
+        gofs = TaskGoFFactory.create_batch(size=3)
         TaskGoFFieldFactory.create_batch(size=3,
+                                         task_gof=factory.Iterator(gofs),
                                          field=factory.Iterator(fields))
 
-        field_ids = [field.field_id for field in fields]
+        field_ids = ['FIELD_ID-1', 'FIELD_ID-2']
 
         # Act
-        field_ids_having_read_permission_for_user = \
+        field_ids_having_permission_for_user = \
             storage.get_field_ids_permissions_for_user_in_projects(
                 field_ids=field_ids, task_project_roles=user_roles)
 
         # Assert
-        assert field_ids_having_read_permission_for_user == field_ids
+        assert field_ids_having_permission_for_user == field_ids
 
     def test_get_field_ids_having_read_permission_for_user_when_fields_has_all_roles(
             self, storage):
