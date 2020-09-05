fed6f04f654c8019043e69c8a6e87dd237cd15b0
DurgaPrime <durgaprasad@ibhubs.co>
2020-09-05 10:30:54 +0530

    added migrations

--- ib_discussions/models/discussion_set.py
+++ ib_discussions/models/discussion_set.py
@@ -11,7 +11,7 @@ class DiscussionSet(models.Model):
     id = models.UUIDField(primary_key=True, default=generate_uuid,
                           editable=False)
     entity_id = models.CharField(max_length=200)
-    entity_type = models.CharField(max_length=50)
+    entity_type = models.CharField(max_length=25)
 
     class Meta:
         unique_together = ("entity_id", "entity_type")
