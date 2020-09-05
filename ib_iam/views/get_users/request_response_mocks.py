49e2d92f62fcc7abe83f2207cc69bcecea8bfb80
DurgaPrime <durgaprasad@ibhubs.co>
2020-09-05 18:22:18 +0530

    fixed import statement in ib task

--- ib_iam/views/get_users/request_response_mocks.py
+++ ib_iam/views/get_users/request_response_mocks.py
@@ -5,7 +5,7 @@ RESPONSE_200_JSON = """
 {
     "users": [
         {
-            "user_id": "6f6c75fc-910f-4b96-9cff-4217e8c967c0",
+            "user_id": "89d96f4b-c19d-4e69-8eae-e818f3123b09",
             "name": "string",
             "email": "string",
             "roles": [
@@ -16,7 +16,7 @@ RESPONSE_200_JSON = """
             ],
             "teams": [
                 {
-                    "team_id": "d20c30d6-de30-44c3-8208-3d9343132a68",
+                    "team_id": "89d96f4b-c19d-4e69-8eae-e818f3123b09",
                     "team_name": "string"
                 }
             ],
