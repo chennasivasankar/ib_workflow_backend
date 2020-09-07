fa00408d1249909b99a65600ead800c167e06828
DurgaPrime <durgaprasad@ibhubs.co>
2020-09-07 11:59:55 +0530

    refactoring the ib_utils_tools

--- ib_utility_tools/tests/presenters/snapshots/snap_test_get_checklist_presenter_implementation.py
+++ ib_utility_tools/tests/presenters/snapshots/snap_test_get_checklist_presenter_implementation.py
@@ -7,17 +7,17 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestGetChecklistPresenterImplementation.test_whether_it_returns_checklist_items_and_entity_http_response response'] = {
+snapshots['TestGetChecklistPresenterImplementation.test_with_valid_details_then_returns_response response'] = {
     'checklist': [
         {
             'checklist_item_id': '7ee2c7b4-34c8-4d65-a83a-f87da75db24e',
             'is_checked': False,
-            'text': 'text1'
+            'text': 'text0'
         },
         {
             'checklist_item_id': '09b6cf6d-90ea-43ac-b0ee-3cee3c59ce5a',
             'is_checked': False,
-            'text': 'text2'
+            'text': 'text1'
         }
     ]
 }
