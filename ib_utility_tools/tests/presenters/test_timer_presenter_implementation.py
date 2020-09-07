f7816906948007cb7a0ec571d2bcd5a3e7bcb145
DurgaPrime <durgaprasad@ibhubs.co>
2020-09-07 12:16:43 +0530

    refactoring the ib_utils_tools

--- ib_utility_tools/tests/presenters/test_timer_presenter_implementation.py
+++ ib_utility_tools/tests/presenters/test_timer_presenter_implementation.py
@@ -1,20 +1,30 @@
 import json
-
-from ib_utility_tools.presenters.timer_presenter_implementation import \
-    TimerPresenterImplementation
+import pytest
 
 
 class TestTimerPresenterImplementation:
+    @pytest.fixture
+    def presenter(self):
+        from ib_utility_tools.presenters.timer_presenter_implementation import (
+            TimerPresenterImplementation
+        )
+        presenter = TimerPresenterImplementation()
+        return presenter
 
-    def test_whether_it_returns_timer_details_http_response(self):
-        json_presenter = TimerPresenterImplementation()
-        from ib_utility_tools.tests.factories.storage_dtos import \
+    def test_with_valid_details_then_returns_timer_details_response(
+            self, presenter
+    ):
+        # Arrange
+        from ib_utility_tools.tests.factories.storage_dtos import (
             TimerDetailsDTOFactory
+        )
+        TimerDetailsDTOFactory.reset_sequence(0)
         timer_details_dto = TimerDetailsDTOFactory()
         expected_duration_in_seconds = timer_details_dto.duration_in_seconds
         expected_is_running = timer_details_dto.is_running
 
-        result = json_presenter.get_success_response_with_timer_details_dto(
+        # Act
+        result = presenter.get_response_for_get_timer_details(
             timer_details_dto=timer_details_dto)
 
         actual_response = json.loads(result.content)
@@ -23,8 +33,9 @@ class TestTimerPresenterImplementation:
         assert actual_duration_in_seconds == expected_duration_in_seconds
         assert actual_is_running == expected_is_running
 
-    def test_whether_it_returns_timer_is_already_running_http_response(self):
-        json_presenter = TimerPresenterImplementation()
+    def test_whether_it_returns_timer_is_already_running_http_response(
+            self, presenter
+    ):
         from ib_utility_tools.constants.exception_messages import \
             TIMER_IS_ALREADY_RUNNING
         expected_response = TIMER_IS_ALREADY_RUNNING[0]
@@ -32,7 +43,7 @@ class TestTimerPresenterImplementation:
         from ib_iam.constants.enums import StatusCode
         expected_http_status_code = StatusCode.BAD_REQUEST.value
 
-        result = json_presenter.raise_timer_is_already_running_exception()
+        result = presenter.response_for_timer_is_already_running_exception()
         response_dict = json.loads(result.content)
         actual_response = response_dict["response"]
         actual_res_status = response_dict["res_status"]
@@ -42,8 +53,9 @@ class TestTimerPresenterImplementation:
         assert actual_res_status == expected_res_status
         assert expected_http_status_code == actual_http_status_code
 
-    def test_whether_it_returns_timer_is_already_stopped_http_response(self):
-        json_presenter = TimerPresenterImplementation()
+    def test_whether_it_returns_timer_is_already_stopped_http_response(
+            self, presenter
+    ):
         from ib_utility_tools.constants.exception_messages import \
             TIMER_IS_ALREADY_STOPPED
         expected_response = TIMER_IS_ALREADY_STOPPED[0]
@@ -51,7 +63,7 @@ class TestTimerPresenterImplementation:
         from ib_iam.constants.enums import StatusCode
         expected_http_status_code = StatusCode.BAD_REQUEST.value
 
-        result = json_presenter.raise_timer_is_already_stopped_exception()
+        result = presenter.response_for_timer_is_already_stopped_exception()
         response_dict = json.loads(result.content)
         actual_response = response_dict["response"]
         actual_res_status = response_dict["res_status"]
