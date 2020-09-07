26defc9a8ada84001737ba0dfe456337606bebbf
prathyushakl <jonathan@notmyemail.com>
2020-09-07 12:58:16 +0530

    Updated test cases fixture

--- ib_tasks/tests/views/get_task_assignees_of_diff_stages_for_an_action/__init__.py
+++
@@ -1,6 +0,0 @@
-# pylint: disable=wrong-import-position
-
-APP_NAME = "ib_tasks"
-OPERATION_NAME = "get_task_assignees_of_diff_stages_for_an_action"
-REQUEST_METHOD = "get"
-URL_SUFFIX = "task/{task_id}/action/{action_id}/stage_assignees/v1/"
