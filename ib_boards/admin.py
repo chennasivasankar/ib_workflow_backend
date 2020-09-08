a5ab7420790e5502ce68c980da1ec12be93fa2ea
geetha.k <geetha.k@ibhubs.co.in>
2020-09-08 12:52:38 +0530

    change a field in board to editable

--- ib_boards/admin.py
+++ ib_boards/admin.py
@@ -1,9 +1,9 @@
 # your django admin
 from django.contrib import admin
 
-from ib_boards.models import Board, Column, ColumnPermission, FieldOrder, FieldDisplayStatus
+from ib_boards.models import (Board, Column, ColumnPermission, FieldOrder,
+                              FieldDisplayStatus)
 
-admin.site.register(Board)
 admin.site.register(ColumnPermission)
 
 
@@ -12,6 +12,12 @@ class ColumnAdmin(admin.ModelAdmin):
     list_editable = 'task_selection_config',
 
 
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('project_id', 'name', 'board_id')
+    list_editable = 'name',
+
+
+admin.site.register(Board, BoardAdmin)
 admin.site.register(Column, ColumnAdmin)
 admin.site.register(FieldDisplayStatus)
 admin.site.register(FieldOrder)
