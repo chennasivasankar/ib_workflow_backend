e3ea2e285470cf49d422b519c08c5d77d363d4aa
DurgaPrime <durgaprasad@ibhubs.co>
2020-09-09 11:51:05 +0530

    run test cases

--- ib_utility_tools/tests/views/delete_checklist_items/snapshots/snap_test_case_01.py
+++ ib_utility_tools/tests/views/delete_checklist_items/snapshots/snap_test_case_01.py
@@ -4,26 +4,11 @@ from __future__ import unicode_literals
 
 from snapshottest import Snapshot
 
-
 snapshots = Snapshot()
 
-snapshots['TestCase01DeleteChecklistItemsAPITestCase.test_success_case_for_delete_checklist_items status_code'] = '200'
+snapshots[
+    'TestCase01DeleteChecklistItemsAPITestCase.test_success_case_for_delete_checklist_items status_code'] = '200'
 
-snapshots['TestCase01DeleteChecklistItemsAPITestCase.test_success_case_for_delete_checklist_items body'] = {
-}
-
-snapshots['TestCase01DeleteChecklistItemsAPITestCase.test_duplicate_items_response status_code'] = '400'
-
-snapshots['TestCase01DeleteChecklistItemsAPITestCase.test_duplicate_items_response body'] = {
-    'http_status_code': 400,
-    'res_status': 'DUPLICATE_CHECKLIST_ITEM_IDS',
-    'response': 'Given checklist item ids are duplicated'
-}
-
-snapshots['TestCase01DeleteChecklistItemsAPITestCase.test_invalid_checklist_item_ids_response status_code'] = '404'
-
-snapshots['TestCase01DeleteChecklistItemsAPITestCase.test_invalid_checklist_item_ids_response body'] = {
-    'http_status_code': 404,
-    'res_status': 'CHECKLIST_ITEM_IDS_NOT_FOUND',
-    'response': 'Given checklist item ids not found'
+snapshots[
+    'TestCase01DeleteChecklistItemsAPITestCase.test_success_case_for_delete_checklist_items body'] = {
 }
