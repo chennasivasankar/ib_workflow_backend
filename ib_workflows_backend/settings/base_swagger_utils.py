a7de9cc9e19730d33a9ae6d8ee68406658f0ddbc
prasaduu <arlabandaswamydurgaprasad@gmail.com>
2020-09-11 12:41:12 +0530

    Add settings minor change

--- ib_workflows_backend/settings/base_swagger_utils.py
+++ ib_workflows_backend/settings/base_swagger_utils.py
@@ -117,14 +117,17 @@ ELASTICSEARCH_DSL = {
 }
 
 # *********************  Authentication **************
-USER_VERIFICATION_EMAIL_EXPIRY_IN_SECONDS = 1800
+RESET_PASSWORD_LINK_EXPIRY_IN_SECONDS = \
+      int(os.environ.get("RESET_PASSWORD_LINK_EXPIRY_IN_SECONDS", '3600'))
+
+USER_VERIFICATION_EMAIL_EXPIRY_IN_SECONDS = \
+    int(os.environ.get("USER_VERIFICATION_EMAIL_EXPIRY_IN_SECONDS", '3600'))
 
 FRONTEND_URL = os.environ.get(
     "FRONTEND_URL",
     'https://ib-workflows-web-alpha.apigateway.in'
 )
 
-RESET_PASSWORD_LINK_EXPIRY_IN_SECONDS = USER_VERIFICATION_EMAIL_EXPIRY_IN_SECONDS
 RESET_PASSWORD_LINK = '{frontend_url}?token='.format(frontend_url=FRONTEND_URL)
 
 USER_VERIFICATION_EMAIL_LINK = "{frontend_url}/verify-email?token=".format(
