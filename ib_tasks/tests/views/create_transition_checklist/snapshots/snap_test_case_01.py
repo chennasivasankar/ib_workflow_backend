a15e079b65dd17bda62e6569f7b844fef87ea446
venkata.bandaru <venkata.bandaru@ibhubs.co.in>
2020-09-15 16:29:56 +0530

    fix create transition checklist api integration test cases

---
+++ ib_tasks/tests/views/create_transition_checklist/snapshots/snap_test_case_01.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['TestCase32CreateTransitionChecklistAPITestCase.test_case status_code'] = '201'
+
+snapshots['TestCase32CreateTransitionChecklistAPITestCase.test_case body'] = {
+    'message': 'transition checklist created successfully'
+}
+
+snapshots['TestCase32CreateTransitionChecklistAPITestCase.test_case task_id'] = 1
+
+snapshots['TestCase32CreateTransitionChecklistAPITestCase.test_case template_id'] = 'template_0'
+
+snapshots['TestCase32CreateTransitionChecklistAPITestCase.test_case task_title'] = 'title_0'
+
+snapshots['TestCase32CreateTransitionChecklistAPITestCase.test_case task_description'] = 'description_0'
+
+snapshots['TestCase32CreateTransitionChecklistAPITestCase.test_case task_start_date'] = '2020-10-12 04:40:00'
+
+snapshots['TestCase32CreateTransitionChecklistAPITestCase.test_case task_due_date'] = '2020-10-22 04:40:00'
+
+snapshots['TestCase32CreateTransitionChecklistAPITestCase.test_case task_priority'] = 'HIGH'
+
+snapshots['TestCase32CreateTransitionChecklistAPITestCase.test_case same_gof_order_1'] = 1
+
+snapshots['TestCase32CreateTransitionChecklistAPITestCase.test_case gof_id_1'] = 'gof_1'
+
+snapshots['TestCase32CreateTransitionChecklistAPITestCase.test_case gof_task_id_1'] = 1
+
+snapshots['TestCase32CreateTransitionChecklistAPITestCase.test_case task_gof_1'] = 1
+
+snapshots['TestCase32CreateTransitionChecklistAPITestCase.test_case field_1'] = 'field_1'
+
+snapshots['TestCase32CreateTransitionChecklistAPITestCase.test_case field_response_1'] = 'hello world'
