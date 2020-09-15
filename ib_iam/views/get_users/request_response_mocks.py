21e806bdf03a4a17e75fd2db02a17048cf0abbf0
DurgaPrime <durgaprasad@ibhubs.co>
2020-09-15 15:45:03 +0530

    run test cases

--- ib_iam/views/get_users/request_response_mocks.py
+++ ib_iam/views/get_users/request_response_mocks.py
@@ -20,7 +20,7 @@ RESPONSE_200_JSON = """
                     "team_name": "string"
                 }
             ],
-            "is_email_verified": "string",
+            "is_email_verified": true,
             "company": {
                 "company_id": "string",
                 "company_name": "string"
