89b891b557b438a4a6f5b1ac940c9e5b361b5932
geetha.k <geetha.k@ibhubs.co.in>
2020-09-15 09:36:28 +0530

    refactor tests

--- ib_tasks/interactors/get_task_fields.py
+++ ib_tasks/interactors/get_task_fields.py
@@ -1,7 +1,6 @@
 from typing import List
 
 from ib_tasks.constants.enum import ViewType
-from ib_tasks.exceptions.stage_custom_exceptions import InvalidTaskStageIds
 from ib_tasks.exceptions.task_custom_exceptions import InvalidTaskIds
 from ib_tasks.interactors.storage_interfaces.fields_dtos import \
     (TaskTemplateStageFieldsDTO, StageTaskFieldsDTO)
@@ -9,10 +8,8 @@ from ib_tasks.interactors.storage_interfaces.fields_storage_interface \
     import FieldsStorageInterface
 from ib_tasks.interactors.storage_interfaces.stage_dtos import \
     TaskTemplateStageDTO
-from ib_tasks.interactors.storage_interfaces.task_dtos import TaskProjectDTO
 from ib_tasks.interactors.storage_interfaces.task_storage_interface import \
     TaskStorageInterface
-from ib_tasks.interactors.task_dtos import GetTaskDetailsDTO
 from ib_tasks.interactors.user_role_validation_interactor import \
     UserRoleValidationInteractor
 
@@ -83,9 +80,9 @@ class GetTaskFieldsInteractor:
         unique_task_ids = list(sorted(set(task_ids)))
         valid_task_ids = self.task_storage.get_valid_task_ids(unique_task_ids)
         invalid_task_ids = [
-            task_id for task_id in task_ids if task_id not in valid_task_ids
+                task_id for task_id in task_ids if
+                task_id not in valid_task_ids
         ]
         if invalid_task_ids:
             raise InvalidTaskIds(invalid_task_ids)
         return task_ids
-
