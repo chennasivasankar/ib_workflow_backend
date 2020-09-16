cd9e5893a33e20a7b8c51970b0b0e685df058671
geetha.k <geetha.k@ibhubs.co.in>
2020-09-16 15:32:02 +0530

    refactor tests

--- ib_tasks/tests/interactors/test_create_or_update_stage_information_interactor.py
+++ ib_tasks/tests/interactors/test_create_or_update_stage_information_interactor.py
@@ -3,10 +3,9 @@ import pytest
 from ib_boards.exceptions.custom_exceptions import InvalidTemplateFields
 from ib_tasks.exceptions.roles_custom_exceptions import \
     InvalidStageRolesException
-from ib_tasks.exceptions.stage_custom_exceptions import (InvalidStageValues,
-                                                         DuplicateStageIds,
-                                                         InvalidStageDisplayLogic,
-                                                         InvalidStagesDisplayName)
+from ib_tasks.exceptions.stage_custom_exceptions import (
+    InvalidStageValues, DuplicateStageIds, InvalidStageDisplayLogic,
+    InvalidStagesDisplayName)
 from ib_tasks.exceptions.task_custom_exceptions import \
     (InvalidStagesTaskTemplateId, InvalidTaskTemplateIds)
 from ib_tasks.interactors.create_or_update_stages import \
@@ -62,31 +61,15 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
                         operator="==",
                         stage="stage_id_1"))]
         get_stage_display_logic_mock(mocker, stage_logics)
-        get_valid_role_ids_in_given_role_ids(mocker)
 
-    def test_given_invalid_roles_raises_exception(
-            self, mocker, stage_interactor,
-            valid_stages_dto, task_storage, stage_storage,
-            task_template_storage):
-        # Arrange
-        StageDTOFactory.reset_sequence()
-        create_stage_dtos = StageDTOFactory.create_batch(
-                size=2, value=-1, roles="ROLE"
-        )
-
-        stage_logics = [StatusOperandStageDTO(
-                variable="status1",
-                operator="==",
-                stage="stage_id_1"
-        )]
-        from ib_tasks.tests.common_fixtures.adapters.roles_service \
-            import get_valid_role_ids_in_given_role_ids
+    @staticmethod
+    def adapters_mock(mocker):
         mocker_obj = get_valid_role_ids_in_given_role_ids(mocker)
-        get_stage_display_logic_mock(mocker, stage_logics)
+        return mocker_obj
 
-        stage_storage.validate_stages_related_task_template_ids. \
-            return_value = []
-        stage_storage.get_existing_status_ids.return_value = ["status1"]
+    @staticmethod
+    @pytest.fixture
+    def storage_method_mocks(task_storage, task_template_storage):
         task_template_storage \
             .get_valid_task_template_ids_in_given_task_template_ids. \
             return_value = ["task_template_id_1", "task_template_id_2"]
@@ -100,7 +83,21 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
                         field_ids=["field_id_1", "field_id_2"]
                 )]
 
-        stage_storage.get_existing_stage_ids.side_effect = [[], ["stage_id_1"]]
+    def test_given_invalid_roles_raises_exception(
+            self, mocker, stage_interactor, storage_method_mocks,
+            valid_stages_dto, task_storage, stage_storage,
+            task_template_storage):
+        # Arrange
+        StageDTOFactory.reset_sequence()
+        create_stage_dtos = StageDTOFactory.create_batch(
+                size=2, value=-1, roles="ROLE"
+        )
+
+        mocker_obj = self.adapters_mock(mocker)
+
+        stage_storage.validate_stages_related_task_template_ids. \
+            return_value = []
+        stage_storage.get_existing_status_ids.return_value = ["status1"]
 
         # Act
         with pytest.raises(InvalidStageRolesException) as error:
@@ -114,35 +111,12 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
     def test_create_stage_given_valid_information_creates_stage_with_given_information(
             self, mocker, create_stage_dtos, stage_interactor,
             valid_stages_dto, task_storage, stage_storage,
-            task_template_storage):
+            task_template_storage, storage_method_mocks):
         # Arrange
-
-        stage_logics = [StageDisplayLogicDTO(
-                current_stage="stage_id_1",
-                display_logic_dto=StatusOperandStageDTO(
-                        variable="status1",
-                        operator="==",
-                        stage="stage_id_1"))]
-        from ib_tasks.tests.common_fixtures.adapters.roles_service \
-            import get_valid_role_ids_in_given_role_ids
-        mocker_obj = get_valid_role_ids_in_given_role_ids(mocker)
-        get_stage_display_logic_mock(mocker, stage_logics)
-        mocker_obj.return_value = ["role_id_1", "role_id_2", "role_id_0"]
+        self.interactors_mock(mocker)
+        self.adapters_mock(mocker)
         stage_storage.validate_stages_related_task_template_ids. \
             return_value = []
-
-        task_template_storage \
-            .get_valid_task_template_ids_in_given_task_template_ids. \
-            return_value = ["task_template_id_1", "task_template_id_2"]
-        task_storage.get_field_ids_for_given_task_template_ids.return_value = [
-                TemplateFieldsDTO(
-                        task_template_id="task_template_id_1",
-                        field_ids=["field_id_1", "field_id_2"]
-                ),
-                TemplateFieldsDTO(
-                        task_template_id="task_template_id_2",
-                        field_ids=["field_id_1", "field_id_2"]
-                )]
 
         stage_storage.get_existing_status_ids.return_value = ["status1"]
         stage_storage.get_existing_stage_ids.side_effect = [[], []]
@@ -159,7 +133,7 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
         )
 
     def test_create_and_update_stage_given_valid_information_creates_and_updates(
-            self, create_stage_dtos, stage_interactor,
+            self, create_stage_dtos, stage_interactor, storage_method_mocks,
             valid_stages_dto, task_storage, stage_storage,
             task_template_storage, mocker):
         # Arrange
@@ -167,21 +141,9 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
         stage_storage.validate_stages_related_task_template_ids. \
             return_value = []
 
-        task_template_storage \
-            .get_valid_task_template_ids_in_given_task_template_ids. \
-            return_value = ["task_template_id_1", "task_template_id_2"]
-        task_storage.get_field_ids_for_given_task_template_ids.return_value = [
-                TemplateFieldsDTO(
-                        task_template_id="task_template_id_1",
-                        field_ids=["field_id_1", "field_id_2"]
-                ),
-                TemplateFieldsDTO(
-                        task_template_id="task_template_id_2",
-                        field_ids=["field_id_1", "field_id_2"]
-                )]
         self.interactors_mock(mocker)
+        self.adapters_mock(mocker)
         stage_storage.get_existing_status_ids.return_value = ["status1"]
-        stage_storage.get_existing_stage_ids.side_effect = [[], ["stage_id_1"]]
 
         # Act
         stage_interactor.create_or_update_stages(
@@ -196,7 +158,7 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
 
     def test_update_stage_when_stage_id_already_exists_for_given_task_template_updates_stage_details(
             self, mocker, create_stage_dtos, stage_interactor,
-            create_task_stages_dtos,
+            create_task_stages_dtos, storage_method_mocks,
             valid_stages_dto, task_storage, stage_storage,
             task_template_storage):
         # Arrange
@@ -205,32 +167,11 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
         storage = stage_storage
         storage.get_existing_stage_ids.return_value = \
             ["stage_id_1", "stage_id_2"]
-        from ib_tasks.tests.common_fixtures.adapters.roles_service \
-            import get_valid_role_ids_in_given_role_ids
-        mocker_obj = get_valid_role_ids_in_given_role_ids(mocker)
-        mocker_obj.return_value = ["role_id_1", "role_id_2", "role_id_0"]
+        self.interactors_mock(mocker)
+        self.adapters_mock(mocker)
         storage.validate_stages_related_task_template_ids.return_value = []
         task_stages_dto = create_task_stages_dtos
-        stage_logics = [StageDisplayLogicDTO(
-                current_stage="stage_id_1",
-                display_logic_dto=StatusOperandStageDTO(
-                        variable="status1",
-                        operator="==",
-                        stage="stage_id_1"))]
-        get_stage_display_logic_mock(mocker, stage_logics)
-        task_template_storage \
-            .get_valid_task_template_ids_in_given_task_template_ids. \
-            return_value = ["task_template_id_1", "task_template_id_2"]
         stage_storage.get_existing_status_ids.return_value = ["status1"]
-        task_storage.get_field_ids_for_given_task_template_ids.return_value = [
-                TemplateFieldsDTO(
-                        task_template_id="task_template_id_1",
-                        field_ids=["field_id_1", "field_id_2"]
-                ),
-                TemplateFieldsDTO(
-                        task_template_id="task_template_id_2",
-                        field_ids=["field_id_1", "field_id_2"]
-                )]
 
         # Act
 
@@ -246,20 +187,15 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
 
     def test_validate_values_when_given_invalid_values_raises_exception(
             self, mocker, stage_storage, task_storage, task_template_storage,
-            stage_interactor):
+            stage_interactor, storage_method_mocks):
         # Arrange
         StageDTOFactory.reset_sequence()
         stages_details = StageDTOFactory.create_batch(
                 value=-2, size=2
         )
         storage = stage_storage
-        from ib_tasks.tests.common_fixtures.adapters.roles_service \
-            import get_valid_role_ids_in_given_role_ids
-        mocker_obj = get_valid_role_ids_in_given_role_ids(mocker)
-        mocker_obj.return_value = ["role_id_1", "role_id_2", "role_id_0"]
-        task_template_storage \
-            .get_valid_task_template_ids_in_given_task_template_ids. \
-            return_value = ["task_template_id_1", "task_template_id_2"]
+        self.interactors_mock(mocker)
+        self.adapters_mock(mocker)
         storage.get_existing_stage_ids.return_value = []
         storage.validate_stages_related_task_template_ids.return_value = []
 
@@ -274,7 +210,7 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
     def test_invalid_task_template_id_with_valid_stage_id_raises_exception(
             self, mocker, create_stage_dtos, stage_interactor,
             create_task_stages_dtos, task_storage, stage_storage,
-            task_template_storage
+            task_template_storage, storage_method_mocks
     ):
         # Arrange
         stages_details = create_stage_dtos
@@ -287,30 +223,8 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
         stage_storage.validate_stages_related_task_template_ids. \
             return_value = ["PR_PENDING RP APPROVAL"]
         task_template_ids = ["task_template_id_1", "task_template_id_2"]
-        from ib_tasks.tests.common_fixtures.adapters.roles_service \
-            import get_valid_role_ids_in_given_role_ids
-        mocker_obj = get_valid_role_ids_in_given_role_ids(mocker)
-        mocker_obj.return_value = ["role_id_1", "role_id_2", "role_id_0"]
-
-        task_storage.get_field_ids_for_given_task_template_ids.return_value = [
-                TemplateFieldsDTO(
-                        task_template_id="task_template_id_1",
-                        field_ids=["field_id_1", "field_id_2"]
-                ),
-                TemplateFieldsDTO(
-                        task_template_id="task_template_id_2",
-                        field_ids=["field_id_1", "field_id_2"]
-                )]
-        stage_logics = [StageDisplayLogicDTO(
-                current_stage="stage_id_1",
-                display_logic_dto=StatusOperandStageDTO(
-                        variable="status1",
-                        operator="==",
-                        stage="stage_id_1"))]
-        get_stage_display_logic_mock(mocker, stage_logics)
-        task_template_storage \
-            .get_valid_task_template_ids_in_given_task_template_ids. \
-            return_value = task_template_ids
+        self.interactors_mock(mocker)
+        self.adapters_mock(mocker)
 
         # Act
         with pytest.raises(InvalidStagesTaskTemplateId) as err:
@@ -325,20 +239,15 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
         stage_storage.validate_stages_related_task_template_ids. \
             assert_called_once_with(task_stages_dto)
 
-    def test_check_for_duplicate_stage_ids_raises_exception(self,
-                                                            task_storage,
-                                                            stage_storage,
-                                                            stage_interactor,
-                                                            task_template_storage):
+    def test_check_for_duplicate_stage_ids_raises_exception(
+            self, task_storage, stage_storage, storage_method_mocks,
+            stage_interactor, task_template_storage):
         # Arrange
         StageDTOFactory.reset_sequence()
         stages_details = StageDTOFactory.create_batch(
                 stage_id="stage_id_1", size=2
         )
         stage_storage.get_existing_stage_ids.return_value = []
-        task_template_storage \
-            .get_valid_task_template_ids_in_given_task_template_ids. \
-            return_value = ["FIN_PR"]
         stage_storage.validate_stages_related_task_template_ids.return_value \
             = []
 
@@ -355,15 +264,12 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
             stage_storage, task_storage, stage_interactor):
         # Arrange
         stages_details = create_stage_dtos
-        storage = stage_storage
         task_template_ids = ["task_template_id_1", "task_template_id_2"]
+        self.interactors_mock(mocker)
+        self.adapters_mock(mocker)
         task_template_storage \
-            .get_valid_task_template_ids_in_given_task_template_ids \
-            .return_value = []
-        from ib_tasks.tests.common_fixtures.adapters.roles_service \
-            import get_valid_role_ids_in_given_role_ids
-        mocker_obj = get_valid_role_ids_in_given_role_ids(mocker)
-        mocker_obj.return_value = ["role_id_1", "role_id_2", "role_id_0"]
+            .get_valid_task_template_ids_in_given_task_template_ids. \
+            return_value = ["task_template_id_1"]
 
         # Act
         with pytest.raises(InvalidTaskTemplateIds) as error:
@@ -397,34 +303,16 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
     def test_validate_fields_of_given_task_template_raises_exception(
             self, mocker, create_stage_dtos, stage_interactor,
             create_task_stages_dtos, valid_stages_dto, task_storage,
-            stage_storage, task_template_storage):
+            stage_storage, task_template_storage, storage_method_mocks):
         # Arrange
         stages_details = create_stage_dtos
-
+        valid_template_ids = ["task_template_id_1", "task_template_id_2"]
         storage = stage_storage
         storage.get_existing_stage_ids.return_value = \
             ["stage_id_0", "stage_id_1"]
-        storage.validate_stages_related_task_template_ids.return_value = []
-        stage_storage.get_existing_status_ids.return_value = ["status1"]
-
-        stage_logics = [StageDisplayLogicDTO(
-                current_stage="stage_id_1",
-                display_logic_dto=StatusOperandStageDTO(
-                        variable="status1",
-                        operator="==",
-                        stage="stage_id_1"))]
-        get_stage_display_logic_mock(mocker, stage_logics)
-
-        from ib_tasks.tests.common_fixtures.adapters.roles_service \
-            import get_valid_role_ids_in_given_role_ids
-        mocker_obj = get_valid_role_ids_in_given_role_ids(mocker)
-        mocker_obj.return_value = ["role_id_1", "role_id_2", "role_id_0"]
-
-        valid_template_ids = ["task_template_id_1", "task_template_id_2"]
         task_template_storage \
             .get_valid_task_template_ids_in_given_task_template_ids. \
             return_value = valid_template_ids
-
         task_storage.get_field_ids_for_given_task_template_ids.return_value = [
                 TemplateFieldsDTO(
                         task_template_id="task_template_id_1",
@@ -434,6 +322,10 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
                         task_template_id="task_template_id_2",
                         field_ids=["field_id_1", "field_id_0"]
                 )]
+        storage.validate_stages_related_task_template_ids.return_value = []
+        stage_storage.get_existing_status_ids.return_value = ["status1"]
+        self.interactors_mock(mocker)
+        self.adapters_mock(mocker)
 
         # Act
         with pytest.raises(InvalidTemplateFields):
@@ -449,7 +341,7 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
 
     def test_validate_stage_display_logic_invalid_stage_display_logic_raises_exception(
             self, task_template_storage, stage_storage, task_storage, mocker,
-            stage_interactor):
+            stage_interactor, storage_method_mocks):
         # Arrange
 
         StageDTOFactory.reset_sequence()
@@ -459,20 +351,8 @@ class TestCreateOrUpdateStageInformation(StorageMethodsMock):
         storage = stage_storage
 
         storage.get_existing_stage_ids.return_value = []
-        storage.get_existing_status_ids.return_value = []
-        task_storage.get_field_ids_for_given_task_template_ids.return_value = [
-                TemplateFieldsDTO(
-                        task_template_id="task_template_id_1",
-                        field_ids=["field_id_1", "field_id_2"]
-                ),
-                TemplateFieldsDTO(
-                        task_template_id="task_template_id_2",
-                        field_ids=["field_id_1", "field_id_2"]
-                )]
         self.interactors_mock(mocker)
-        task_template_storage \
-            .get_valid_task_template_ids_in_given_task_template_ids. \
-            return_value = ["task_template_id_1", "task_template_id_2"]
+        self.adapters_mock(mocker)
 
         # Act
         with pytest.raises(InvalidStageDisplayLogic) as error:
