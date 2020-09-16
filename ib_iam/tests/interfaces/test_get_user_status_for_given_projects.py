d4b76c1e4af2abd21cc90cf1d206e65c99be66f2
DurgaPrime <durgaprasad@ibhubs.co>
2020-09-16 15:30:36 +0530

    run test cases

--- ib_iam/tests/interfaces/test_get_user_status_for_given_projects.py
+++ ib_iam/tests/interfaces/test_get_user_status_for_given_projects.py
@@ -106,10 +106,17 @@ class TestGetUserStatusForGivenProjects:
                 is_exist=project_id in valid_user_exist_project_ids
             ) for project_id in project_ids
         ]
+        expected_result = sorted(
+            expected_result,
+            key=lambda user_status_dto: user_status_dto.user_id
+        )
 
         result = service_interface.get_user_status_for_given_projects(
             user_id=user_id, project_ids=project_ids)
 
+        result = sorted(
+            result, key=lambda user_status_dto: user_status_dto.user_id
+        )
         assert expected_result == result
 
     @pytest.mark.django_db
