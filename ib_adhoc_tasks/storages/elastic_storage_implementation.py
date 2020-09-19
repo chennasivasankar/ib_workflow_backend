54eba7ddfc5dff323c64c9913152fde4b0c98afe
Your Name <jaswanth.mamidipudi@ibhubs.co.in>
2020-09-19 23:26:44 +0530

    written elastic storage implementation file

---
+++ ib_adhoc_tasks/storages/elastic_storage_implementation.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from ib_adhoc_tasks.interactors.storage_interfaces.dtos import \
+    FilterForStageDTO, FilterForFieldDTO
+from ib_adhoc_tasks.interactors.storage_interfaces \
+    .elastic_storage_interface import ElasticStorageInterface
+
+
+class ElasticStorageImplementation(ElasticStorageInterface):
+
+    def get_task_ids_filtered_on_stage_id(
+            self, filter_for_stage_dto: FilterForStageDTO
+    ) -> List[str]:
+        pass
+
+    def get_task_ids_filtered_on_given_field(
+            self,
+            filter_for_field_dto: FilterForFieldDTO
+    ):
+        pass
