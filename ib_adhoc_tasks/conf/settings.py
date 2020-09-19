88dbf33d7b6601810facecb30f405eda72759573
prasaduu <arlabandaswamydurgaprasad@gmail.com>
2020-09-19 20:43:39 +0530

    app name of adhoc project

---
+++ ib_adhoc_tasks/conf/settings.py
@@ -0,0 +1,1 @@
+# write your ib_adhoc_tasks settings
--- ib_adhok_tasks/app.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class IbAdhok-tasksAppConfig(AppConfig):
-    name = "ib_adhok_tasks"
-
-    def ready(self):
-        from ib_adhok-tasks import signals # pylint: disable=unused-variable
