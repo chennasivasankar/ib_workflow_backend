8b02e73b44d0600a995a4eedee1d0db759c8208e
venkata.bandaru <venkata.bandaru@ibhubs.co.in>
2020-09-19 14:23:06 +0530

    add snapshots for success test case in create task

---
+++ ib_tasks/tests/views/create_task/snapshots/snap_test_case_08.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['TestCase15CreateTaskAPITestCase.test_case status_code'] = '400'
+
+snapshots['TestCase15CreateTaskAPITestCase.test_case body'] = {
+    'http_status_code': 400,
+    'res_status': 'USER_DID_NOT_FILL_REQUIRED_FIELDS',
+    'response': "user did not fill required fields: ['DISPLAY_NAME-1']"
+}
