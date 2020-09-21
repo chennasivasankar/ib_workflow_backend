25bff6f33f47c8739542d14778a6fac91801c29b
Your Name <jaswanth.mamidipudi@ibhubs.co.in>
2020-09-21 13:25:39 +0530

    Formattings and divided large code into functions

--- ib_adhoc_tasks/storages/elastic_storage_implementation.py
+++ ib_adhoc_tasks/storages/elastic_storage_implementation.py
@@ -3,7 +3,7 @@ from typing import List
 from elasticsearch_dsl import Q, Search
 
 from ib_adhoc_tasks.interactors.dtos import TaskIdsAndCountDTO, \
-    TaskIdsForGroupsParameterDTO
+    TaskIdsForGroupsParameterDTO, GroupByValueDTO
 from ib_adhoc_tasks.interactors.storage_interfaces \
     .elastic_storage_interface import ElasticStorageInterface
 
@@ -22,26 +22,14 @@ class ElasticStorageImplementation(ElasticStorageInterface):
         )
         from ib_adhoc_tasks.constants.config import TASK_INDEX_NAME
         search = Search(index=TASK_INDEX_NAME)
-        query = Q('term',
-                  project_id__keyword=task_ids_for_groups_parameter_dto.project_id) \
-                & Q('term',
-                    template_id__keyword=task_ids_for_groups_parameter_dto.template_id) \
-                & Q('terms', stages__stage_id__keyword=stage_ids)
+        query = self._get_query_for_project_template_and_stage_ids(
+            stage_ids=stage_ids,
+            task_ids_for_groups_parameter_dto=task_ids_for_groups_parameter_dto
+        )
         for groupby_dto in task_ids_for_groups_parameter_dto.groupby_value_dtos:
-            from ib_adhoc_tasks.constants.enum import GroupByType
-            if groupby_dto.group_by_display_name == GroupByType.ASSIGNEE.value:
-                query &= Q(
-                    'term',
-                    assignees__assignee_id__keyword=groupby_dto.group_by_value
-                )
-            elif groupby_dto.group_by_display_name == GroupByType.STAGE.value:
-                query &= Q(
-                    'term',
-                    stages__stage_id__keyword=groupby_dto.group_by_value
-                )
-            else:
-                attribute = groupby_dto.group_by_display_name + '.keyword'
-                query &= Q('term', **{attribute: groupby_dto.group_by_value})
+            query &= self._get_query_for_given_groupby_field(
+                groupby_value_dto=groupby_dto
+            )
 
         task_objects = search.filter(query)
         total_tasks_count = task_objects.count()
@@ -55,3 +43,34 @@ class ElasticStorageImplementation(ElasticStorageInterface):
             task_ids=task_ids,
             total_tasks_count=total_tasks_count
         )
+
+    def _get_query_for_given_groupby_field(
+            self, groupby_value_dto: GroupByValueDTO
+    ):
+        from ib_adhoc_tasks.constants.enum import GroupByType
+        if groupby_value_dto.group_by_display_name == GroupByType.ASSIGNEE.value:
+            return Q(
+                'term',
+                assignees__assignee_id__keyword=groupby_value_dto.group_by_value
+            )
+        elif groupby_value_dto.group_by_display_name == GroupByType.STAGE.value:
+            return Q(
+                'term',
+                stages__stage_id__keyword=groupby_value_dto.group_by_value
+            )
+        else:
+            attribute = groupby_value_dto.group_by_display_name + '.keyword'
+            return Q('term', **{attribute: groupby_value_dto.group_by_value})
+
+    def _get_query_for_project_template_and_stage_ids(
+            self,
+            stage_ids: List[str],
+            task_ids_for_groups_parameter_dto: TaskIdsForGroupsParameterDTO
+    ):
+        query = \
+            Q('term',
+              project_id__keyword=task_ids_for_groups_parameter_dto.project_id) \
+            & Q('term',
+                template_id__keyword=task_ids_for_groups_parameter_dto.template_id) \
+            & Q('terms', stages__stage_id__keyword=stage_ids)
+        return query
