ed5418f3daf9647b95c42ed0a4f2f15383578e7c
venkata.bandaru <venkata.bandaru@ibhubs.co.in>
2020-09-21 16:33:11 +0530

    refactor create transition checklist api and add some test cases as well since create transition checklist has changed to create or update transition checklist

--- ib_tasks/storages/action_storage_implementation.py
+++ ib_tasks/storages/action_storage_implementation.py
@@ -57,7 +57,7 @@ class ActionsStorageImplementation(ActionStorageInterface):
             raise InvalidStageId(stage_id)
         return
 
-    def validate_transition_template_id_is_related_to_given_stage_action(
+    def validate_transition_template_relation_with_action(
             self, transition_checklist_template_id, action_id, stage_id
     ) -> Optional[TransitionTemplateIsNotRelatedToGivenStageAction]:
         transition_checklist_template_is_related_to_given_stage_action = \
---
+++ ib_tasks/tests/views/create_transition_checklist/snapshots/snap_test_case_34.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case status_code'] = '201'
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case body'] = {
+    'message': 'transition checklist created successfully'
+}
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case task_id'] = 1
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case template_id'] = 'transition_template_1'
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case task_title'] = 'title_0'
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case task_description'] = 'description_0'
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case task_start_date'] = '2020-10-12 04:40:00'
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case task_due_date'] = '2020-10-22 04:40:00'
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case task_priority'] = 'HIGH'
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case same_gof_order_1'] = 1
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case gof_id_1'] = 'FIN_REQUESTOR_DETAILS'
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case gof_task_id_1'] = 1
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case same_gof_order_2'] = 1
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case gof_id_2'] = 'gof_1'
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case gof_task_id_2'] = 1
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case task_gof_1'] = 1
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case field_1'] = 'FIN_TEAM_NAME'
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case field_response_1'] = 'Beta Team - updated'
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case task_gof_2'] = 2
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case field_2'] = 'field_1'
+
+snapshots['TestCase34CreateTransitionChecklistAPITestCase.test_case field_response_2'] = 'HELLO WORLD'
