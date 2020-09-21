d64c5eace23f495b70eacc3cdf6bc25f061e2085
venkata.bandaru <venkata.bandaru@ibhubs.co.in>
2020-09-21 11:30:26 +0530

    add save and act on task integration tests

---
+++ ib_tasks/tests/views/save_and_act_on_a_task/snapshots/snap_test_case_08.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['TestCase08SaveAndActOnATaskAPITestCase.test_case status_code'] = '400'
+
+snapshots['TestCase08SaveAndActOnATaskAPITestCase.test_case body'] = {
+    'http_status_code': 400,
+    'res_status': 'DUE_DATE_TIME_IS_REQUIRED',
+    'response': 'due datetime is required if action type is not no validations'
+}
