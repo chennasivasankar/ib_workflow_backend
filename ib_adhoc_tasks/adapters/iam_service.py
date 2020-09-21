c2c852c8a06f71fd13bec9587a45887cd6c9c8e5
prasaduu <arlabandaswamydurgaprasad@gmail.com>
2020-09-21 20:03:38 +0530

    Add files to gitignore

--- ib_adhoc_tasks/adapters/iam_service.py
+++ ib_adhoc_tasks/adapters/iam_service.py
@@ -8,7 +8,7 @@ class IamService:
         from ib_iam.app_interfaces.service_interface import ServiceInterface
         return ServiceInterface()
 
-    def get_user_role_ids(self, user_id: str):
+    def get_user_role_ids(self, user_id: str) -> List[str]:
         user_role_ids = self.interface.get_user_role_ids(
             user_id=user_id
         )
