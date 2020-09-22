3fd86adf6284717fc00e68d97b1374a836bc8138
prasaduu <arlabandaswamydurgaprasad@gmail.com>
2020-09-22 11:00:51 +0530

    Fix spec Changes

---
+++ ib_adhoc_tasks/tests/views/get_adhoc_tasks_for_kanban_view/test_case_01.py
@@ -0,0 +1,34 @@
+"""
+# TODO: Update test case description
+"""
+import pytest
+from django_swagger_utils.utils.test_utils import TestUtils
+from . import APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX
+
+
+class TestCase01GetAdhocTasksForKanbanViewAPITestCase(TestUtils):
+    APP_NAME = APP_NAME
+    OPERATION_NAME = OPERATION_NAME
+    REQUEST_METHOD = REQUEST_METHOD
+    URL_SUFFIX = URL_SUFFIX
+    SECURITY = {'oauth': {'scopes': ['read']}}
+
+    @pytest.mark.django_db
+    def test_case(self, snapshot):
+        body = {}
+        path_params = {}
+        query_params = {
+            'project_id': 'string',
+            'limit': 772,
+            'offset': 564,
+            'group1_limit': 10,
+            'group1_offset': 198,
+            'group2_limit': 10,
+            'group2_offset': 215
+        }
+        headers = {}
+        response = self.make_api_call(body=body,
+                                      path_params=path_params,
+                                      query_params=query_params,
+                                      headers=headers,
+                                      snapshot=snapshot)
