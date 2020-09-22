073a5fbfbf10c9374dbcbc5c4fcd9474c8f68766
Jaswanth <jaswanth.mamidipudi@ibhubs.co.in>
2020-09-22 22:56:11 +0530

    controller of get_subtask_of_task after build

---
+++ ib_adhoc_tasks/tests/views/get_subtasks_of_task/__init__.py
@@ -0,0 +1,6 @@
+# pylint: disable=wrong-import-position
+
+APP_NAME = "ib_adhoc_tasks"
+OPERATION_NAME = "get_subtasks_of_task"
+REQUEST_METHOD = "post"
+URL_SUFFIX = "projects/adhoc_board/groups/task/subtasks/v1/"
