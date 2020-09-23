67ce260ee5c0977c86f3e1d97ecdf132bf7eddda
pavankumar <pavankumar.pamuru@ibhubs.com.in>
2020-09-23 15:36:49 +0530

    remove imports

--- ib_tasks/interactors/storage_interfaces/task_storage_interface.py
+++ ib_tasks/interactors/storage_interfaces/task_storage_interface.py
@@ -18,15 +18,9 @@ from ib_tasks.interactors.storage_interfaces.stage_dtos import \
     TaskIdWithStageValueDTO, StageIdWithTemplateIdDTO, TaskStagesDTO, StageDTO
 from ib_tasks.interactors.storage_interfaces.status_dtos import \
     TaskTemplateStatusDTO, StatusVariableDTO
-from ib_tasks.interactors.storage_interfaces.task_dtos import \
-    TaskDisplayIdDTO, \
-    TaskProjectDTO, TaskDueMissingDTO, \
 from ib_tasks.interactors.storage_interfaces.task_dtos import TaskDisplayIdDTO, \
     TaskProjectDTO, TaskDueMissingDTO, \
     SubTasksCountDTO, SubTasksIdsDTO
-from ib_tasks.interactors.task_dtos import CreateTaskLogDTO, \
-    GetTaskDetailsDTO, \
-    TaskDelayParametersDTO
 from ib_tasks.interactors.task_dtos import CreateTaskLogDTO, GetTaskDetailsDTO, \
     TaskDelayParametersDTO
 
