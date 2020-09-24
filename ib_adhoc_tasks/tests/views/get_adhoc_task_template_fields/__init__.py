402ffb766e5dc738da898fdeb883bfc88076fe8c
Jaswanth <jaswanth.mamidipudi@ibhubs.co.in>
2020-09-24 15:08:38 +0530

    modify get_task_template_fields method from post to get

--- ib_adhoc_tasks/tests/views/get_adhoc_task_template_fields/__init__.py
+++ ib_adhoc_tasks/tests/views/get_adhoc_task_template_fields/__init__.py
@@ -2,5 +2,5 @@
 
 APP_NAME = "ib_adhoc_tasks"
 OPERATION_NAME = "get_adhoc_task_template_fields"
-REQUEST_METHOD = "post"
+REQUEST_METHOD = "get"
 URL_SUFFIX = "project/adhoc_board/adhoc_template/fields/v1/"
