125ca634024a115fb064916527afe6650b388cd8
Jaswanth <jaswanth.mamidipudi@ibhubs.co.in>
2020-09-24 15:39:03 +0530

    Add validation mixin with some methods for invali project, user, limit, offset

--- ib_adhoc_tasks/interactors/mixins/validation_mixin.py
+++ ib_adhoc_tasks/interactors/mixins/validation_mixin.py
@@ -5,8 +5,8 @@ class ValidationMixin:
         from ib_adhoc_tasks.exceptions.custom_exceptions import \
             InvalidProjectId
         from ib_adhoc_tasks.adapters.service_adapter import get_service_adapter
-        service_adapter = get_service_adapter()
-        valid_project_ids = service_adapter.iam_service.get_valid_project_ids(
+        service = get_service_adapter()
+        valid_project_ids = service.iam_service.get_valid_project_ids(
             project_ids=[project_id]
         )
         is_project_id_invalid = not valid_project_ids
@@ -15,16 +15,27 @@ class ValidationMixin:
 
     @staticmethod
     def _validate_is_user_exists_in_project(project_id: str, user_id: str):
-        from ib_adhoc_tasks.exceptions.custom_exceptions import \
-            UserNotExistInProject
         from ib_adhoc_tasks.adapters.service_adapter import get_service_adapter
         service_adapter = get_service_adapter()
-        is_user_exist_in_project = service_adapter.iam_service \
-            .is_user_exist_in_project(user_id=user_id)
-        is_not_user_exist_in_project = not is_user_exist_in_project
-        if is_not_user_exist_in_project:
-            raise UserNotExistInProject
+        service_adapter.iam_service.is_valid_user_id_for_given_project(
+            user_id=user_id, project_id=project_id
+        )
 
-    # todo invalid limit
+    @staticmethod
+    def _validate_user_project_and_user_existence_in_project(
+            project_id: str, user_id: str
+    ):
+        from ib_adhoc_tasks.adapters.service_adapter import get_service_adapter
+        service = get_service_adapter()
+        service.iam_service.is_valid_user_id_for_given_project(
+            project_id=project_id, user_id=user_id
+        )
 
-    # todo invalid offset
+    @staticmethod
+    def _validate_limit_offset_values(limit: int, offset: int):
+        from ib_adhoc_tasks.exceptions.custom_exceptions import \
+            InvalidGroupLimitValue, InvalidGroupOffsetValue
+        if limit < 0:
+            raise InvalidGroupLimitValue
+        if offset < 0:
+            raise InvalidGroupOffsetValue
