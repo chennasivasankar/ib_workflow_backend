d66a23869ae44b130c6a81f51d1ae093154d0212
Jaswanth <jaswanth.mamidipudi@ibhubs.co.in>
2020-09-24 14:55:51 +0530

    Add get_task_template_fields controller's success test and its snapshot

---
+++ ib_adhoc_tasks/tests/views/get_adhoc_task_template_fields/snapshots/snap_test_case_01.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['TestCase01GetAdhocTaskTemplateFieldsAPITestCase.test_given_valid_data_returns_group_by_key_and_display_names status_code'] = '200'
+
+snapshots['TestCase01GetAdhocTaskTemplateFieldsAPITestCase.test_given_valid_data_returns_group_by_key_and_display_names body'] = [
+    {
+        'display_name': 'field_display_name1',
+        'group_by_key': 'field_1'
+    },
+    {
+        'display_name': 'STAGE',
+        'group_by_key': 'STAGE'
+    },
+    {
+        'display_name': 'ASSIGNEE',
+        'group_by_key': 'ASSIGNEE'
+    }
+]
