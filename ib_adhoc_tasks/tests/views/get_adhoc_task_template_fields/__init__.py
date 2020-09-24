8c6d8a91e2c11b060e7586f7d2149e918574b314
Jaswanth <jaswanth.mamidipudi@ibhubs.co.in>
2020-09-24 12:35:17 +0530

    Add get_task_template_fields controller and its test file

---
+++ ib_adhoc_tasks/tests/views/get_adhoc_task_template_fields/__init__.py
@@ -0,0 +1,6 @@
+# pylint: disable=wrong-import-position
+
+APP_NAME = "ib_adhoc_tasks"
+OPERATION_NAME = "get_adhoc_task_template_fields"
+REQUEST_METHOD = "post"
+URL_SUFFIX = "project/adhoc_board/adhoc_template/fields/v1/"
