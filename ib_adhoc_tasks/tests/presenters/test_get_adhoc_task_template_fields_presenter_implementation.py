3904b5da96162e08a21e7decb16fdc66809d2a7a
Jaswanth <jaswanth.mamidipudi@ibhubs.co.in>
2020-09-24 14:56:51 +0530

    Add reset sequence to a factory in get_task_template_fields presenter implementation removed unnecessary code

--- ib_adhoc_tasks/tests/presenters/test_get_adhoc_task_template_fields_presenter_implementation.py
+++ ib_adhoc_tasks/tests/presenters/test_get_adhoc_task_template_fields_presenter_implementation.py
@@ -16,14 +16,9 @@ class TestGetGroupByPresenterImplementation:
             self, presenter, snapshot
     ):
         # Arrange
-        from ib_adhoc_tasks.tests.factories.storage_dtos import \
-            GroupByResponseDTOFactory
-        GroupByResponseDTOFactory.reset_sequence(0)
-        GroupByResponseDTOFactory.order.reset()
-        GroupByResponseDTOFactory.group_by_key.reset()
-        GroupByResponseDTOFactory.display_name.reset()
         from ib_adhoc_tasks.tests.factories.adapter_dtos import \
             FieldIdAndNameDTOFactory
+        FieldIdAndNameDTOFactory.reset_sequence(0)
         field_dtos = FieldIdAndNameDTOFactory.create_batch(size=2)
 
         # Act
