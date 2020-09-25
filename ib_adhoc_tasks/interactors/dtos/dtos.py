d41b08c651c6a8aa0af502b26c202f8ce75de133
Jaswanth <jaswanth.mamidipudi@ibhubs.co.in>
2020-09-25 11:21:05 +0530

    Add interactor and its test for get_task_template_fields_and_group_by_fields

--- ib_adhoc_tasks/interactors/dtos/dtos.py
+++ ib_adhoc_tasks/interactors/dtos/dtos.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
 from typing import List, Optional
 
+from ib_adhoc_tasks.adapters.dtos import FieldIdAndNameDTO
 from ib_adhoc_tasks.constants.enum import GroupByEnum, ViewType
+from ib_adhoc_tasks.interactors.storage_interfaces.dtos import \
+    GroupByResponseDTO
 
 
 @dataclass
@@ -109,3 +112,9 @@ class GetChildGroupsInGroupInputDTO:
     group_limit: int
     group_offset: int
     group_by_value: str
+
+
+@dataclass
+class TemplateFieldsAndGroupByFieldsDTO:
+    group_by_fields_dtos: List[GroupByResponseDTO]
+    field_dtos: List[FieldIdAndNameDTO]
