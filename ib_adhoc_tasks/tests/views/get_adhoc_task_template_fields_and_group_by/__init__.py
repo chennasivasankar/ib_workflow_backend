8b24af0227f3564e17aaa8a086624641bd41ca32
Jaswanth <jaswanth.mamidipudi@ibhubs.co.in>
2020-09-25 14:43:21 +0530

    fixed a typo in urls and build

--- ib_adhoc_tasks/tests/views/get_adhoc_task_template_fields_and_group_by/__init__.py
+++ ib_adhoc_tasks/tests/views/get_adhoc_task_template_fields_and_group_by/__init__.py
@@ -3,4 +3,4 @@
 APP_NAME = "ib_adhoc_tasks"
 OPERATION_NAME = "get_adhoc_task_template_fields_and_group_by"
 REQUEST_METHOD = "get"
-URL_SUFFIX = "project/adhoc_board/adhoc_template/group_by_and_fields/v1/"
+URL_SUFFIX = "projects/adhoc_board/adhoc_template/group_by_and_fields/v1/"
