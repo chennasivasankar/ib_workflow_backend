a4231ae6c43df9ab65a26b8da48a5ebaebf2dca6
venkata.bandaru <venkata.bandaru@ibhubs.co.in>
2020-09-28 13:05:47 +0530

    add create sub task integration tests up to user action permission denied exception

---
+++ ib_tasks/tests/views/create_sub_task/snapshots/snap_test_case_39.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['TestCase39CreateSubTaskAPITestCase.test_case status_code'] = '400'
+
+snapshots['TestCase39CreateSubTaskAPITestCase.test_case body'] = {
+    'http_status_code': 400,
+    'res_status': 'INVALID_TIME_FORMAT',
+    'response': 'given invalid format for time: 20:70:00 for field: DISPLAY_NAME-0! Try with this format: %H:%M:%S'
+}
