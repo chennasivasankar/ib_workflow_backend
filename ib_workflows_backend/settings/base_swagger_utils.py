c5cc7ca61ef5ac207640d8253f973606b6948f8d
venkata.bandaru <venkata.bandaru@ibhubs.co.in>
2020-09-29 14:21:02 +0530

    add environment variables in base_swagger_utils related django silk

--- ib_workflows_backend/settings/base_swagger_utils.py
+++ ib_workflows_backend/settings/base_swagger_utils.py
@@ -139,3 +139,9 @@ RESET_PASSWORD_LINK = '{frontend_url}update-password?token='.format(
 USER_VERIFICATION_EMAIL_LINK = "{frontend_url}verify-email?token=".format(
     frontend_url=FRONTEND_URL
 )
+
+# ************************** django silk ************************
+
+SILKY_PYTHON_PROFILER = True
+SILKY_PYTHON_PROFILER_BINARY = True
+SILKY_STORAGE_CLASS = 'silk.storage.ProfilerResultStorage'
