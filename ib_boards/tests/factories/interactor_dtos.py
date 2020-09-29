086dd22279b42a32fa60f3d6c76370ce829abe1f
Vedavidh Budimuri <vedavidhbudimuri@gmail.com>
2020-09-29 20:11:56 +0530

    fixed GetTaskDetailsDTOFactory renaming issue

--- ib_boards/tests/factories/interactor_dtos.py
+++ ib_boards/tests/factories/interactor_dtos.py
@@ -117,7 +117,6 @@ class GetTaskDetailsDTOFactory(factory.Factory):
         model = GetTaskDetailsDTO
 
     stage_id = factory.Sequence(lambda n: f'STAGE_ID_{n + 1}')
-    task_display_id = factory.Sequence(lambda n: f'TASK_ID_{n + 1}')
     task_id = factory.Sequence(lambda n: n + 1)
 
 
