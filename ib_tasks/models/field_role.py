5fccca3aa198869cf75f2eb8660fb3da4142c7be
rajesh.ganjikunta <rajesh.ganjikunta@ibhubs.co.in>
2020-09-29 16:14:51 +0530

    add index to role, field

--- ib_tasks/models/field_role.py
+++ ib_tasks/models/field_role.py
@@ -5,12 +5,15 @@ from ib_tasks.models.field import Field
 
 
 class FieldRole(models.Model):
-    field = models.ForeignKey(Field, on_delete=models.CASCADE, db_index=True)
-    role = models.CharField(max_length=50)
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, db_index=True)
     permission_type = models.CharField(max_length=100,
                                        choices=Permission_Types)
 
-
+    class Meta:
+        indexes = [
+            models.Index(fields=['role', 'permission_type']),
+        ]
 
     def __str__(self):
         return "{} with {}".format(self.field_id, self.role)
