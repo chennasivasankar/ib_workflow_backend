e39aa727095535c4dceb075c5581eccb7734539c
rajesh.ganjikunta <rajesh.ganjikunta@ibhubs.co.in>
2020-09-29 20:03:04 +0530

    change role_id length in stage_permitted_roles, action_permitted_roles

--- ib_tasks/models/stage_permitted_roles.py
+++ ib_tasks/models/stage_permitted_roles.py
@@ -5,7 +5,7 @@ from ib_tasks.models import Stage
 
 class StagePermittedRoles(models.Model):
     stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
-    role_id = models.CharField(max_length=200, db_index=True)
+    role_id = models.CharField(max_length=50, db_index=True)
 
     def __str__(self):
         return f"{self.role_id} of {self.stage.stage_id}"
