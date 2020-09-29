2f5516544958a9fb755e9a41d749eae565d6b77e
Jaswanth <jaswanth.mamidipudi@ibhubs.co.in>
2020-09-29 20:26:31 +0530

    Add pm and sub users interactor

---
+++ ib_iam/interactors/users/add_pm_and_sub_users_interactor.py
@@ -0,0 +1,146 @@
+from typing import List
+
+from ib_iam.constants.config import DEFAULT_TEAM_ID, DEFAULT_TEAM_NAME, \
+    LEVEL_0_HIERARCHY, LEVEL_0_NAME, LEVEL_1_HIERARCHY, LEVEL_1_NAME
+from ib_iam.interactors.dtos.dtos import PMAndSubUsersAuthTokensDTO, \
+    TeamMemberLevelIdWithMemberIdsDTO, ImmediateSuperiorUserIdWithUserIdsDTO
+from ib_iam.interactors.storage_interfaces.dtos import UserIdWithTokenDTO
+from ib_iam.interactors.storage_interfaces.project_storage_interface import \
+    ProjectStorageInterface
+from ib_iam.interactors.storage_interfaces \
+    .team_member_level_storage_interface import TeamMemberLevelStorageInterface
+from ib_iam.interactors.storage_interfaces.team_storage_interface import \
+    TeamStorageInterface
+from ib_iam.interactors.storage_interfaces.user_storage_interface import \
+    UserStorageInterface
+
+
+class PMAndSubUsersInteractor:
+
+    def __init__(
+            self,
+            team_storage: TeamStorageInterface,
+            project_storage: ProjectStorageInterface,
+            user_storage: UserStorageInterface,
+            team_member_level_storage: TeamMemberLevelStorageInterface
+    ):
+        self.project_storage = project_storage
+        self.team_storage = team_storage
+        self.team_member_level_storage = team_member_level_storage
+        self.user_storage = user_storage
+
+    def add_pm_and_sub_users(
+            self, pm_and_sub_user_dtos: List[PMAndSubUsersAuthTokensDTO]
+    ):
+        user_tokens = self.get_all_user_tokens(
+            pm_and_sub_user_dtos=pm_and_sub_user_dtos
+        )
+        user_id_with_token_dtos = self.user_storage.get_user_and_token_dtos(
+            tokens=user_tokens
+        )
+
+        pm_id_and_sub_user_ids = self.get_pm_and_sub_users(
+            user_id_with_token_dtos=user_id_with_token_dtos,
+            pm_and_sub_user_dtos=pm_and_sub_user_dtos
+        )
+
+        team_id = DEFAULT_TEAM_ID
+        team_name = DEFAULT_TEAM_NAME
+        is_created = self.team_storage.get_or_create(
+            team_id=team_id, name=team_name
+        )
+        if is_created:
+            from ib_iam.constants.config import PROJECT_ID
+            self.project_storage.assign_teams_to_projects(
+                project_id=PROJECT_ID, team_ids=[team_id]
+            )
+        self.add_pm_and_sub_users_to_levels_for_a_team(
+            pm_id_and_sub_user_ids=pm_id_and_sub_user_ids, team_id=team_id
+        )
+        self.add_pm_and_sub_users_as_superior_and_members(
+            pm_id_and_sub_user_ids=pm_id_and_sub_user_ids, team_id=team_id
+        )
+
+    @staticmethod
+    def get_all_user_tokens(
+            pm_and_sub_user_dtos: List[PMAndSubUsersAuthTokensDTO]
+    ):
+        user_tokens = []
+        for pm_and_sub_user_dto in pm_and_sub_user_dtos:
+            user_tokens.extend(
+                [pm_and_sub_user_dto.pm_auth_token,
+                 pm_and_sub_user_dto.sub_user_auth_token]
+            )
+        user_tokens = list(set(user_tokens))
+        return user_tokens
+
+    def add_pm_and_sub_users_as_superior_and_members(
+            self, pm_id_and_sub_user_ids, team_id: str,
+    ):
+        member_level_hierarchy = 0
+        immediate_superior_user_id_with_member_ids_dtos = [
+            ImmediateSuperiorUserIdWithUserIdsDTO(
+                immediate_superior_user_id=pm_id,
+                member_ids=user_ids
+            )
+            for pm_id, user_ids in pm_id_and_sub_user_ids.items()
+        ]
+
+        self.team_member_level_storage.add_members_to_superiors(
+            team_id=team_id, member_level_hierarchy=member_level_hierarchy,
+            immediate_superior_user_id_with_member_ids_dtos=
+            immediate_superior_user_id_with_member_ids_dtos
+        )
+
+    def add_pm_and_sub_users_to_levels_for_a_team(
+            self, pm_id_and_sub_user_ids,
+            team_id: str
+    ):
+        team_member_level_0 = self.team_member_level_storage \
+            .get_or_create_team_member_level_hierarchy(
+            team_id=team_id, level_hierarchy=LEVEL_0_HIERARCHY,
+            level_name=LEVEL_0_NAME
+        )
+        team_member_level_1 = self.team_member_level_storage \
+            .get_or_create_team_member_level_hierarchy(
+            team_id=team_id, level_hierarchy=LEVEL_1_HIERARCHY,
+            level_name=LEVEL_1_NAME
+        )
+        pm_ids = list(pm_id_and_sub_user_ids.keys())
+        member_ids = []
+        for sub_user_ids in pm_id_and_sub_user_ids.values():
+            member_ids.extend(sub_user_ids)
+        team_member_level_id_with_member_ids_dtos = [
+            TeamMemberLevelIdWithMemberIdsDTO(
+                team_member_level_id=team_member_level_0,
+                member_ids=pm_ids
+            ),
+            TeamMemberLevelIdWithMemberIdsDTO(
+                team_member_level_id=team_member_level_1,
+                member_ids=member_ids
+            )
+        ]
+        self.team_member_level_storage.add_members_to_levels_for_a_team(
+            team_member_level_id_with_member_ids_dtos=
+            team_member_level_id_with_member_ids_dtos,
+        )
+
+    @staticmethod
+    def get_pm_and_sub_users(
+            user_id_with_token_dtos: List[UserIdWithTokenDTO],
+            pm_and_sub_user_dtos
+    ):
+        user_token_and_id_dictionary = {}
+        for user_id_with_token_dto in user_id_with_token_dtos:
+            user_token_and_id_dictionary[user_id_with_token_dto.token] = \
+                user_id_with_token_dto.user_id
+        pm_id_and_sub_user_ids_dictionary = {}
+        for pm_and_sub_user_dto in pm_and_sub_user_dtos:
+            pm_id_and_sub_user_ids_dictionary[
+                user_token_and_id_dictionary[pm_and_sub_user_dto.pm_auth_token]
+            ].append(
+                user_token_and_id_dictionary[
+                    pm_and_sub_user_dto.sub_user_auth_token
+                ]
+            )
+            return pm_id_and_sub_user_ids_dictionary
