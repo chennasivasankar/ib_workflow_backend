9e974fd733369887861451fdf6b847608590c372
rajesh.ganjikunta <rajesh.ganjikunta@ibhubs.co.in>
2020-09-30 17:45:10 +0530

    fix storage tests in ib_tasks

--- ib_tasks/tests/storages/test_stages_storage_implementation/snapshots/snap_test_get_stage_gof_dtos_for_given_stages_and_gofs.py
+++ ib_tasks/tests/storages/test_stages_storage_implementation/snapshots/snap_test_get_stage_gof_dtos_for_given_stages_and_gofs.py
@@ -8,9 +8,9 @@ from snapshottest import GenericRepr, Snapshot
 snapshots = Snapshot()
 
 snapshots['TestGetStageGoFDTOsForGivenStagesAndGoFs.test_when_stage_gofs_exists_returns_stage_gof_dtos stage_gofs'] = [
-    GenericRepr("StageIdWithGoFIdDTO(stage_id=1, gof_id='gof_1')"),
-    GenericRepr("StageIdWithGoFIdDTO(stage_id=1, gof_id='gof_3')"),
-    GenericRepr("StageIdWithGoFIdDTO(stage_id=2, gof_id='gof_2')")
+    GenericRepr("DBStageIdWithGoFIdDTO(stage_id=1, gof_id='gof_1')"),
+    GenericRepr("DBStageIdWithGoFIdDTO(stage_id=1, gof_id='gof_3')"),
+    GenericRepr("DBStageIdWithGoFIdDTO(stage_id=2, gof_id='gof_2')")
 ]
 
 snapshots['TestGetStageGoFDTOsForGivenStagesAndGoFs.test_when_stage_gofs_not_exists_returns_empty_list stage_gofs'] = [
