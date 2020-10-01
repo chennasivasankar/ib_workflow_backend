928a9426925ae6719ec71da71d487f59a7273f28
Sanmitha Doki <dsanmitha@gmail.com>
2020-10-01 11:13:48 +0530

    change in views and add factories

--- ib_tasks/populate/write_stage_actions.py
+++ ib_tasks/populate/write_stage_actions.py
@@ -16,7 +16,6 @@ def writing_data_to_stage_actions_logic():
         content = ''
         for action_dto in actions_dto:
             content += _define_single_method(action_dto=action_dto)
-            action_dto.py_function_import_path = file_path_name
         formatted_content = FormatCode(content)[0]
         file_.write(formatted_content)
         file_.close()
@@ -31,6 +30,10 @@ def writing_data_to_stage_actions_logic():
 
 
 def _define_single_method(action_dto: StageActionLogicDTO):
+    from django.conf import settings
+    function_path = 'ib_tasks.populate.stage_actions_logic_{}.'.format(
+        settings.STAGE
+    )
     stage_id = action_dto.stage_id
     action_name = action_dto.action_name
     action_logic = action_dto.action_logic
@@ -38,6 +41,8 @@ def _define_single_method(action_dto: StageActionLogicDTO):
     function_name = f'{stage_id}_{action_name}'
     function_name = function_name.replace(' ', '_').replace('-', '_').replace(
         '\n', '')
+    function_path = function_path + function_name
+    action_dto.py_function_import_path = function_path
     function_as_str += f"\n\ndef {function_name}(task_dict, global_constants, stage_value_dict):\n"
 
     action_logic_lines = action_logic.split("\n")
