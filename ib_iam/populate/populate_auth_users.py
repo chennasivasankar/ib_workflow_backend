bd61b52f71c7800bba5476bbf1127296cf32b587
Vedavidh Budimuri <vedavidhbudimuri@gmail.com>
2020-10-04 10:11:05 +0530

    minor changes

--- ib_iam/populate/populate_auth_users.py
+++ ib_iam/populate/populate_auth_users.py
@@ -51,22 +51,23 @@ class AuthUsers:
         max_value = int((len(auth_user_dtos) / chunk_size) + 1)
         import time
         values = []
+        all_failed_data = []
         for i in range(max_value):
             a = time.time()
-            interactor.auth_user_dtos(
+            failed_data = interactor.auth_user_dtos(
                 auth_user_dtos=auth_user_dtos[i * chunk_size:(i + 1) * chunk_size],
                 project_id=project_id,
                 role_ids=role_ids,
                 is_assign_auth_token_users_to_team=is_assign_auth_token_users_to_team
             )
             b = time.time()
-            print("Time Elapsed: ", b-a)
+            print("Time Elapsed: ", b-a, failed_data)
+            all_failed_data += failed_data
             print("Iteration {} out of {}".format(i+1, max_value))
             values.append(b-a)
-            time.sleep(5)
-            input("Enter any key to proceed next: ")
+            time.sleep(10)
         print("Average Time Delay: ", sum(values)/len(values))
-        return
+        return all_failed_data
 
     @staticmethod
     def _convert_auth_user_dtos(auth_users) -> List[AuthUserDTO]:
