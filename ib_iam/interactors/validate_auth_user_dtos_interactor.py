1061afc149b6011fcc79638f4e61c5bc467bd8ee
Jaswanth <jaswanth.mamidipudi@ibhubs.co.in>
2020-10-04 15:19:00 +0530

    Add todos

---
+++ ib_iam/interactors/validate_auth_user_dtos_interactor.py
@@ -0,0 +1,16 @@
+class ValidateAuthUserDTOsInteractor:
+# TODO invitation code not empty
+# invitation code not be a  duplicate, check in given dtos and database too
+# either of email or phone number should exists
+# duplicate email exists, check in given dtos and database too
+
+# Can duplicate auth tokens exists, check in given dtos and database too
+# Can duplicate auth token user_id exists, check in given dtos and database too
+
+# Can duplicate phone_number exists, check in given dtos and database too
+
+{
+    "field": "",
+    "cause": ""
+    "auth_user_dict"
+}
